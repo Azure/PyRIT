c987739dafd5efbe65825a41e57fddea0a59f5ee
Christiano Bianchet <cbia@microsoft.com>
2024-02-08 14:56:06 -0800

    Added flag character transformer to init file.

--- pyrit/prompt_transformer/__init__.py
+++ pyrit/prompt_transformer/__init__.py
@@ -4,6 +4,7 @@
 from pyrit.prompt_transformer.prompt_transformer import PromptTransformer
 from pyrit.prompt_transformer.base64_transformer import Base64Transformer
 from pyrit.prompt_transformer.no_op_transformer import NoOpTransformer
+from pyrit.prompt_transformer.flag_char_transformer import FlagCharTransformer
 
 
-__all__ = ["PromptTransformer", "Base64Transformer", "NoOpTransformer"]
+__all__ = ["PromptTransformer", "Base64Transformer", "NoOpTransformer", "FlagCharTransformer"]
