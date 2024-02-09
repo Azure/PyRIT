06e8097979f518fde4519f6e500fabec7c4a2d2c
Richard Lundeen <rlundeen@microsoft.com>
2024-02-08 17:19:30 -0800

    pr feedback

--- pyrit/common/environment_variables.py
+++ pyrit/common/environment_variables.py
@@ -10,6 +10,14 @@ def get_required_value(environment_variable_name: str, passed_value: str) -> str
     prefering the passed value
 
     If no value is found, raises a KeyError
+
+    :param environment_variable_name: The name of the environment variable
+    :type environment_variable_name: str
+    :param passed_value: The value passed as an argument
+    :type passed_value: str
+    :return: The required value
+    :rtype: str
+    :raises ValueError: If no value is found
     """
     if passed_value:
         return passed_value
