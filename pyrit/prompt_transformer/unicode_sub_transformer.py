3b596ba9799b3460aa75b3fb4db419d9fa05e198
Christiano Bianchet <cbia@microsoft.com>
2024-02-12 11:23:36 -0800

    Fixed unicode transformer formatiing.

--- pyrit/prompt_transformer/unicode_sub_transformer.py
+++ pyrit/prompt_transformer/unicode_sub_transformer.py
@@ -5,8 +5,7 @@ from pyrit.prompt_transformer import PromptTransformer
 
 
 class UnicodeSubstitutionTransformer(PromptTransformer):
-
-    def __init__(self, start_value = 0xE0000):
+    def __init__(self, start_value=0xE0000):
         self.startValue = start_value
 
     def transform(self, prompt: str) -> str:
