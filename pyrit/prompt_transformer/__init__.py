58b8c7c341552fba90a833a746e32f64febcbdfa
ChristBian1 <chris.a.bianchet@gmail.com>
2024-02-12 14:17:57 -0800

    Added Unicode substitution transformer for invisible prompts. (#38)
    * Added Unicode substitution transformer to allow for invisible prompts.
    
    ---------
    
    Co-authored-by: Christiano Bianchet <cbia@microsoft.com>

--- pyrit/prompt_transformer/__init__.py
+++ pyrit/prompt_transformer/__init__.py
@@ -4,6 +4,7 @@
 from pyrit.prompt_transformer.prompt_transformer import PromptTransformer
 from pyrit.prompt_transformer.base64_transformer import Base64Transformer
 from pyrit.prompt_transformer.no_op_transformer import NoOpTransformer
+from pyrit.prompt_transformer.unicode_sub_transformer import UnicodeSubstitutionTransformer
 
 
-__all__ = ["PromptTransformer", "Base64Transformer", "NoOpTransformer"]
+__all__ = ["PromptTransformer", "Base64Transformer", "NoOpTransformer", "UnicodeSubstitutionTransformer"]
--- tests/test_prompt_transformer.py
+++ tests/test_prompt_transformer.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from pyrit.prompt_transformer import Base64Transformer, NoOpTransformer
+from pyrit.prompt_transformer import Base64Transformer, NoOpTransformer, UnicodeSubstitutionTransformer
 
 
 def test_prompt_transformer() -> None:
@@ -12,3 +12,13 @@ def test_prompt_transformer() -> None:
 def test_base64_prompt_transformer() -> None:
     transformer = Base64Transformer()
     assert transformer.transform("test") == "dGVzdA=="
+
+
+def test_unicode_sub_default_prompt_transformer() -> None:
+    transformer = UnicodeSubstitutionTransformer()
+    assert transformer.transform("test") == "\U000e0074\U000e0065\U000e0073\U000e0074"
+
+
+def test_unicode_sub_ascii_prompt_transformer() -> None:
+    transformer = UnicodeSubstitutionTransformer(0x00000)
+    assert transformer.transform("test") == "\U00000074\U00000065\U00000073\U00000074"
