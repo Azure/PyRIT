b1d86f43677a910842437084c3b15b02ca0c5777
Richard Lundeen <rlundeen@microsoft.com>
2024-02-15 22:08:02 -0800

    renaming tests

--- tests/test_common_default.py
+++ tests/test_common_default.py
@@ -7,17 +7,17 @@ import pytest
 from pyrit.common import default_values
 
 
-def test_environment_variables_prefers_passed():
+def test_get_required_value_prefers_passed():
     os.environ["TEST_ENV_VAR"] = "fail"
     assert default_values.get_required_value(env_var_name="TEST_ENV_VAR", passed_value="passed") == "passed"
 
 
-def test_environment_variables_uses_default():
+def test_get_required_value_uses_default():
     os.environ["TEST_ENV_VAR"] = "default"
     assert default_values.get_required_value(env_var_name="TEST_ENV_VAR", passed_value="") == "default"
 
 
-def test_environment_variables_throws_if_not_set():
+def test_get_required_value_throws_if_not_set():
     os.environ["TEST_ENV_VAR"] = ""
     with pytest.raises(ValueError):
         default_values.get_required_value(env_var_name="TEST_ENV_VAR", passed_value="") == "default"
