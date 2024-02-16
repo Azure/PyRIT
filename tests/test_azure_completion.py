f4eb51f8b0f9db00ad70c556e1325c22f4266aa2
Richard Lundeen <rlundeen@microsoft.com>
2024-02-15 21:46:59 -0800

    build pipeline fixes

--- tests/test_azure_completion.py
+++ tests/test_azure_completion.py
@@ -6,15 +6,13 @@ import pytest
 
 from pyrit.completion import AzureCompletion
 
+
 def test_valid_init():
     os.environ[AzureCompletion.API_KEY_ENVIRONMENT_VARIABLE] = ""
-    completion = AzureCompletion(
-            api_key="xxxxx",
-            endpoint="https://mock.azure.com/",
-            deployment="gpt-4"
-        )
+    completion = AzureCompletion(api_key="xxxxx", endpoint="https://mock.azure.com/", deployment="gpt-4")
 
     assert completion is not None
+
 
 def test_valid_init_env():
     os.environ[AzureCompletion.API_KEY_ENVIRONMENT_VARIABLE] = "xxxxx"
@@ -23,6 +21,7 @@ def test_valid_init_env():
 
     completion = AzureCompletion()
     assert completion is not None
+
 
 def test_invalid_key_raises():
     os.environ[AzureCompletion.API_KEY_ENVIRONMENT_VARIABLE] = ""
@@ -34,6 +33,7 @@ def test_invalid_key_raises():
             api_version="some_version",
         )
 
+
 def test_invalid_endpoint_raises():
     os.environ[AzureCompletion.ENDPOINT_URI_ENVIRONMENT_VARIABLE] = ""
     with pytest.raises(ValueError):
@@ -42,6 +42,7 @@ def test_invalid_endpoint_raises():
             deployment="gpt-4",
             api_version="some_version",
         )
+
 
 def test_invalid_deployment_raises():
     os.environ[AzureCompletion.DEPLOYMENT_ENVIRONMENT_VARIABLE] = ""
--- tests/test_azure_text_embedding.py
+++ tests/test_azure_text_embedding.py
@@ -6,15 +6,13 @@ import pytest
 
 from pyrit.embedding import AzureTextEmbedding
 
+
 def test_valid_init():
     os.environ[AzureTextEmbedding.API_KEY_ENVIRONMENT_VARIABLE] = ""
-    completion = AzureTextEmbedding(
-            api_key="xxxxx",
-            endpoint="https://mock.azure.com/",
-            deployment="gpt-4"
-        )
+    completion = AzureTextEmbedding(api_key="xxxxx", endpoint="https://mock.azure.com/", deployment="gpt-4")
 
     assert completion is not None
+
 
 def test_valid_init_env():
     os.environ[AzureTextEmbedding.API_KEY_ENVIRONMENT_VARIABLE] = "xxxxx"
@@ -23,6 +21,7 @@ def test_valid_init_env():
 
     completion = AzureTextEmbedding()
     assert completion is not None
+
 
 def test_invalid_key_raises():
     os.environ[AzureTextEmbedding.API_KEY_ENVIRONMENT_VARIABLE] = ""
@@ -34,6 +33,7 @@ def test_invalid_key_raises():
             api_version="some_version",
         )
 
+
 def test_invalid_endpoint_raises():
     os.environ[AzureTextEmbedding.ENDPOINT_URI_ENVIRONMENT_VARIABLE] = ""
     with pytest.raises(ValueError):
@@ -42,6 +42,7 @@ def test_invalid_endpoint_raises():
             deployment="gpt-4",
             api_version="some_version",
         )
+
 
 def test_invalid_deployment_raises():
     os.environ[AzureTextEmbedding.DEPLOYMENT_ENVIRONMENT_VARIABLE] = ""
