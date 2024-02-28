b874e4bae17614ea3f2d1583db67d9a8004463eb
Richard Lundeen <rlundeen@microsoft.com>
2024-02-27 16:21:53 -0800

    fixing import

--- pyrit/prompt_converter/__init__.py
+++ pyrit/prompt_converter/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from PyRIT.pyrit.prompt_converter.prompt_converter import PromptConverter
-from PyRIT.pyrit.prompt_converter.base64_converter import Base64Converter
-from PyRIT.pyrit.prompt_converter.no_op_converter import NoOpConverter
-from PyRIT.pyrit.prompt_converter.unicode_sub_converter import UnicodeSubstitutionConverter
+from pyrit.prompt_converter.prompt_converter import PromptConverter
+from pyrit.prompt_converter.base64_converter import Base64Converter
+from pyrit.prompt_converter.no_op_converter import NoOpConverter
+from pyrit.prompt_converter.unicode_sub_converter import UnicodeSubstitutionConverter
 
 
 __all__ = ["PromptConverter", "Base64Converter", "NoOpConverter", "UnicodeSubstitutionConverter"]
