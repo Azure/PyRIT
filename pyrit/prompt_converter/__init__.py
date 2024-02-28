d6de7b7683f09581d7e0e969baa62c0d0b7f57b0
rlundeen2 <137218279+rlundeen2@users.noreply.github.com>
2024-02-28 14:10:51 -0800

    Adding StringJoinConverter (#70)
    * Adding strjoinconverter
    
    * build fixes
    
    * pr feedback
    
    * typo

--- pyrit/prompt_converter/__init__.py
+++ pyrit/prompt_converter/__init__.py
@@ -4,7 +4,14 @@
 from pyrit.prompt_converter.prompt_converter import PromptConverter
 from pyrit.prompt_converter.base64_converter import Base64Converter
 from pyrit.prompt_converter.no_op_converter import NoOpConverter
+from pyrit.prompt_converter.string_join_converter import StringJoinConverter
 from pyrit.prompt_converter.unicode_sub_converter import UnicodeSubstitutionConverter
 
 
-__all__ = ["PromptConverter", "Base64Converter", "NoOpConverter", "UnicodeSubstitutionConverter"]
+__all__ = [
+    "PromptConverter",
+    "Base64Converter",
+    "NoOpConverter",
+    "StringJoinConverter",
+    "UnicodeSubstitutionConverter",
+]
