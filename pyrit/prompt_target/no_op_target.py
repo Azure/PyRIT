e889725f68b67f7f82b7cfe9de56fb0d7a0a8917
Richard Lundeen <rlundeen@microsoft.com>
2024-03-01 11:20:24 -0800

    adding no_op target

--- pyrit/prompt_target/no_op_target.py
+++ pyrit/prompt_target/no_op_target.py
@@ -1,13 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from pyrit.chat.azure_openai_chat import AzureOpenAIChat
 from pyrit.memory import FileMemory, MemoryInterface
 from pyrit.models import ChatMessage
 from pyrit.prompt_target import PromptTarget
 
 
-class AzureOpenAIChatTarget(PromptTarget):
+class NoOpTarget(PromptTarget):
+    """
+    The NoOpTarget takes prompts, adds them to memory and prints them, but doesn't send them anywhere
+
+    This can be useful in various situations, for example, if operators want to generate prompts
+    but enter them manually.
+    """
+
     def __init__(self, *, memory: MemoryInterface = None) -> None:
         self.memory = memory if memory else FileMemory()
 
@@ -24,22 +30,11 @@ class AzureOpenAIChatTarget(PromptTarget):
         )
 
     def send_prompt(self, normalized_prompt: str, conversation_id: str, normalizer_id: str) -> str:
-        messages = self.memory.get_chat_messages_with_conversation_id(conversation_id=conversation_id)
-
         msg = ChatMessage(role="user", content=normalized_prompt)
-
-        messages.append(msg)
+        print(msg)
 
         self.memory.add_chat_message_to_memory(
             conversation=msg, conversation_id=conversation_id, normalizer_id=normalizer_id
         )
 
-        resp = super().complete_chat(messages=messages, temperature=self.temperature)
-
-        self.memory.add_chat_message_to_memory(
-            conversation=ChatMessage(role="assistant", content=resp),
-            conversation_id=conversation_id,
-            normalizer_id=normalizer_id,
-        )
-
-        return resp
+        return ""
