91523b8cb8bae68add5000daeb59e1516b1ad1e2
rlundeen2 <137218279+rlundeen2@users.noreply.github.com>
2024-03-01 17:39:21 -0800

    Adding Support for 1:N PromptConverters (#75)
    * 1:n prompts working
    
    * fixing build mypy issues
    
    * adding end to end flow
    
    * Update pyrit/prompt_converter/string_join_converter.py
    
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>
    
    * Update pyrit/prompt_converter/string_join_converter.py
    
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>
    
    * pr feedback
    
    * missed saved file
    
    * doc additions
    
    * refactoring include_original to only include once
    
    * removing old test
    
    * refactoring include_original separation of duties
    
    ---------
    
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

--- pyrit/prompt_converter/string_join_converter.py
+++ pyrit/prompt_converter/string_join_converter.py
@@ -6,10 +6,10 @@ from pyrit.prompt_converter import PromptConverter
 
 class StringJoinConverter(PromptConverter):
 
-    def __init__(self, join_value="-"):
+    def __init__(self, *, join_value="-"):
         self.join_value = join_value
 
-    def convert(self, prompt: str) -> str:
+    def convert(self, prompts: list[str]) -> list[str]:
         """
         Simple converter that uses str join for letters between. E.g. with a `-`
         it converts a prompt of `test` to `t-e-s-t`
@@ -18,8 +18,9 @@ class StringJoinConverter(PromptConverter):
 
         Args:
             prompt (str): The prompt to be converted.
+            include_original (bool): Whether or not to include original prompt in the output
 
         Returns:
-            str: The converted prompt.
+            list[str]: The converted prompts.
         """
-        return self.join_value.join(prompt)
+        return [self.join_value.join(prompt) for prompt in prompts]
