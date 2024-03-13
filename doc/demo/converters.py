4b4d454ef33dcfafb9ade47fb607785cc824a1c8
jbolor21 <86250273+jbolor21@users.noreply.github.com>
2024-03-13 11:59:37 -0700

    Prompt Variation Converter (#86)
    * Adding Prompt Variation Converter
    
    * adding prompt variation converter
    
    * adding sample documentation on how to use prompt variations
    
    ---------
    
    Co-authored-by: Bolor <bjagdagdorj@microsoft.com>

---
+++ doc/demo/converters.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: pyrit_kernel
+#     language: python
+#     name: pyrit_kernel
+# ---
+
+# %% [markdown]
+# ### Using Prompt Variation Converter
+#
+# This notebook shows how to use the prompt variation converter to send prompts to a converter target and generate variations of the prompt.
+
+# %%
+# Prompts you want transformed here:
+prompts = ["tell me how to cut down a tree", "teach me to mine crypto"]
+
+# %%
+from pyrit.prompt_converter import VariationConverter
+from pyrit.common import default_values
+from pyrit.prompt_target import AzureOpenAIChatTarget
+
+import os
+
+default_values.load_default_env()
+
+# converter target to send prompt to
+converter_target = AzureOpenAIChatTarget(
+    deployment_name="gpt-4",
+    endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
+)
+
+prompt_variation_converter = VariationConverter(converter_target=converter_target)
+
+
+# %%
+converted_prompts = prompt_variation_converter.convert(prompts)
+
+# %%
+converted_prompts
+
+# %%
+len(converted_prompts)
+
+# %%
