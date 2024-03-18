06c3e8f20ed3a3b82451f5d0ddc118d179bb0669
gary <dlmgary@outlook.com>
2024-03-18 15:21:53 -0400

    refactor: remove magic line in Jupyter notebook that is breaking pre-commit hooks

--- doc/code/orchestrator.py
+++ doc/code/orchestrator.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# 
+#
 # The Orchestrator is a top level component that red team operators will interact with most. It is responsible for telling PyRIT what endpoints to connect to and how to send prompts.
-# 
+#
 # In general, a strategy for tackling a scenario will be
-# 
+#
 # 1. Making/using a `PromptTarget`
 # 1. Making/using a set of initial prompts
 # 1. Making/using a `PromptConverter` (default is often to not transform)
 # 1. Making/using a `Scorer` (this is often to self ask)
 # 1. Making/using an `Orchestrator`
-# 
+#
 # Orchestrators can tackle complicated scenarios, but this example is about as simple as it gets (while still being useful). Here, we'll send all prompts in a file, use a converter to base64-encode the prompts, and send them to a PromptTarget.
-# 
+#
 # Note to run this demo you need to setup your `.env` file to properly authenticate to an Azure OpenAI endpoint as described [here](../setup/setup_azure.md)
 
 # In[1]:
 
-
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
 
 import os
 
@@ -71,43 +68,40 @@ qa_ds = QuestionAnsweringDataset(
             question="What is the capital of France?",
             answer_type="str",
             correct_answer="Paris",
-            choices = [
+            choices=[
                 QuestionChoice(index=0, text="Paris"),
                 QuestionChoice(index=1, text="London"),
                 QuestionChoice(index=2, text="Berlin"),
-                QuestionChoice(index=3, text="Madrid")
-            ]
+                QuestionChoice(index=3, text="Madrid"),
+            ],
         ),
         QuestionAnsweringEntry(
             question="What is the capital of Germany?",
             answer_type="str",
             correct_answer="Berlin",
-            choices = [
+            choices=[
                 QuestionChoice(index=0, text="Paris"),
                 QuestionChoice(index=1, text="London"),
                 QuestionChoice(index=2, text="Berlin"),
-                QuestionChoice(index=3, text="Madrid")
-            ]
+                QuestionChoice(index=3, text="Madrid"),
+            ],
         ),
         QuestionAnsweringEntry(
             question="Which of the following is a prime number?",
             answer_type="str",
             correct_answer="507961",
-            choices = [
+            choices=[
                 QuestionChoice(index=0, text="507963"),
                 QuestionChoice(index=1, text="507962"),
                 QuestionChoice(index=2, text="507960"),
-                QuestionChoice(index=3, text="507961")
-            ]
+                QuestionChoice(index=3, text="507961"),
+            ],
         ),
-
-    ]
+    ],
 )
 
 benchmark_orchestrator = QuestionAnsweringBenchmarkOrchestrator(
-    chat_model_under_evaluation=gpt_35_target,
-    question_answering_ds=qa_ds,
-    verbose=True
+    chat_model_under_evaluation=gpt_35_target, question_answering_ds=qa_ds, verbose=True
 )
 
 
@@ -117,7 +111,3 @@ print(benchmark_orchestrator.aggregated_results)
 
 
 # In[ ]:
-
-
-
-
