1216f94e231de8e424d95dcee39d828f553889e6
Richard Lundeen <rlundeen@microsoft.com>
2024-03-18 14:20:49 -0700

    staging

--- pyrit/chat/__init__.py
+++ pyrit/chat/__init__.py
@@ -1,8 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from pyrit.chat.aml_online_endpoint_chat import AMLOnlineEndpointChat
 from pyrit.chat.azure_openai_chat import AzureOpenAIChat
 from pyrit.chat.hugging_face_chat import HuggingFaceChat
 
-__all__ = ["AzureOpenAIChat", "AMLOnlineEndpointChat", "HuggingFaceChat"]
+__all__ = ["AzureOpenAIChat", "HuggingFaceChat"]
--- pyrit/prompt_target/__init__.py
+++ pyrit/prompt_target/__init__.py
@@ -3,9 +3,16 @@
 
 from pyrit.prompt_target.prompt_target import PromptTarget
 from pyrit.prompt_target.prompt_chat_target import PromptChatTarget
+
+from pyrit.prompt_target.azure_ml_chat_target import AzureMLChatTarget
 from pyrit.prompt_target.azure_openai_chat_target import AzureOpenAIChatTarget
 from pyrit.prompt_target.gandalf_target import GandalfTarget
 from pyrit.prompt_target.text_target import TextTarget
 
 
-__all__ = ["AzureOpenAIChatTarget", "GandalfTarget", "PromptChatTarget", "PromptTarget", "TextTarget"]
+__all__ = ["AzureMLChatTarget",
+           "AzureOpenAIChatTarget",
+           "GandalfTarget",
+           "PromptChatTarget",
+           "PromptTarget",
+           "TextTarget"]
