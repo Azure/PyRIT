949517e792f6048e869a07ace4b47abd9e017984
Nina C <nichikan@microsoft.com>
2024-03-19 16:33:44 -0700

    add docstrings + roman pr feedback

--- pyrit/prompt_target/azure_blob_storage_target.py
+++ pyrit/prompt_target/azure_blob_storage_target.py
@@ -4,6 +4,7 @@
 from azure.core.exceptions import ClientAuthenticationError
 from azure.storage.blob.aio import ContainerClient as AsyncContainerClient
 from azure.storage.blob import ContainerClient, ContentSettings
+from enum import Enum
 import logging
 
 from pyrit.common import default_values
@@ -14,15 +15,26 @@ from pyrit.prompt_target import PromptTarget
 logger = logging.getLogger(__name__)
 
 
+class SupportedContentType(Enum):
+    """
+    All supported content types for uploading blobs to provided storage account container.
+    See all options here: https://www.iana.org/assignments/media-types/media-types.xhtml
+    """
+
+    PLAIN_TEXT = "text/plain"
+
+
 class AzureBlobStorageTarget(PromptTarget):
     """
     The AzureBlobStorageTarget takes prompts, saves the prompts to a file, and stores them as a blob in a provided
     storage account container.
 
     Args:
-        container_url: URL to the Azure Blob Storage Container.
-        sas_token: Blob SAS token required to authenticate blob operations.
-        memory: MemoryInterface to use for the class. FileMemory by default.
+        container_url (str): URL to the Azure Blob Storage Container.
+        sas_token (str): Blob SAS token required to authenticate blob operations.
+        blob_content_type (SupportedContentType): Expected Content Type of the blob, chosen from the
+            SupportedContentType enum. Set to PLAIN_TEXT by default.
+        memory (str): MemoryInterface to use for the class. FileMemory by default.
     """
 
     AZURE_STORAGE_CONTAINER_ENVIRONMENT_VARIABLE: str = "AZURE_STORAGE_ACCOUNT_CONTAINER_URL"
@@ -33,12 +45,12 @@ class AzureBlobStorageTarget(PromptTarget):
         *,
         container_url: str | None = None,
         sas_token: str | None = None,
-        blob_content_type: str = "text/plain",
+        blob_content_type: SupportedContentType = SupportedContentType.PLAIN_TEXT,
         memory: MemoryInterface | None = None,
     ) -> None:
         default_values.load_default_env()
 
-        self._blob_content_type = blob_content_type
+        self._blob_content_type: str = blob_content_type.value
 
         self._container_url: str = default_values.get_required_value(
             env_var_name=self.AZURE_STORAGE_CONTAINER_ENVIRONMENT_VARIABLE, passed_value=container_url
@@ -61,6 +73,15 @@ class AzureBlobStorageTarget(PromptTarget):
         super().__init__(memory=memory)
 
     def _upload_blob_exception_handling(self, exc: Exception) -> None:
+        """
+        Handles exceptions for uploading blob to storage container.
+
+        Raises:
+            ClientAuthenticationError: If authentication fails, either from an invalid SAS token or from
+                an invalid container URL.
+            Exception: If anything except ClientAuthenticationError is caught when uploading a blob.
+        """
+
         if type(exc) is ClientAuthenticationError:
             logger.exception(
                 msg="Authentication failed. Verify the container's existence in the Azure Storage Account and "
@@ -76,9 +97,9 @@ class AzureBlobStorageTarget(PromptTarget):
         Handles uploading blob to given storage container.
 
         Args:
-            file_name: File name to assign to uploaded blob.
-            data: Byte representation of content to upload to container.
-            content_type: Content type to upload.
+            file_name (str): File name to assign to uploaded blob.
+            data (bytes): Byte representation of content to upload to container.
+            content_type (str): Content type to upload.
         """
 
         content_settings = ContentSettings(content_type=f"{content_type}")
@@ -101,6 +122,18 @@ class AzureBlobStorageTarget(PromptTarget):
         conversation_id: str,
         normalizer_id: str,
     ) -> str:
+        """
+        Sends prompt to target, which creates a file and uploads it as a blob
+        to the provided storage container.
+
+        Args:
+            normalized_prompt (str): A normalized prompt to be sent to the prompt target.
+            conversation_id (str): The ID of the conversation.
+            normalizer_id (str): ID provided by the prompt normalizer.
+
+        Returns:
+            blob_url (str): The Blob URL of the created blob within the provided storage container.
+        """
         file_name = f"{conversation_id}.txt"
         data = str.encode(normalized_prompt)
         blob_url = self._container_url + "/" + file_name
@@ -120,9 +153,9 @@ class AzureBlobStorageTarget(PromptTarget):
         (Async) Handles uploading blob to given storage container.
 
         Args:
-            file_name: File name to assign to uploaded blob.
-            data: Byte representation of content to upload to container.
-            content_type: Content type to upload.
+            file_name (str): File name to assign to uploaded blob.
+            data (bytes): Byte representation of content to upload to container.
+            content_type (str): Content type to upload.
         """
 
         content_settings = ContentSettings(content_type=f"{content_type}")
@@ -145,6 +178,19 @@ class AzureBlobStorageTarget(PromptTarget):
         conversation_id: str,
         normalizer_id: str,
     ) -> str:
+        """
+        (Async) Sends prompt to target, which creates a file and uploads it as a blob
+        to the provided storage container.
+
+        Args:
+            normalized_prompt (str): A normalized prompt to be sent to the prompt target.
+            conversation_id (str): The ID of the conversation.
+            normalizer_id (str): ID provided by the prompt normalizer.
+
+        Returns:
+            blob_url (str): The Blob URL of the created blob within the provided storage container.
+        """
+
         file_name = f"{conversation_id}.txt"
         data = str.encode(normalized_prompt)
         blob_url = self._container_url + "/" + file_name
