98323e76a7d6ec4533fd8624433c885ab2ca8ffb
Richard Lundeen <rlundeen@microsoft.com>
2024-03-20 08:45:38 -0700

    pr feedback

--- pyrit/prompt_target/azure_openai_chat_target.py
+++ pyrit/prompt_target/azure_openai_chat_target.py
@@ -138,8 +138,10 @@ class AzureOpenAIChatTarget(ChatSupport, PromptChatTarget):
     def parse_chat_completion(self, response):
         """
         Parses chat message to get response
+
         Args:
             response (ChatMessage): The chat messages object containing the generated response message
+
         Returns:
             str: The generated response message
         """
@@ -153,39 +155,42 @@ class AzureOpenAIChatTarget(ChatSupport, PromptChatTarget):
         return response_message
 
     async def complete_chat_async(
-        self,
-        messages: list[ChatMessage],
-        max_tokens: int = 1024,
-        temperature: float = 1.0,
-        top_p: int = 1,
-        frequency_penalty: float = 0.5,
-        presence_penalty: float = 0.5,
-    ) -> str:
-        """
-        Completes asynchronous chat request
-        Parses chat message to get response
-        Args:
-            message (list[ChatMessage]): The chat message objects containing the role and content.
-            max_tokens (int, optional): The maximum number of tokens to generate. Defaults to 1024.
-            temperature (float, optional): Controls randomness in the response generation. Defaults to 1.0.
-            top_p (int, optional): Controls diversity of the response generation. Defaults to 1.
-            frequency_penalty (float, optional): Controls frequency of generating same lines of text. Defaults to 0.5.
-            presence_penalty (float, optional):  Controls likelihood to talk about new topics. Defaults to 0.5.
-        Returns:
-            str: The generated response message
-        """
-        response: ChatCompletion = await self._async_client.chat.completions.create(
-            model=self._deployment_name,
-            max_tokens=max_tokens,
-            temperature=temperature,
-            top_p=top_p,
-            frequency_penalty=frequency_penalty,
-            presence_penalty=presence_penalty,
-            n=1,
-            stream=False,
-            messages=[{"role": msg.role, "content": msg.content} for msg in messages],  # type: ignore
-        )
-        return self.parse_chat_completion(response)
+            self,
+            messages: list[ChatMessage],
+            max_tokens: int = 1024,
+            temperature: float = 1.0,
+            top_p: int = 1,
+            frequency_penalty: float = 0.5,
+            presence_penalty: float = 0.5,
+        ) -> str:
+            """
+            Completes asynchronous chat request.
+
+            This method sends a chat message to the OpenAI chat model and retrieves the generated response.
+
+            Args:
+                messages (list[ChatMessage]): The chat message objects containing the role and content.
+                max_tokens (int, optional): The maximum number of tokens to generate. Defaults to 1024.
+                temperature (float, optional): Controls randomness in the response generation. Defaults to 1.0.
+                top_p (int, optional): Controls diversity of the response generation. Defaults to 1.
+                frequency_penalty (float, optional): Controls the frequency of generating the same lines of text. Defaults to 0.5.
+                presence_penalty (float, optional): Controls the likelihood to talk about new topics. Defaults to 0.5.
+
+            Returns:
+                str: The generated response message.
+            """
+            response: ChatCompletion = await self._async_client.chat.completions.create(
+                model=self._deployment_name,
+                max_tokens=max_tokens,
+                temperature=temperature,
+                top_p=top_p,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty,
+                n=1,
+                stream=False,
+                messages=[{"role": msg.role, "content": msg.content} for msg in messages],  # type: ignore
+            )
+            return self.parse_chat_completion(response)
 
     def complete_chat(
         self,
@@ -198,13 +203,15 @@ class AzureOpenAIChatTarget(ChatSupport, PromptChatTarget):
     ) -> str:
         """
         Parses chat message to get response
+
         Args:
-            message (list[ChatMessage]): The chat message objects containing the role and content.
+            messages (list[ChatMessage]): The chat message objects containing the role and content.
             max_tokens (int, optional): The maximum number of tokens to generate. Defaults to 1024.
             temperature (float, optional): Controls randomness in the response generation. Defaults to 1.0.
             top_p (int, optional): Controls diversity of the response generation. Defaults to 1.
             frequency_penalty (float, optional): Controls frequency of generating same lines of text. Defaults to 0.5.
-            presence_penalty (float, optional):  Controls likelihood to talk about new topics. Defaults to 0.5.
+            presence_penalty (float, optional): Controls likelihood to talk about new topics. Defaults to 0.5.
+
         Returns:
             str: The generated response message
         """
