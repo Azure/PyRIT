d93906aecd0132292ac870f24d48bc32d1082e9b
Daniel Fernández <dfmartinez@gmail.com>
2024-03-21 17:34:05 +0100

    Add support to OpenAI API to use official or custom endpoints (#65)
    * Add support to OpenAI API to use official or custom endpoints
    
    Signed-off-by: Daniel Fernández <daniel@dfer.io>
    
    * Added testcases for OpenAI API support
    
    Signed-off-by: Daniel Fernández <daniel@dfer.io>
    
    * Fixes typo in "pyrit/chat/openai_chat.py".
    
    Signed-off-by: Daniel Fernández <daniel@dfer.io>
    
    * Refactored common code of OpenAIChat and AzureOpenAIChat into class OpenAIBase
    
    Signed-off-by: Daniel Fernández <daniel@dfer.io>
    
    ---------
    
    Signed-off-by: Daniel Fernández <daniel@dfer.io>
    Co-authored-by: Richard Lundeen <rlundeen@microsoft.com>

--- pyrit/chat/__init__.py
+++ pyrit/chat/__init__.py
@@ -3,5 +3,6 @@
 
 from pyrit.chat.aml_online_endpoint_chat import AMLOnlineEndpointChat
 from pyrit.chat.hugging_face_chat import HuggingFaceChat
+from pyrit.chat.openai_chat import OpenAIChat
 
-__all__ = ["AMLOnlineEndpointChat", "HuggingFaceChat"]
+__all__ = ["AzureOpenAIChat", "AMLOnlineEndpointChat", "HuggingFaceChat", "OpenAIChat"]
