7fe3a1cee246fd7c7e513750d9a117dd863b6d6a
Richard Lundeen <rlundeen@microsoft.com>
2024-03-28 11:27:59 -0700

    Removing deployment from jupytext runs

--- doc/run_jupytext.ps1
+++ doc/run_jupytext.ps1
@@ -1,5 +1,6 @@
 $currDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-$files = Get-ChildItem -Path $currDir -Recurse -Include *.py -File
+$files = Get-ChildItem -Path $currDir -Recurse -Include *.py -File|
+Where-Object { -not $_.FullName.ToLower().Contains("\deployment\") }
 
 foreach ($file in $files) {
     write-host "Processing $file"
--- doc/run_jupytext.sh
+++ doc/run_jupytext.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curr_dir="$(dirname "$(realpath "$0")")"
-find "$curr_dir" -type f -name "*.py" -print0 | while IFS= read -r -d '' file
+find "$curr_dir" \( -type d -name "deployment" -prune \) -o \( -type f -name "*.py" -print0 \) | while IFS= read -r -d '' file
 do
     echo "Processing $file"
     jupytext --execute --to notebook "$file"
