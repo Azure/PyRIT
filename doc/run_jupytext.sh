ec56d1d04470cfed20f428ac8b0fa3df096e1e4d
rlundeen2 <137218279+rlundeen2@users.noreply.github.com>
2024-03-29 09:58:26 -0700

    FIX: Removing Deployment directory from jupytext runs (#126)
    * Removing deployment from jupytext runs
    
    * adding a comment

--- doc/run_jupytext.sh
+++ doc/run_jupytext.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Find all .py files in the same directory as this script and convert them to .ipynb
+# This excludes the deployment directory
+
 curr_dir="$(dirname "$(realpath "$0")")"
-find "$curr_dir" -type f -name "*.py" -print0 | while IFS= read -r -d '' file
+find "$curr_dir" \( -type d -name "deployment" -prune \) -o \( -type f -name "*.py" -print0 \) | while IFS= read -r -d '' file
 do
     echo "Processing $file"
     jupytext --execute --to notebook "$file"
