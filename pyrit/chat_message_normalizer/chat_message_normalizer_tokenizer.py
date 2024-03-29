4b35fd0376df7ded9aa97337c4af22f1e39101f7
Blake Bullwinkel <blakebullwinkel@gmail.com>
2024-03-29 09:14:33 -0700

    added docstrings to ChatMessageNormalizerTokenizerTemplate class

--- pyrit/chat_message_normalizer/chat_message_normalizer_tokenizer.py
+++ pyrit/chat_message_normalizer/chat_message_normalizer_tokenizer.py
@@ -7,15 +7,30 @@ from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 
 
 class ChatMessageNormalizerTokenizerTemplate(ChatMessageNormalizer[str]):
+    """
+    This class enables you to apply the chat template stored in a Hugging Face tokenizer
+    to a list of chat messages. For more details, see
+    https://huggingface.co/docs/transformers/main/en/chat_templating
+    """
 
     def __init__(self, tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast):
+        """
+        Initializes an instance of the ChatMessageNormalizerTokenizerTemplate class.
+
+        Args:
+            tokenizer (PreTrainedTokenizer | PreTrainedTokenizerFast): A Hugging Face tokenizer.
+        """
         self.tokenizer = tokenizer
 
     def normalize(self, messages: list[ChatMessage]) -> str:
-        """Convert a string of text to the format specified by the chat
-        template loaded in a custom Hugging Face tokenizer.
-        This method utilizes the `apply_chat_template` method documented at
-        https://huggingface.co/docs/transformers/main/en/chat_templating
+        """
+        Applies the chat template stored in the tokenizer to a list of chat messages.
+
+        Args:
+            messages (list[ChatMessage]): A list of ChatMessage objects.
+
+        Returns:
+            str: The formatted chat messages.
         """
 
         messages_list = []
