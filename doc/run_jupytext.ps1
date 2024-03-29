2990bf61409a25069f8601a5702fcaba2d06e07a
Richard Lundeen <rlundeen@microsoft.com>
2024-03-29 09:03:51 -0700

    adding a comment

--- doc/run_jupytext.ps1
+++ doc/run_jupytext.ps1
@@ -1,3 +1,6 @@
+# Find all .py files in the same directory as this script and convert them to .ipynb
+# This excludes the deployment directory
+
 $currDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $files = Get-ChildItem -Path $currDir -Recurse -Include *.py -File|
 Where-Object { -not $_.FullName.ToLower().Contains("\deployment\") }
