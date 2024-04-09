199c9f11ef1268764a303cb40bbc1b2b2f37a04f
Peter Greko <petergreko@microsoft.com>
2024-04-09 11:05:05 -0700

    this time with pre-commit hooks done

---
+++ pyrit/prompt_converter/capital_letters_converter.py
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from pyrit.memory.memory_models import PromptDataType
+from pyrit.prompt_converter import PromptConverter
+
+
+class CapitalLetterConverter(PromptConverter):
+    def convert(self, *, prompt: str, input_type: PromptDataType = "text") -> str:
+        """
+        Simple converter that converts the prompt to capital letters.
+        """
+        if not self.is_supported(input_type):
+            raise ValueError("Input type not supported")
+
+        output = prompt.upper()
+        return output
+
+    def is_supported(self, input_type: PromptDataType) -> bool:
+        return input_type == "text"
