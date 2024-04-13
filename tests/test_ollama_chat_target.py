6d88a5a6f8f14eecf2fa1301571a2f7b5e0d5f4d
Adriano Maia <adriano@drstrange.wtf>
2024-04-13 14:41:13 -0500

    - Fixed async test for OllamaChatTarget

--- tests/test_ollama_chat_target.py
+++ tests/test_ollama_chat_target.py
@@ -24,13 +24,13 @@ def ollama_chat_engine() -> OllamaChatTarget:
     )
 
 @pytest.mark.asyncio
-async def test_ollama_complete_chat_async_return(ollama_mock_return: dict, ollama_chat_engine: OllamaChatTarget):
+async def test_ollama_complete_chat_async_return(ollama_chat_engine: OllamaChatTarget):
     with patch("pyrit.common.net_utility.make_request_and_raise_if_error_async", new_callable=AsyncMock) as mock_create:
-        mock_create.return_value = ollama_mock_return
+        mock_create.return_value = httpx.Response(200, json={"model":"mistral","created_at":"2024-04-13T16:14:52.69602Z","message":{"role":"assistant","content":" Hello."},"done":True,"total_duration":254579625,"load_duration":276542,"prompt_eval_count":20,"prompt_eval_duration":222911000,"eval_count":3,"eval_duration":30879000})
         ret = await ollama_chat_engine._complete_chat_async(messages=[ChatMessage(role="user", content="hello")])
         assert ret == " Hello."
 
-def test_ollama_complete_chat_return(ollama_mock_return: dict, ollama_chat_engine: OllamaChatTarget):
+def test_ollama_complete_chat_return(ollama_chat_engine: OllamaChatTarget):
     with patch("pyrit.common.net_utility.make_request_and_raise_if_error") as mock_create:
         mock_create.return_value = httpx.Response(200, json={"model":"mistral","created_at":"2024-04-13T16:14:52.69602Z","message":{"role":"assistant","content":" Hello."},"done":True,"total_duration":254579625,"load_duration":276542,"prompt_eval_count":20,"prompt_eval_duration":222911000,"eval_count":3,"eval_duration":30879000})
         ret = ollama_chat_engine._complete_chat(messages=[ChatMessage(role="user", content="hello")])
