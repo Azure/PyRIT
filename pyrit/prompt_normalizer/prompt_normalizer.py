adf0d969f23332d84c4d15e85e8f9947ec4d19db
Richard Lundeen <rlundeen@microsoft.com>
2024-04-15 14:36:59 -0700

    documenting

--- pyrit/prompt_normalizer/prompt_normalizer.py
+++ pyrit/prompt_normalizer/prompt_normalizer.py
@@ -21,90 +21,127 @@ class PromptNormalizer(abc.ABC):
         self.id = str(uuid4())
 
     def send_prompt(
-        self,
-        normalizer_request: NormalizerRequest,
-        target: PromptTarget,
-        conversation_id: str = None,
-        sequence: int = -1,
-        labels={},
-        orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
-    ) -> PromptRequestResponse:
-        """
-        Sends a single request to a target
-        """
+            self,
+            normalizer_request: NormalizerRequest,
+            target: PromptTarget,
+            conversation_id: str = None,
+            sequence: int = -1,
+            labels={},
+            orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
+        ) -> PromptRequestResponse:
+            """
+            Sends a single request to a target.
 
-        request_response = self._get_prompt_request_response(
-            request=normalizer_request,
-            target=target,
-            conversation_id=conversation_id,
-            sequence=sequence,
-            labels=labels,
-            orchestrator=orchestrator,
-        )
-        try:
-            # Use the synchronous prompt sending method by default.
+            Args:
+                normalizer_request (NormalizerRequest): The request to be sent.
+                target (PromptTarget): The target to send the request to.
+                conversation_id (str, optional): The ID of the conversation. Defaults to None.
+                sequence (int, optional): The sequence number of the request. Defaults to -1.
+                labels (dict, optional): Additional labels for the request. Defaults to {}.
+                orchestrator (Orchestrator, optional): The orchestrator to use. Defaults to None.
 
-            return target.send_prompt(prompt_request=request_response)
-        except NotImplementedError:
-            # Alternatively, use async if sync is unavailable.
-            pool = concurrent.futures.ThreadPoolExecutor()
-            return pool.submit(asyncio.run, target.send_prompt_async(prompt_request=request_response)).result()
+            Returns:
+                PromptRequestResponse: The response received from the target.
+            """
+
+            request_response = self._build_prompt_request_response(
+                request=normalizer_request,
+                target=target,
+                conversation_id=conversation_id,
+                sequence=sequence,
+                labels=labels,
+                orchestrator=orchestrator,
+            )
+            try:
+                # Use the synchronous prompt sending method by default.
+                return target.send_prompt(prompt_request=request_response)
+            except NotImplementedError:
+                # Alternatively, use async if sync is unavailable.
+                pool = concurrent.futures.ThreadPoolExecutor()
+                return pool.submit(asyncio.run, target.send_prompt_async(prompt_request=request_response)).result()
 
     async def send_prompt_async(
-        self,
-        normalizer_request: NormalizerRequest,
-        target: PromptTarget,
-        conversation_id: str = None,
-        sequence: int = -1,
-        labels={},
-        orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
-    ) -> PromptRequestResponse:
-        """
-        Sends a single request to a target
-        """
+            self,
+            normalizer_request: NormalizerRequest,
+            target: PromptTarget,
+            conversation_id: str = None,
+            sequence: int = -1,
+            labels={},
+            orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
+        ) -> PromptRequestResponse:
+            """
+            Sends a single request to a target.
 
-        request = self._get_prompt_request_response(
-            request=normalizer_request,
-            target=target,
-            conversation_id=conversation_id,
-            sequence=sequence,
-            labels=labels,
-            orchestrator=orchestrator,
-        )
+            Args:
+                normalizer_request (NormalizerRequest): The request to be sent.
+                target (PromptTarget): The target to send the request to.
+                conversation_id (str, optional): The ID of the conversation. Defaults to None.
+                sequence (int, optional): The sequence number. Defaults to -1.
+                labels (dict, optional): Additional labels for the request. Defaults to {}.
+                orchestrator (Orchestrator, optional): The orchestrator. Defaults to None.
 
-        response = await target.send_prompt_async(prompt_request=request)
-        return response
+            Returns:
+                PromptRequestResponse: The response received from the target.
+            """
+            request = self._build_prompt_request_response(
+                request=normalizer_request,
+                target=target,
+                conversation_id=conversation_id,
+                sequence=sequence,
+                labels=labels,
+                orchestrator=orchestrator,
+            )
+
+            response = await target.send_prompt_async(prompt_request=request)
+            return response
 
     async def send_prompt_batch_to_target_async(
-        self,
-        requests: list[NormalizerRequest],
-        target: PromptTarget,
-        labels={},
-        orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
-        batch_size: int = 10,
-    ) -> list[PromptRequestResponse]:
+            self,
+            requests: list[NormalizerRequest],
+            target: PromptTarget,
+            labels={},
+            orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
+            batch_size: int = 10,
+        ) -> list[PromptRequestResponse]:
+            """
+            Sends a batch of prompts to the target asynchronously.
 
-        results = []
+            Args:
+                requests (list[NormalizerRequest]): A list of NormalizerRequest objects representing the prompts to
+                    be sent.
+                target (PromptTarget): The target to which the prompts should be sent.
+                labels (dict, optional): Additional labels to be included with the prompts. Defaults to an empty
+                    dictionary.
+                orchestrator (Orchestrator, optional): The orchestrator to use for sending the prompts. Defaults
+                    to None.
+                batch_size (int, optional): The size of each batch of prompts. Defaults to 10.
 
-        for prompts_batch in self._chunked_prompts(requests, batch_size):
-            tasks = []
-            for prompt in prompts_batch:
-                tasks.append(
-                    self.send_prompt_async(
-                        normalizer_request=prompt, target=target, labels=labels, orchestrator=orchestrator
+            Returns:
+                list[PromptRequestResponse]: A list of PromptRequestResponse objects representing the responses
+                    received for each prompt.
+            """
+
+            results = []
+
+            for prompts_batch in self._chunked_prompts(requests, batch_size):
+                tasks = []
+                for prompt in prompts_batch:
+                    tasks.append(
+                        self.send_prompt_async(
+                            normalizer_request=prompt, target=target, labels=labels, orchestrator=orchestrator
+                        )
                     )
-                )
 
-            batch_results = await asyncio.gather(*tasks)
-            results.extend(batch_results)
+                batch_results = await asyncio.gather(*tasks)
+                results.extend(batch_results)
 
-        return results
+            return results
 
     def _chunked_prompts(self, prompts, size):
         for i in range(0, len(prompts), size):
             yield prompts[i : i + size]
 
-    def _get_prompt_request_response(
+    def _build_prompt_request_response(
         self,
         request: NormalizerRequest,
         target: PromptTarget,
@@ -113,6 +150,22 @@ class PromptNormalizer(abc.ABC):
         labels={},
         orchestrator: "Orchestrator" = None,  # type: ignore # noqa: F821
     ) -> PromptRequestResponse:
+        """
+        Builds a prompt request response based on the given parameters.
+
+        Applies parameters and converters to the prompt text and puts all the pieces together.
+
+        Args:
+            request (NormalizerRequest): The normalizer request object.
+            target (PromptTarget): The prompt target object.
+            conversation_id (str, optional): The conversation ID. Defaults to None.
+            sequence (int, optional): The sequence number. Defaults to -1.
+            labels (dict, optional): The labels dictionary. Defaults to {}.
+            orchestrator (Orchestrator, optional): The orchestrator object. Defaults to None.
+
+        Returns:
+            PromptRequestResponse: The prompt request response object.
+        """
 
         entries = []
 
