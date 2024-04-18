720f405a5c585c76c9eca65a369d2849bed2568d
Peter Greko <petergreko@microsoft.com>
2024-04-18 16:25:28 -0700

    FEAT: text to audio converter

--- pyrit/prompt_converter/text_to_audio_converter.py
+++ pyrit/prompt_converter/text_to_audio_converter.py
@@ -2,6 +2,8 @@
 # Licensed under the MIT license.
 import logging
 import pathlib
+import os
+import uuid
 
 import azure.cognitiveservices.speech as speechsdk
 from pyrit.memory.memory_models import PromptDataType
@@ -25,81 +27,79 @@ class TextToAudioConverter(PromptConverter):
         https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support
         filename (str): File name to be generated.
     """
-    SPEECH_REGION_ENVIRONMENT_VARIABLE: str = "SPEECH_REGION"
-    SPEECH_KEY_TOKEN_ENVIRONMENT_VARIABLE: str = "SPEECH_KEY_TOKEN"
 
-    def __init__(self, 
-            filename: str = "hi.wav",
-            speech_region: str = None,
-            speech_key: str = None,
-            synthesis_language: str = None,
-            synthesis_voice_name: str = None,
-        ) -> None:
-        
+    AZURE_SPEECH_REGION_ENVIRONMENT_VARIABLE: str = "AZURE_SPEECH_REGION"
+    AZURE_SPEECH_KEY_TOKEN_ENVIRONMENT_VARIABLE: str = "AZURE_SPEECH_KEY_TOKEN"
+    SUPPORTED_OUTPUT_FORMATS = ["wav", "mp3"]
+
+    def __init__(
+        self,
+        filename: str = None,
+        azure_speech_region: str = None,
+        azure_speech_key: str = None,
+        synthesis_language: str = "en_US",
+        synthesis_voice_name: str = "en-US-AvaNeural",
+        output_format: str = "wav",
+    ) -> None:
+
         self.filename = filename
-
-        if speech_region is None:
-            self.speech_region: str = default_values.get_required_value(
-                env_var_name=self.SPEECH_REGION_ENVIRONMENT_VARIABLE, passed_value=speech_region
+        if output_format not in self.SUPPORTED_OUTPUT_FORMATS:
+            raise ValueError(
+                f"Invalid output format {output_format}. Supported output formats are {self.SUPPORTED_OUTPUT_FORMATS}"
             )
-        else:
-            self.speech_region = speech_region
 
-        if speech_key is None:
-            self.speech_key: str = default_values.get_required_value(
-                env_var_name=self.SPEECH_KEY_TOKEN_ENVIRONMENT_VARIABLE, passed_value=speech_key
-            )
-        else:
-            self.speech_key = speech_key
+        self.azure_speech_region: str = default_values.get_required_value(
+            env_var_name=self.AZURE_SPEECH_REGION_ENVIRONMENT_VARIABLE, passed_value=azure_speech_region
+        )
 
-        if synthesis_language is None:
-            self.synthesis_language = "en_US"
-        else:
-            self.synthesis_language = synthesis_language
+        self.azure_speech_key: str = default_values.get_required_value(
+            env_var_name=self.AZURE_SPEECH_KEY_TOKEN_ENVIRONMENT_VARIABLE, passed_value=azure_speech_key
+        )
 
-        if synthesis_voice_name is None:
-            self.synthesis_voice_name = "en-US-AvaNeural"
-        else:
-            self.synthesis_voice_name = synthesis_voice_name
+        self.synthesis_language = synthesis_language
+
+        self.synthesis_voice_name = synthesis_voice_name
 
         self.output_dir = pathlib.Path(RESULTS_PATH) / "audio"
-        
-        if self.has_wav_extension(self.filename):
-            self.filename = filename
-        else:
-            logger.error("File name for wav file does not contain .wav")
-            raise
+
+        self.output_format = output_format
 
     def is_supported(self, input_type: PromptDataType) -> bool:
         return input_type == "text"
 
-    def has_wav_extension(self, file_name):
-        return file_name.lower().endswith(".wav") 
-
-    def send_prompt_to_audio(self, prompt):
-        if prompt is None:
-            logger.error("Prompt was empty")
-            raise
+    def send_prompt_to_audio_file(self, prompt: str, output_format: str):
+        if prompt == "":
+            raise ValueError("Prompt was empty. Please provide valid input prompt.")
         try:
-            speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.speech_region)
+            speech_config = speechsdk.SpeechConfig(subscription=self.azure_speech_key, region=self.azure_speech_region)
             speech_config.speech_synthesis_language = self.synthesis_language
             speech_config.speech_synthesis_voice_name = self.synthesis_voice_name
-            file_name = str(self.filename)
+            if output_format == "mp3":
+                speech_config.set_speech_synthesis_output_format(
+                    speechsdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3
+                )
+            if not self.filename:
+                self.filename = f"{uuid.uuid4()}.wav"
+            if not os.path.isdir(self.output_dir):
+                os.mkdir(self.output_dir)
+            file_name = os.path.join(self.output_dir, self.filename)
             file_config = speechsdk.audio.AudioOutputConfig(filename=file_name)
             speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=file_config)
-            speech_synthesizer
             result = speech_synthesizer.speak_text_async(prompt).get()
+            if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+                logger.info(
+                    "Speech synthesized for text [{}], and the audio was saved to [{}]".format(prompt, file_name)
+                )
+            elif result.reason == speechsdk.ResultReason.Canceled:
+                cancellation_details = result.cancellation_details
+                logger.info("Speech synthesis canceled: {}".format(cancellation_details.reason))
+                if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                    logger.error("Error details: {}".format(cancellation_details.error_details))
         except Exception as e:
-           logger.error(e)
-           raise
+            logger.error("Failed to convert prompt to audio: %s", str(e))
+            raise
 
-    def convert(self, *, prompt: str, input_type: PromptDataType = "text") -> str:
-        """
-        Simple converter that converts the prompt to capital letters via a percentage .
-        """
+    def convert(self, *, prompt: str, input_type: PromptDataType = "text") -> None:
         if not self.is_supported(input_type):
             raise ValueError("Input type not supported")
-
-        self.send_prompt_to_audio(prompt)
-        
-        return ("The following prompt: \"",prompt,"\" was converted into the audio file ",self.filename)
+        self.send_prompt_to_audio_file(prompt, self.output_format)
