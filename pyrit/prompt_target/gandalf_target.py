075de04c1f0fb77112c3f2bdc10ca750bb9aa855
Richard Lundeen <rlundeen@microsoft.com>
2024-04-19 12:13:54 -0700

    adding async wrapper

--- pyrit/prompt_target/gandalf_target.py
+++ pyrit/prompt_target/gandalf_target.py
@@ -1,9 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import asyncio
+import concurrent.futures
 import logging
+from multiprocessing import pool
 
-from pyrit.completion import GandalfCompletionEngine, GandalfLevel
+from pyrit.completion import GandalfLevel
+from pyrit.common import net_utility
 from pyrit.memory import DuckDBMemory, MemoryInterface
 from pyrit.models import PromptRequestResponse
 from pyrit.prompt_target import PromptTarget
@@ -12,18 +16,27 @@ from pyrit.prompt_target import PromptTarget
 logger = logging.getLogger(__name__)
 
 
-class GandalfTarget(GandalfCompletionEngine, PromptTarget):
+class GandalfTarget(PromptTarget):
     def __init__(
         self,
         *,
         level: GandalfLevel,
         memory: MemoryInterface = None,
     ) -> None:
-        super().__init__(level=level)
         self._memory = memory if memory else DuckDBMemory()
 
-    def send_prompt(self, *, prompt_request: PromptRequestResponse) -> PromptRequestResponse:
+        self._endpoint = "https://gandalf.lakera.ai/api/send-message"
+        self._defender = level.value
 
+    def send_prompt(self, *, prompt_request: PromptRequestResponse) -> PromptRequestResponse:
+        """
+        Deprecated. Use send_prompt_async instead.
+        """
+        pool = concurrent.futures.ThreadPoolExecutor()
+        return pool.submit(asyncio.run, self.send_prompt_async(prompt_request=prompt_request)).result()
+
+
+    async def send_prompt_async(self, *, prompt_request: PromptRequestResponse) -> PromptRequestResponse:
         request = prompt_request.request_pieces[0]
 
         messages = self._memory.get_chat_messages_with_conversation_id(conversation_id=request.conversation_id)
@@ -33,18 +46,26 @@ class GandalfTarget(GandalfCompletionEngine, PromptTarget):
 
         logger.info(f"Sending the following prompt to the prompt target: {request}")
 
-        response = super().complete_text(text=request.converted_prompt_text)
-
-        if not response.completion:
-            raise ValueError("The chat returned an empty response.")
-
-        logger.info(f'Received the following response from the prompt target "{response.completion}"')
+        response = await self._complete_text_async(request.converted_prompt_text)
 
         response_entry = self._memory.add_response_entries_to_memory(
-            request=request, response_text_pieces=[response.completion]
+            request=request, response_text_pieces=[response]
         )
 
         return response_entry
 
-    async def send_prompt_async(self, *, prompt_request: PromptRequestResponse) -> PromptRequestResponse:
-        raise NotImplementedError()
+    async def _complete_text_async(self, text: str) -> str:
+        payload = {
+            "defender": self._defender,
+            "prompt": text,
+        }
+
+        resp = await net_utility.make_request_and_raise_if_error_async(
+            endpoint_uri=self._endpoint, method="POST", request_body=payload, post_type="data"
+        )
+
+        if not resp.text:
+            raise ValueError("The chat returned an empty response.")
+
+        logger.info(f'Received the following response from the prompt target "{resp.text}"')
+        return resp.text
