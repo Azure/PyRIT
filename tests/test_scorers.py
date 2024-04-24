b5e93794ab1cde3da1f282e6d3addaa081d294d0
Blake Bullwinkel <blakebullwinkel@gmail.com>
2024-04-24 16:08:59 -0700

    ran pre-commit hooks

--- tests/test_scorers.py
+++ tests/test_scorers.py
@@ -18,13 +18,12 @@ def test_scorer_set_system_prompt(chat_target: AzureOpenAIChatTarget):
     with patch.object(AzureOpenAIChatTarget, "set_system_prompt") as mock_method:
 
         scorer = SelfAskGptClassifier(
-            content_classifier=ContentClassifiers.PROMPT_INJECTION_CLASSIFIER, 
-            chat_target=chat_target
+            content_classifier=ContentClassifiers.PROMPT_INJECTION_CLASSIFIER, chat_target=chat_target
         )
 
         # assert that the set_system_prompt method was called
         mock_method.assert_called_once()
 
         # assert that the PROMPT_INJECTION_CLASSIFIER were loaded into system prompt
-        assert "regular_text" in scorer._system_prompt 
+        assert "regular_text" in scorer._system_prompt
         assert "prompt_injection" in scorer._system_prompt
