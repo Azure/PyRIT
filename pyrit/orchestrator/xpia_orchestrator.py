822e7f62aa30c78fc042c5e25590b0128f205624
Blake Bullwinkel <blakebullwinkel@gmail.com>
2024-04-24 21:48:22 -0700

    FEAT: Refactored SelfAskGptClassifier into SelfAskScore class and added Likert scale scoring (#154)
    * added additional optional fields to the Score dataclass
    
    * updated scoring docs with Likert scale scoring example
    
    * added SelfAskGptClassifier and SelfAskGptLikertScale as child classes of a generalized SelfAskScore class, which takes a system prompt and a content classifier
    
    * added scoring system prompts and classifiers to __init__
    
    * added scoring system prompts
    
    * added new content classifiers
    
    * updated system prompts and classifiers to use absolute paths from path.py
    
    * moved Score dataclass from models.py to score.py
    
    * updated import paths for Score dataclass
    
    * renamed score.py to score_class.py and added copyright
    
    * moved SupportTextClassification from interfaces.py into score directory to avoid circular import error
    
    * reran scoring notebook and confirmed that circular import error is fixed
    
    * ran pre-commit hooks
    
    * removed internal attributes from SelfAskScore docstring
    
    * added clarifying details to scoring demo notebook
    
    * listed score modules in alphabetical order
    
    * moved content classifiers and scoring system prompts into datasets/score
    
    * updated paths for content classifiers and scoring system prompts
    
    * removed biochem classifier
    
    * separated likert scales and content classifiers into separate directories
    
    * updated paths for content classifiers and likert scales
    
    * created enum classes for scoring system prompts and classifiers
    
    * updated scoring demo notebook with new enum classes
    
    * updated how to guide with new scoring API
    
    * ran pre-commit hooks
    
    * added test to check that scorer system prompt is loaded correctly
    
    * fixed GandalfLevel import
    
    * updated SelfAskScore docstring with new enum classes
    
    * reran jupytext on scoring notebook
    
    * ran pre-commit hooks

--- pyrit/orchestrator/xpia_orchestrator.py
+++ pyrit/orchestrator/xpia_orchestrator.py
@@ -4,10 +4,10 @@
 import logging
 from typing import Optional
 from uuid import uuid4
-from pyrit.interfaces import SupportTextClassification
+from pyrit.score import SupportTextClassification
 
 from pyrit.memory import MemoryInterface
-from pyrit.models import Score
+from pyrit.score import Score
 from pyrit.orchestrator import Orchestrator
 from pyrit.prompt_normalizer import PromptNormalizer
 from pyrit.prompt_target import PromptTarget
