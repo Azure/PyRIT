96a9c49c0795806574a61aa0157686b0b4a13351
Raja Sekhar Rao Dheekonda <rdheekonda@microsoft.com>
2024-04-25 01:00:17 -0700

    Add jupytext module for gpt v prompt chat target

---
+++ doc/code/targets/gpt_v_target.py
@@ -0,0 +1,41 @@
+# %% [markdown]
+# ## Azure OpenAI GPT-V Target Demo
+# This notebook demonstrates how to use the Azure OpenAI GPT-V target to accept multimodal input (text+image) and generate text output.
+
+# %%
+from pyrit.models import PromptRequestPiece, PromptRequestResponse
+from pyrit.prompt_target import AzureOpenAIGPTVChatTarget
+from pyrit.common import default_values
+import uuid
+
+default_values.load_default_env()
+test_conversation_id = str(uuid.uuid4())
+
+request_pieces = [
+PromptRequestPiece(
+    role="user",
+    conversation_id=test_conversation_id,
+    original_prompt_text="Describe this picture:",
+    original_prompt_data_type="text",
+    converted_prompt_data_type="text"
+), 
+PromptRequestPiece(
+    role="user",
+    conversation_id=test_conversation_id,
+    original_prompt_text="C://data//images//aeroplane.jpg",
+    original_prompt_data_type="image_path",
+    converted_prompt_data_type="image_path"
+)]
+
+# %%
+prompt_request_response = PromptRequestResponse(request_pieces=request_pieces)
+
+# %%
+with AzureOpenAIGPTVChatTarget() as azure_openai_chat_target:
+    resp = await azure_openai_chat_target.send_prompt_async(prompt_request=prompt_request_response)
+    print(resp)
+
+# %%
+
+
+
