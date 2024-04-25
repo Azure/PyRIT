94024bc0db438011f74982fbd0cb10bbe1badaa8
Raja Sekhar Rao Dheekonda <rdheekonda@microsoft.com>
2024-04-25 10:27:07 -0700

    Update image example input to pyrit architecture

--- doc/code/targets/gpt_v_target.py
+++ doc/code/targets/gpt_v_target.py
@@ -10,27 +10,31 @@
 from pyrit.models import PromptRequestPiece, PromptRequestResponse
 from pyrit.prompt_target import AzureOpenAIGPTVChatTarget
 from pyrit.common import default_values
+import pathlib
+from pyrit.common.path import HOME_PATH
 import uuid
 
 default_values.load_default_env()
 test_conversation_id = str(uuid.uuid4())
 
+# use the image from our docs
+image_path = pathlib.Path(HOME_PATH) / "assets" / "pyrit_architecture.png"
+
 request_pieces = [
-    PromptRequestPiece(
-        role="user",
-        conversation_id=test_conversation_id,
-        original_prompt_text="Describe this picture:",
-        original_prompt_data_type="text",
-        converted_prompt_data_type="text",
-    ),
-    PromptRequestPiece(
-        role="user",
-        conversation_id=test_conversation_id,
-        original_prompt_text="C://data//images//aeroplane.jpg",
-        original_prompt_data_type="image_path",
-        converted_prompt_data_type="image_path",
-    ),
-]
+PromptRequestPiece(
+    role="user",
+    conversation_id=test_conversation_id,
+    original_prompt_text="Describe this picture:",
+    original_prompt_data_type="text",
+    converted_prompt_data_type="text"
+), 
+PromptRequestPiece(
+    role="user",
+    conversation_id=test_conversation_id,
+    original_prompt_text=str(image_path),
+    original_prompt_data_type="image_path",
+    converted_prompt_data_type="image_path"
+)]
 
 # %%
 prompt_request_response = PromptRequestResponse(request_pieces=request_pieces)
@@ -41,3 +45,6 @@ with AzureOpenAIGPTVChatTarget() as azure_openai_chat_target:
     print(resp)
 
 # %%
+
+
+
