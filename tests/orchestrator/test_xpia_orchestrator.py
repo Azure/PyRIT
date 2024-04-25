bbf7204c92c5b343170acbe33cf2b7fb58de5231
Roman Lutz <romanlutz13@gmail.com>
2024-04-25 12:26:32 -0700

    test fix

--- tests/orchestrator/test_xpia_orchestrator.py
+++ tests/orchestrator/test_xpia_orchestrator.py
@@ -12,7 +12,7 @@ import pytest
 
 from unittest.mock import Mock, patch
 
-from pyrit.prompt_target.prompt_target import PromptTarget
+from pyrit.prompt_target import PromptTarget
 from pyrit.score import Score, SupportTextClassification
 from tests.mocks import get_memory_interface, MockPromptTarget
 
@@ -23,7 +23,7 @@ def memory_interface() -> Generator[MemoryInterface, None, None]:
 
 
 @pytest.fixture
-def prompt_target(memory_interface) -> PromptTarget:
+def attack_setup_target(memory_interface) -> PromptTarget:
     return MockPromptTarget(memory=memory_interface)
 
 
@@ -39,7 +39,7 @@ def success_scorer() -> SupportTextClassification:
     return scorer
 
 
-def test_xpia_orchestrator_execute_no_scorer(prompt_target):
+def test_xpia_orchestrator_execute_no_scorer(attack_setup_target):
     def processing_callback():
         return_request_response_obj = Mock()
         return_response_piece = Mock()
@@ -49,13 +49,13 @@ def test_xpia_orchestrator_execute_no_scorer(prompt_target):
 
     xpia_orchestrator = XPIAOrchestrator(
         attack_content="test",
-        prompt_target=prompt_target,
+        attack_setup_target=attack_setup_target,
         processing_callback=processing_callback,
     )
     assert xpia_orchestrator.execute() is None
 
 
-def test_xpia_orchestrator_execute(prompt_target, success_scorer):
+def test_xpia_orchestrator_execute(attack_setup_target, success_scorer):
     def processing_callback():
         return_request_response_obj = Mock()
         return_response_piece = Mock()
@@ -65,7 +65,7 @@ def test_xpia_orchestrator_execute(prompt_target, success_scorer):
 
     xpia_orchestrator = XPIAOrchestrator(
         attack_content="test",
-        prompt_target=prompt_target,
+        attack_setup_target=attack_setup_target,
         scorer=success_scorer,
         processing_callback=processing_callback,
     )
@@ -74,12 +74,12 @@ def test_xpia_orchestrator_execute(prompt_target, success_scorer):
     assert success_scorer.score_text.called_once
 
 
-def test_xpia_manual_processing_orchestrator_execute(prompt_target, success_scorer, monkeypatch):
+def test_xpia_manual_processing_orchestrator_execute(attack_setup_target, success_scorer, monkeypatch):
     # Mocking user input to be "test"
     monkeypatch.setattr("builtins.input", lambda _: "test")
     xpia_orchestrator = XPIAManualProcessingOrchestrator(
         attack_content="test",
-        prompt_target=prompt_target,
+        attack_setup_target=attack_setup_target,
         scorer=success_scorer,
     )
     score = xpia_orchestrator.execute()
@@ -87,13 +87,13 @@ def test_xpia_manual_processing_orchestrator_execute(prompt_target, success_scor
     assert success_scorer.score_text.called_once
 
 
-def test_xpia_test_orchestrator_execute(prompt_target, processing_target, success_scorer):
+def test_xpia_test_orchestrator_execute(attack_setup_target, processing_target, success_scorer):
     with patch.object(processing_target, "send_prompt") as mock_send_to_processing_target:
         xpia_orchestrator = XPIATestOrchestrator(
             attack_content="test",
             processing_prompt="some instructions and the required <test>",
             processing_target=processing_target,
-            prompt_target=prompt_target,
+            attack_setup_target=attack_setup_target,
             scorer=success_scorer,
         )
         score = xpia_orchestrator.execute()
@@ -102,7 +102,7 @@ def test_xpia_test_orchestrator_execute(prompt_target, processing_target, succes
         assert mock_send_to_processing_target.called_once
 
 
-def test_xpia_orchestrator_process_async(prompt_target, processing_target, success_scorer):
+def test_xpia_orchestrator_process_async(attack_setup_target, processing_target, success_scorer):
     with patch.object(processing_target, "send_prompt") as mock_send_to_processing_target:
         with patch.object(processing_target, "send_prompt_async") as mock_send_async_to_processing_target:
             mock_send_to_processing_target.side_effect = NotImplementedError()
@@ -110,7 +110,7 @@ def test_xpia_orchestrator_process_async(prompt_target, processing_target, succe
                 attack_content="test",
                 processing_prompt="some instructions and the required <test>",
                 processing_target=processing_target,
-                prompt_target=prompt_target,
+                attack_setup_target=attack_setup_target,
                 scorer=success_scorer,
             )
             score = xpia_orchestrator.execute()
