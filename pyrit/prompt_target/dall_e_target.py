949fafbed4245879c2f8d46c0bfe6cd736abdfcf
jbolor21 <86250273+jbolor21@users.noreply.github.com>
2024-04-26 17:34:53 -0700

    MAINT Refactoring Dalle Target (#156)
    * initial commit, adding draft changes
    
    * addressing feedback more refactors
    
    * fixing unit test
    
    * restructuring to get rid of output file and mypy fixes
    
    * fixing pre-commit
    
    * adding unit tests
    
    * moving the error parsing
    
    * pre commit fixes and refactors
    
    * minor changes addressing feedback
    
    * Getting rid of unnecessary function and comments
    
    * adding unit test for save base 64
    
    * minor changes
    
    * mypy

--- pyrit/prompt_target/dall_e_target.py
+++ pyrit/prompt_target/dall_e_target.py
@@ -2,37 +2,22 @@
 # Licensed under the MIT license.
 import json
 import logging
-import os
 import pathlib
-from enum import Enum
-import uuid
+import concurrent.futures
+import asyncio
+from typing import Literal, Optional
 
-import requests
 from openai import BadRequestError
 
 from pyrit.common.path import RESULTS_PATH
 from pyrit.memory.memory_interface import MemoryInterface
-from pyrit.models import PromptRequestResponse, PromptRequestPiece
+from pyrit.models import PromptRequestResponse
+from pyrit.models.prompt_request_piece import PromptRequestPiece, PromptResponseError
 from pyrit.prompt_target import PromptTarget
 from pyrit.prompt_target.prompt_chat_target.openai_chat_target import AzureOpenAIChatTarget
+from pyrit.prompt_normalizer import data_serializer_factory
 
 logger = logging.getLogger(__name__)
-
-
-class ImageSizing(Enum):
-    SIZE256 = "256x256"
-    SIZE512 = "512x512"
-    SIZE1024 = "1024x1024"
-
-
-class ResponseFormat(Enum):
-    B64 = "b64_json"
-    URL = "url"
-
-
-class SupportedDalleVersions(Enum):
-    V2 = 2
-    V3 = 3
 
 
 class DALLETarget(PromptTarget):
@@ -47,10 +32,13 @@ class DALLETarget(PromptTarget):
         api_version (str, optional): The API version. Defaults to "2024-02-01".
         image_size (str, optional): The size of the image to output, must be a value of VALID_SIZES.
             Defaults to 1024x1024.
-        response_format (str, optional): Format of output (base64 or url). Defaults to b64_json.
-        num_images (int, optional): The num ber of output images to generate.
+        num_images (int, optional): The number of output images to generate.
             Defaults to 1. For DALLE-3, can only be 1, for DALLE-2 max is 10 images.
         dalle_version (int, optional): Version of DALLE service. Defaults to 3.
+        memory: (memory, optional): Memory to store the chat messages. DuckDBMemory will be used by default.
+        headers (dict, optional): Headers of the endpoint.
+        quality (str, optional): picture quality. Defaults to standard
+        style (str, optional): image style. Defaults to natural
     """
 
     def __init__(
@@ -59,29 +47,30 @@ class DALLETarget(PromptTarget):
         deployment_name: str = None,
         endpoint: str = None,
         api_key: str = None,
-        api_version: str | None = "2024-02-01",
-        image_size: ImageSizing | None = ImageSizing.SIZE1024,
-        response_format: ResponseFormat | None = ResponseFormat.B64,
-        num_images: int | None = 1,
-        dalle_version: SupportedDalleVersions | None = SupportedDalleVersions.V2,
+        api_version: str = "2024-02-01",
+        image_size: Literal["256x256", "512x512", "1024x1024"] = "1024x1024",
+        num_images: int = 1,
+        dalle_version: Literal["dall-e-2", "dall-e-3"] = "dall-e-2",
         memory: MemoryInterface | None = None,
+        headers: Optional[dict[str, str]] = None,
+        quality: Literal["standard", "hd"] = "standard",
+        style: Literal["natural", "vivid"] = "natural",
     ):
 
         super().__init__(memory=memory)
 
-        if num_images is None:
-            num_images = 1  # set 1 as default
-
-        # make sure number of images is allowed by Dall-e version
-        if dalle_version == SupportedDalleVersions.V3:
+        # make sure number of images and headers are allowed by Dall-e version
+        self.dalle_version = dalle_version
+        if dalle_version == "dall-e-3":
             if num_images != 1:
                 raise ValueError("DALL-E-3 can only generate 1 image at a time.")
-        elif dalle_version == SupportedDalleVersions.V2:
+            self.quality = quality
+            self.style = style
+        elif dalle_version == "dall-e-2":
             if num_images < 1 or num_images > 10:
                 raise ValueError("DALL-E-2 can generate only up to 10 images at a time.")
 
-        self.image_size = image_size.value
-        self.response_format = response_format.value
+        self.image_size = image_size
         self.n = num_images
 
         self.deployment_name = deployment_name
@@ -90,28 +79,7 @@ class DALLETarget(PromptTarget):
         )
         self.output_dir = pathlib.Path(RESULTS_PATH) / "images"
 
-    def download_image(self, image_url: str, output_filename: str) -> str:
-        """
-        Downloads the image from a URL and stores the image locally
-        Args:
-            image_url (str): URL which image is stored at
-            output_filename (str): name of file to store image in
-        Returns: file location
-        """
-
-        # This will likely be replaced once our memory is refactored!
-        # If the directory doesn't exist, create it
-        if not os.path.isdir(self.output_dir):
-            os.mkdir(self.output_dir)
-
-        # Initialize the image path
-        image_path = self.output_dir / output_filename
-        # Retrieve the generated image
-
-        generated_image = requests.get(image_url).content  # download the image
-        with open(image_path, "wb") as image_file:
-            image_file.write(generated_image)
-        return str(image_path)
+        self.headers = headers
 
     def send_prompt(
         self,
@@ -119,23 +87,10 @@ class DALLETarget(PromptTarget):
         prompt_request: PromptRequestResponse,
     ) -> PromptRequestResponse:
         """
-        Sends prompt to image target and returns response
-        Args:
-            normalized_prompt (str): the prompt to send
-            conversation_id (str): the ID of the conversation for memory
-            normalizer_id (str): the ID provided by the prompt normalizer
-        Returns: response from target model in a JSON format
+        Deprecated. Use send_prompt_async instead.
         """
-
-        request = prompt_request.request_pieces[0]
-
-        output_filename = f"{uuid.uuid4()}.png"
-
-        self._memory.add_request_response_to_memory(request=prompt_request)
-
-        resp = self._generate_images(prompt=request.converted_prompt_text)
-
-        return self._parse_response_and_add_to_memory(resp, output_filename, request)
+        pool = concurrent.futures.ThreadPoolExecutor()
+        return pool.submit(asyncio.run, self.send_prompt_async(prompt_request=prompt_request)).result()
 
     async def send_prompt_async(
         self,
@@ -145,93 +100,87 @@ class DALLETarget(PromptTarget):
         """
         (Async) Sends prompt to image target and returns response
         Args:
-            normalized_prompt (str): the prompt to send
-            conversation_id (str): the ID of the conversation for memory
-            normalizer_id (str): the ID provided by the prompt normalizer
-        Returns: response from target model in a JSON format
+            prompt_request (PromptRequestResponse): the prompt to send formatted as an object
+        Returns: response from target model formatted as an object
         """
 
+        self.validate_request(prompt_request=prompt_request)
         request = prompt_request.request_pieces[0]
-
-        output_filename = f"{uuid.uuid4()}.png"
 
         self._memory.add_request_response_to_memory(request=prompt_request)
 
-        resp = await self._generate_images_async(prompt=request.converted_prompt_text)
-        return self._parse_response_and_add_to_memory(resp, output_filename, request)
+        return await self._generate_images_async(prompt=request.converted_prompt_text, request=request)
 
-    def _parse_response_and_add_to_memory(
-        self, resp: dict, output_filename: str, prompt_request: PromptRequestPiece
-    ) -> PromptRequestResponse:
-        if "error" not in resp.keys():
-            image_location = output_filename
-
-            if self.response_format == "url":
-                image_url = resp["data"][0]["url"]  # extract image URL from response
-                image_location = self.download_image(image_url=image_url, output_filename=output_filename)
-                resp["image_file_location"] = image_location
-
-            # TODO save image if not URL
-
-            return self._memory.add_response_entries_to_memory(
-                request=prompt_request,
-                response_text_pieces=[image_location],
-                response_type="image_path",
-                prompt_metadata=json.dumps(resp),
-            )
-
-        else:
-            if resp["exception type"] == "Blocked":
-
-                return self._memory.add_response_entries_to_memory(
-                    request=prompt_request,
-                    response_text_pieces=[],
-                    response_type="image_path",
-                    prompt_metadata=json.dumps(resp),
-                    error="blocked",
-                )
-
-            elif resp["exception type"] == "JSON Error":
-                logger.error(f"Response could not be interpreted in the JSON format\n{resp['error']}")
-                raise
+    async def _generate_images_async(self, prompt: str, request=PromptRequestPiece) -> PromptRequestResponse:
+        try:
+            if self.dalle_version == "dall-e-3":
+                if self.quality and self.style:
+                    response = await self.image_target._async_client.images.generate(
+                        model=self.deployment_name,
+                        prompt=prompt,
+                        n=self.n,
+                        size=self.image_size,
+                        response_format="b64_json",
+                        quality=self.quality,
+                        style=self.style,
+                    )
+                else:
+                    response = await self.image_target._async_client.images.generate(
+                        model=self.deployment_name,
+                        prompt=prompt,
+                        n=self.n,
+                        size=self.image_size,
+                        response_format="b64_json",
+                    )
             else:
-                logger.error(f"Error in calling deployment {self.deployment_name}\n{resp['error']}")
-                raise
-
-    async def _generate_images_async(self, prompt: str) -> dict:
-        try:
-            response = await self.image_target._async_client.images.generate(
-                model=self.deployment_name,
-                prompt=prompt,
-                n=self.n,
-                size=self.image_size,
-                response_format=self.response_format,
-            )
+                response = await self.image_target._async_client.images.generate(
+                    model=self.deployment_name,
+                    prompt=prompt,
+                    n=self.n,
+                    size=self.image_size,
+                    response_format="b64_json",
+                )
             json_response = json.loads(response.model_dump_json())
-            return json_response
-        except BadRequestError as e:
-            return {"error": e, "exception type": "Blocked"}
-        except json.JSONDecodeError as e:
-            return {"error": e, "exception type": "JSON Error"}
-        except Exception as e:
-            return {"error": e, "exception type": "exception"}
 
-    def _generate_images(self, prompt: str) -> dict:
-        try:
-            response = self.image_target._client.images.generate(
-                model=self.deployment_name,
-                prompt=prompt,
-                n=self.n,
-                size=self.image_size,
-                response_format=self.response_format,
-            )
-            json_response = json.loads(response.model_dump_json())
-            return json_response
+            data = data_serializer_factory(data_type="image_path")
+            b64_data = json_response["data"][0]["b64_json"]
+            data.save_b64_image(data=b64_data)
+            prompt_text = data.prompt_text
+            error: PromptResponseError = "none"
+
         except BadRequestError as e:
-            logger.error(f"Content Blocked\n{e}")
-            return {"error": e}
+            json_response = {"exception type": "Blocked", "data": ""}
+            json_response["error"] = e.body
+            prompt_text = "content blocked"
+            error = "blocked"
+
         except json.JSONDecodeError as e:
-            logger.error(f"Response could not be interpreted in the JSON format\n{e}")
+            json_response = {"error": e, "exception type": "JSON Error"}
+            prompt_text = "JSON Error"
+            error = "processing"
+
         except Exception as e:
-            logger.error(f"Error in calling deployment {self.deployment_name}\n{e}")
-        return {}
+            json_response = {"error": e, "exception type": "exception"}
+            prompt_text = "target error"
+            error = "unknown"
+
+        return self._memory.add_response_entries_to_memory(
+            request=request,
+            response_text_pieces=[prompt_text],
+            response_type="image_path",
+            prompt_metadata=json.dumps(json_response),
+            error=error,
+        )
+
+    def validate_request(self, *, prompt_request: PromptRequestResponse) -> None:
+        if len(prompt_request.request_pieces) != 1:
+            raise ValueError("This target only supports a single prompt request piece.")
+
+        if prompt_request.request_pieces[0].converted_prompt_data_type != "text":
+            raise ValueError("This target only supports text prompt input.")
+
+        request = prompt_request.request_pieces[0]
+        messages = self._memory.get_chat_messages_with_conversation_id(conversation_id=request.conversation_id)
+
+        if len(messages) > 0:
+            raise ValueError("This target only supports a single turn conversation.")
