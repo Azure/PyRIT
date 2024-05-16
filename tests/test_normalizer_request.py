456cf748e5f5921b0fc5965f27c9441ac9c7d9ea
Bolor <bjagdagdorj@gmail.com>
2024-05-16 15:28:46 -0700

    precommit

--- tests/test_normalizer_request.py
+++ tests/test_normalizer_request.py
@@ -18,7 +18,7 @@ class MockPromptConverter(PromptConverter):
         return ConverterResult(output_text=prompt, output_type="text")
 
     async def async_convert(self, *, prompt: str, input_type: PromptDataType = "text") -> ConverterResult:
-        return await ConverterResult(output_text=prompt, output_type="text") #type: ignore
+        return await ConverterResult(output_text=prompt, output_type="text")  # type: ignore
 
     def input_supported(self, input_type: PromptDataType) -> bool:
         return input_type == "text"
