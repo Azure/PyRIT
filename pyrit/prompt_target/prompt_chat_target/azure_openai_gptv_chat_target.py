3543e9546f55a68f297a04bcc6a7d134ef5a4b0e
rlundeen2 <rlundeen@microsoft.com>
2024-06-06 14:20:48 -0700

    small pr feedback

--- pyrit/prompt_target/prompt_chat_target/azure_openai_gptv_chat_target.py
+++ pyrit/prompt_target/prompt_chat_target/azure_openai_gptv_chat_target.py
@@ -158,11 +158,6 @@ class AzureOpenAIGPTVChatTarget(PromptChatTarget):
             response_entry = construct_response_from_request(request=request, response_text_pieces=[resp_text])
         except BadRequestError as bre:
             response_entry = handle_bad_request_exception(response_text=bre.message, request=request)
-        except Exception as ex:
-            construct_response_from_request(
-                request=request, response_text_pieces=[str(ex)], response_type="error", error="processing"
-            )
-            raise
 
         return response_entry
 
