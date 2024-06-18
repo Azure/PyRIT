701183dc043a603fc881b1ea6bcd35fc0a8cf135
SafwanA02 <safwanahmad2002@gmail.com>
2024-06-17 20:43:26 -0700

    added init file changes

--- pyrit/orchestrator/__init__.py
+++ pyrit/orchestrator/__init__.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 from pyrit.orchestrator.orchestrator_class import Orchestrator
+from pyrit.orchestrator.master_key_orchestrator import MasterKeyOrchestrator
 from pyrit.orchestrator.prompt_sending_orchestrator import PromptSendingOrchestrator
 from pyrit.orchestrator.red_teaming_orchestrator import RedTeamingOrchestrator
 from pyrit.orchestrator.scoring_orchestrator import ScoringOrchestrator
@@ -12,6 +13,7 @@ from pyrit.orchestrator.xpia_orchestrator import (
 )
 
 __all__ = [
+    "MasterKeyOrchestrator"
     "Orchestrator",
     "PromptSendingOrchestrator",
     "RedTeamingOrchestrator",
