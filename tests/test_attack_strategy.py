ca01dd9d2f9415f76ee15b602875e174306cb402
gary <dlmgary@outlook.com>
2024-06-23 11:14:15 -0400

    bugfix: fixed issue with relative test file reference.

--- tests/test_attack_strategy.py
+++ tests/test_attack_strategy.py
@@ -1,9 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-import os
-import pathlib
 import pytest
+
+from pyrit.common.path import DATASETS_PATH
 from pyrit.models import AttackStrategy
 
 
@@ -14,13 +14,11 @@ def test_attack_strategy_strings():
 
 
 def test_attack_strategy_from_file():
-    strategy_path = (
-        pathlib.Path(os.getcwd()) / "pyrit" / "datasets" / "orchestrators" / "red_teaming" / "text_generation.yaml"
-    )
+    strategy_path = DATASETS_PATH / "orchestrators" / "red_teaming" / "text_generation.yaml"
     with open(strategy_path, "r") as strategy_file:
         strategy = strategy_file.read()
         string_before_template = "template: |\n  "
-        strategy_template = strategy[strategy.find(string_before_template) + len(string_before_template) :]
+        strategy_template = strategy[strategy.find(string_before_template) + len(string_before_template):]
         strategy_template = strategy_template.replace("\n  ", "\n")
     assert strategy_template.replace("{{ conversation_objective }}", "my objective") == str(
         AttackStrategy(strategy=strategy_path, conversation_objective="my objective")
