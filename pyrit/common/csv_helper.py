762a1ee46d54bb328521979f3745b39bce13b900
Volkan Kutal <50370416+KutalVolkan@users.noreply.github.com>
2024-08-03 01:11:36 +0200

    FEAT Add many-shot jailbreaking feature implementation (#254)
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

---
+++ pyrit/common/csv_helper.py
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import csv
+from typing import List, Dict
+
+
+def read_csv(file) -> List[Dict[str, str]]:
+    reader = csv.DictReader(file)
+    return [row for row in reader]
+
+
+def write_csv(file, examples: List[Dict[str, str]]):
+    writer = csv.DictWriter(file, fieldnames=examples[0].keys())
+    writer.writeheader()
+    writer.writerows(examples)
