6fdbb9f4a52c50f1d46ffe7d0b0bfcf07821312f
rlundeen2 <rlundeen@microsoft.com>
2024-08-06 13:45:22 -0700

    pr feedback

--- doc/code/orchestrators/6_crescendo_orchestrator.py
+++ doc/code/orchestrators/6_crescendo_orchestrator.py
@@ -4,24 +4,27 @@
 #     cell_metadata_filter: -all
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.2
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: pyrit
 #     language: python
 #     name: python3
 # ---
 
-# # Crescendo Orchestrator
+# %% [markdown]
+# # Crescendo Orhcestrator
 #
-# This demo showcases the use of the `CrescendoOrchestrator` in PyRIT.
+# Orchestrators are a great way to implement many attack techniques. This demo showcases the use of the `CrescendoOrchestrator` in PyRIT.
 #
-# The Crescendo Attack is a strategy that gradually guides a model to generate harmful content through small, seemingly harmless steps. The `CrescendoOrchestrator` utilizes a red teaming chat target to create and send increasingly harmful prompts to the attack target. Initially, the generated prompts are benign, but they gradually become more dangerous. After each prompt is sent, the target chat bot's response is evaluated by the `CrescendoOrchestrator` to determine if it has produced harmful content. If the target chat bot refuses to respond, the `CrescendoOrchestrator` backtracks by forgetting the last turn and tries a different approach. If the chat bot does respond, the `CrescendoOrchestrator` evaluates whether the conversation objective has been achieved. If the objective has not been achieved, the red teaming chat will generate a new prompt and send it to the chat bot. This process continues until the chat bot produces harmful content or the maximum number of attempts is reached.
+# The Crescendo Attack is a strategy that gradually guides a model to generate harmful content through small, seemingly harmless steps. The `CrescendoOrchestrator` utilizes a red teaming chat target to create and send increasingly harmful prompts to the target chat bot. Initially, the generated prompts are benign, but they gradually become more dangerous. After each prompt is sent, the target chat bot's response is evaluated by the `CrescendoOrchestrator` to determine if it has produced harmful content. If the target chat bot refuses to respond, the `CrescendoOrchestrator` backtracks the red teaming chat target's memory and tries a different approach. If the chat bot does respond, the `CrescendoOrchestrator` evaluates whether the conversation objective has been achieved. If the objective has not been achieved, the red teaming chat will generate a new prompt and send it to the chat bot. This process continues until the chat bot produces harmful content or the maximum number of attempts is reached.
 #
-# Note that for this to succeed, the `CrescendoOrchestrator` requires an LLM endpoint without content moderation or other safety filtering mechanisms. Even then, success may depend on the model and may not be guaranteed every time.
+# Note that for this to succeed, the `CrescendoOrchestrator` requires a model endpoint without content moderation or other safety mechanisms. Even then, success may depend on the model and may not be guaranteed every time.
+#
+# Before you begin, ensure you are set up with the correct version of PyRIT installed and have secrets configured as described [here](../../setup/).
 
-# +
+# %%
 from pyrit.common import default_values
 from pyrit.orchestrator import CrescendoOrchestrator
 from pyrit.prompt_target import AzureOpenAIChatTarget, AzureMLChatTarget
@@ -33,8 +36,15 @@ prompt_target = AzureMLChatTarget()
 red_teaming_chat = AzureOpenAIChatTarget()
 scorer = AzureOpenAIChatTarget()
 
-with CrescendoOrchestrator(conversation_objective=conversation_objective, prompt_target=prompt_target, red_teaming_chat=red_teaming_chat, scoring_target=scorer) as orchestrator:
-        
-    score = await orchestrator.apply_crescendo_attack_async(max_rounds = 5, max_backtracks=5)  # type: ignore
-    orchestrator.print_prompt_target_memory()
-    print(score)
+with CrescendoOrchestrator(
+    conversation_objective=conversation_objective,
+    prompt_target=prompt_target,
+    red_teaming_chat=red_teaming_chat,
+    scoring_target=scorer
+) as orchestrator:
+
+    # For five turns this can take a couple minutes depending on LLM latency
+    score = await orchestrator.apply_crescendo_attack_async(max_rounds=5, max_backtracks=5)  # type: ignore
+    orchestrator.print_conversation()
+
+    print(f"{score} {score.score_metadata} {score.score_rationale} ")
--- tests/memory/test_memory_interface.py
+++ tests/memory/test_memory_interface.py
@@ -188,7 +188,7 @@ def test_duplicate_conversation_excluding_last_turn(memory: MemoryInterface):
     all_memory = memory.get_all_prompt_pieces()
     assert len(all_memory) == 7
 
-    duplicate_conversation = memory._get_prompt_pieces_with_conversation_id(conversation_id_1)
+    duplicate_conversation = memory._get_prompt_pieces_with_conversation_id(conversation_id=conversation_id_1)
     assert len(duplicate_conversation) == 2
 
     for piece in duplicate_conversation:
