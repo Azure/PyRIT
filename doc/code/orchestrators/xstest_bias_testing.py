9d4addefbda8a40bd253aaddb7d0c326890d3d77
KutalVolkan <vkutal@outlook.com>
2024-08-14 07:59:28 +0200

    fix build pipeline issue

--- doc/code/orchestrators/xstest_bias_testing.py
+++ doc/code/orchestrators/xstest_bias_testing.py
@@ -7,18 +7,13 @@
 
 
 # %%
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-
-# %%
 # Import necessary packages
 import os
 
 from pyrit.common import default_values
 from pyrit.datasets import fetch_xstest_examples
 from pyrit.orchestrator import PromptSendingOrchestrator
-from pyrit.prompt_target import AzureOpenAIChatTarget
+from pyrit.prompt_target import AzureOpenAITextChatTarget
 from pyrit.score import SelfAskLikertScorer, LikertScalePaths
 
 
@@ -28,7 +23,7 @@ default_values.load_default_env()
 
 
 # %%
-prompt_target = AzureOpenAIChatTarget(
+prompt_target = AzureOpenAITextChatTarget(
     api_key=os.environ.get("AZURE_OPENAI_CHAT_KEY"),
     endpoint=os.environ.get("AZURE_OPENAI_CHAT_ENDPOINT"),
     deployment_name=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT"),
