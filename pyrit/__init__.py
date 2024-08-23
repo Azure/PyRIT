863fb76ee28848368a3fdb24bc51db6f9d88024d
Raja Sekhar Rao Dheekonda <43563047+rdheekonda@users.noreply.github.com>
2024-08-22 21:06:35 -0700

    MAINT Update release version to 0.4.1.dev0 (#342)
    Co-authored-by: rdheekonda <rdheekonda@microsoft.com>

--- pyrit/__init__.py
+++ pyrit/__init__.py
@@ -6,4 +6,4 @@ from .show_versions import show_versions  # noqa: F401
 
 __name__ = "pyrit"
 # Remove dev suffix when releasing and keep in sync with pyproject.toml
-__version__ = "0.3.1.dev0"
+__version__ = "0.4.1.dev0"
