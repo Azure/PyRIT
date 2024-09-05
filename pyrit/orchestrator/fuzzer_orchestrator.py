d0e8be9f0c11ac375eb1375db649e52fbbc4e082
gseetha04 <47045216+gseetha04@users.noreply.github.com>
2024-09-05 10:29:45 -0400

    Add files via upload

---
+++ pyrit/orchestrator/fuzzer_orchestrator.py
@@ -0,0 +1,407 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import asyncio
+from dataclasses import dataclass
+import logging
+import random
+import numpy as np
+from typing import Optional
+from uuid import uuid4
+from colorama import Fore, Style
+from pathlib import Path
+
+from pyrit.common.notebook_utils import is_in_ipython_session
+from pyrit.common.path import SCALES_PATH
+from pyrit.memory import MemoryInterface
+from pyrit.models import PromptRequestPiece, Score
+from pyrit.models.prompt_request_response import PromptRequestResponse
+from pyrit.orchestrator import Orchestrator
+from pyrit.prompt_normalizer import PromptNormalizer, NormalizerRequest
+from pyrit.prompt_target import PromptTarget
+from pyrit.prompt_converter import PromptConverter
+from pyrit.score import Scorer, Score
+from pyrit.score import SelfAskScaleScorer
+from pyrit.score.float_scale_threshold_scorer import FloatScaleThresholdScorer
+from pyrit.score.self_ask_category_scorer import ContentClassifierPaths
+from pyrit.models import PromptTemplate, PromptDataset
+from typing import Callable
+from pyrit.prompt_converter import shorten_converter, expand_converter
+from pyrit.prompt_target import PromptTarget, PromptChatTarget
+#from pyrit.orchestrator.fuzzer_orchestrator import PromptNode
+
+from pyrit.exceptions import MissingPromptPlaceHolderException, pyrit_placeholder_retry
+TEMPLATE_PLACEHOLDER = "{{ prompt }}"
+logger = logging.getLogger(__name__)
+
+class PromptNode:
+    def __init__(
+        self,
+        template: str,
+        parent: Optional['PromptNode'] = None,
+    ):
+        """ Class to maintain the tree information for each prompt template
+
+        Args:
+
+        template: Prompt template.
+
+        Parent: Parent node. 
+
+        """
+    
+        self.template: str = template
+
+        self.parent: PromptNode = parent
+        self.children: list[PromptNode] = []
+        self.level: int = 0 if parent is None else parent.level + 1
+        self.visited_num = 0
+        self.rewards:int = 0 
+        if self.parent is not None:
+            self.parent.children.append(self)
+ 
+class FuzzerOrchestrator(Orchestrator):
+    _memory: MemoryInterface
+
+    def __init__(
+        self,
+        *,
+        prompts: list[str], 
+        prompt_target: PromptTarget,
+        prompt_templates: list[str],
+        prompt_converters: Optional[list[PromptConverter]] = None,
+        template_converter: list[PromptConverter],  # shorten/expand
+        scoring_target: PromptChatTarget,
+        memory: Optional[MemoryInterface] = None,
+        memory_labels: Optional[dict[str, str]] = None,
+        verbose: bool = False,
+        frequency_weight=0.5,
+        reward_penalty=0.1,
+        minimum_reward=0.2,
+        non_leaf_node_probability=0.1,
+        random_seed=None,
+        batch_size: int = 10,
+        jailbreak_goal = 1,
+        query_limit: Optional[int] = None,
+    ) -> None:
+        """Creates an orchestrator that explores a variety of jailbreak options via fuzzing.
+
+        Paper: GPTFUZZER: Red Teaming Large Language Models with Auto-Generated Jailbreak Prompts.
+
+            Link: https://arxiv.org/pdf/2309.10253
+
+            Authors: Jiahao Yu, Xingwei Lin, Zheng Yu, Xinyu Xing
+
+            GitHub: https://github.com/sherdencooper/GPTFuzz
+
+        Args:
+
+            prompts: The prompts will be the questions to the target.
+
+            prompt_target: The target to send the prompts to.
+
+            prompt_templates: List of all the jailbreak templates which will act as the seed pool.
+            At each iteration, a seed will be selected using the MCTS-explore algorithm which will be sent to the
+            shorten/expand prompt converter.
+
+                          The converted template along with the prompt will be sent to the target.
+
+            prompt_converters: The prompt_converters to use to convert the prompts before sending
+            them to the prompt target.
+
+            template_converter: The converter that will be applied on the jailbreak template that was
+            selected by MCTS-explore.
+
+                          The prompt converters will not be applied to the prompts.
+                          Shorten/expand prompt converters.
+
+            memory: The memory to use to store the chat messages. If not provided, a DuckDBMemory
+            will be used.
+
+            memory_labels: The labels to use for the memory. This is useful to identify the messages in the memory.
+
+            verbose: Whether to print debug information.
+
+            frequency_weight: constant that balances between the seed with high reward and the seed that is selected fewer times.
+
+            reward_penalty: Reward penalty diminishes the reward for the current node and its ancestors when the path lengthens.
+
+            minimum_reward: Minimal reward prevents the reward of the current node and its ancestors from being too small or negative.
+
+            non_leaf_node_probability: parameter which decides the likelihood of selecting a non-leaf node.
+
+            random_seed: Used to save the state of a random function.
+
+            batch_size (int, optional): The (max) batch size for sending prompts. Defaults to 10.
+
+            Jailbreak_goal: Maximum number of the jailbreaks after which the fuzzer will stop.
+
+            Query_limit: Maximum number of times the fuzzer will run.
+
+        """
+
+        super().__init__(
+            prompt_converters=prompt_converters, memory=memory, memory_labels=memory_labels, verbose=verbose
+        )
+
+        self._prompt_target = prompt_target
+        self._prompts = prompts
+        self._prompt_templates = prompt_templates
+        self._template_converter = template_converter
+        self._frequency_weight = frequency_weight
+        self._reward_penalty = reward_penalty
+        self._minimum_reward = minimum_reward
+        self._non_leaf_node_probability = non_leaf_node_probability
+        # Maximum number of the jailbreaks after which the fuzzer will stop
+        self._jailbreak_goal = jailbreak_goal
+        # Maximum number of times the fuzzer will query the target (not including scoring calls)
+        if query_limit:
+            self._query_limit = query_limit
+        else:
+            self._query_limit = len(self._prompt_templates) * len(self._prompts) * 10
+        self._current_query = 0
+        self._current_jailbreak = 0
+        self._batch_size = batch_size
+        self._new_templates = []
+        self._step = 0  # to keep track of the steps or the count
+        # keeps track of the path that has been currently selected
+        self._mcts_selected_path: list[PromptNode] = []  # type: ignore
+        #global TEMPLATE_PLACEHOLDER 
+        
+        scorer_scale_path = Path(SCALES_PATH / "tree_of_attacks_with_pruning_jailbreak.yaml")
+        scale_scorer = SelfAskScaleScorer(
+            chat_target=scoring_target,
+            scale_path=scorer_scale_path,
+            memory=self._memory,
+        )
+        self._scorer = FloatScaleThresholdScorer(
+            scorer=scale_scorer,
+            threshold=0.8,
+            memory=self._memory,
+        )
+
+        if not self._prompt_templates:
+            raise ValueError("The initial set of prompt templates cannot be empty.")
+        if not self._prompts:
+            raise ValueError("The initial prompts cannot be empty")
+        if not self._template_converter:
+            raise ValueError("Template converter cannot be empty")
+
+        if self._batch_size == 0:
+            raise ValueError(f"Batch size must be at least 1.")
+        
+        for template in self._prompt_templates:
+            if TEMPLATE_PLACEHOLDER not in template:
+                raise MissingPromptPlaceHolderException(message="Prompt placeholder is empty.")
+                
+
+        # convert each template into a node and maintain the node information parent, child, etc.
+        self._prompt_nodes: list[PromptNode] = [PromptNode(prompt) for prompt in prompt_templates]
+
+        self._initial_prompts_nodes = self._prompt_nodes.copy()
+
+    async def execute_fuzzer(self) -> PromptRequestPiece:
+        """
+        Steps:
+        Generates new templates by applying transformations to existing templates and returns successful ones.
+        
+        This method uses the MCTS-explore algorithm to select a template in each iteration and
+        applies a randomly chosen template converter to generate a new template.
+
+        Subsequently, it creates a set of prompts by populating instances of the new template with all the prompts.
+        
+        These prompts are sent to the target and the responses scored.
+        
+        A new template is considered successful if this resulted in at least one successful jailbreak
+        which is identified by having a high enough score.
+
+        Successful templates are added to the initial list of templates and may be selected again in subsequent iterations.
+        
+        Finally, rewards for all nodes are updated.
+        
+        The algorithm stops when a sufficient number of jailbreaks are found with new templates or 
+        when the query limit is reached.
+
+        Args:
+            prompt_templates: A list of the initial jailbreak templates that needs to be sent to the MCTS algorithm.
+
+        """
+        # stopping criteria
+        if (self._current_query + len(self._prompts)) >= self._query_limit:
+            logger.info(f"Maximum query limit reached.")
+            return FuzzerResult(success=False, template = self._new_templates, prompt_target_conversation_id=conversation_ids, description ="Maximum query limit reached.")
+        
+        elif self._current_jailbreak >= self._jailbreak_goal:
+            logger.info(f"Maximum number of jailbreaks reached.")
+            return FuzzerResult(success=False, nodes = self._new_templates, prompt_target_conversation_id=conversation_ids, description = 'Maximum number of jailbreaks reached.')
+        
+        
+        # 1. Select a seed from the list of the templates using the MCTS
+        print(self._current_query + len(self._prompts))
+        current_seed = self._select(self._initial_prompts_nodes)
+            
+        # 2. Apply seed converter to the selected template.
+        try:
+            target_seed_obj = await self._apply_template_converter(current_seed.template)
+        except:
+            return FuzzerResult(success=False, nodes = self._new_templates, prompt_target_conversation_id=conversation_ids, description = 'Tried to apply to and failed even after retries as it didn\'t preserve the prompt placeholder.')
+
+        target_template = PromptTemplate(target_seed_obj, parameters=["prompt"])
+
+        # convert the target_template into a prompt_node to maintain the tree information
+        target_template_node = PromptNode(template=target_seed_obj, parent=None)
+
+        # 3. Fill in prompts into the newly generated template.
+
+        jailbreak_prompts = []
+        for prompt in self._prompts:
+            jailbreak_prompts.append(target_template.apply_custom_metaprompt_parameters(prompt=prompt))
+
+        # 4. Apply prompt converter if any and send request to the target
+
+        requests: list[NormalizerRequest] = []
+        for jailbreak_prompt in jailbreak_prompts:
+            requests.append(
+                self._create_normalizer_request(
+                    prompt_text=jailbreak_prompt,
+                    prompt_type="text",
+                    converters=self._prompt_converters,
+                )
+            )
+
+        for request in requests:
+            request.validate()
+
+        responses: list[PromptRequestResponse]
+        responses = await self._prompt_normalizer.send_prompt_batch_to_target_async(
+            requests=requests,
+            target=self._prompt_target,
+            labels=self._global_memory_labels,
+            orchestrator_identifier=self.get_identifier(),
+            batch_size=self._batch_size,
+        )
+
+        response_pieces = [response.request_pieces[0] for response in responses]
+        conversation_ids = [response.request_pieces[0].conversation_id for response in responses]
+        
+        # 5. Score responses.
+        scored_response = []
+        for response in response_pieces:
+            scored_response.append(self._scorer.score_async(response))
+
+        batch_scored_response = await asyncio.gather(*scored_response)
+        
+        score_values = [score[0].get_value() for score in batch_scored_response]
+
+        # 6. Update the rewards for each of the nodes.
+        self._num_jailbreak = 0
+        for index, score in enumerate(score_values):
+            if score == True:
+                self._num_jailbreak += 1
+                conversation_ids_jailbreak = conversation_ids[index]
+        num_executed_queries = len(score_values)
+
+        self._current_jailbreak += self._num_jailbreak
+        self._current_query += num_executed_queries
+
+        if self._num_jailbreak > 0:  # successful jailbreak
+            # The template resulted in at least one jailbreak so it will be part of the results
+            # and a potential starting template for future iterations.
+            self._prompt_nodes.append(target_template_node)
+            self._new_templates.append(target_template)
+            return FuzzerResult(success = True, nodes = self._new_templates, prompt_target_conversation_id=conversation_ids_jailbreak, description='Successful Jailbreak' )
+
+        # update the rewards for the target node and others on its path
+        self._update(target_template_node)
+
+    def _select(self) -> PromptNode:
+        """
+            This method selects the template from the list of templates using the MCTS algorithm.
+        """
+        self._step += 1
+
+        current = max(self._initial_prompts_nodes, key=self._best_UCT_score())  # initial path
+        self._mcts_selected_path.append(current)
+
+        # while node is not a leaf
+        while len(current.children) > 0:
+            if np.random.rand() < self._non_leaf_node_probability:
+                break
+            # compute the node with the best UCT score
+            current = max(current.children, key=self._best_UCT_score())
+            # append node to path
+            self._mcts_selected_path.append(current)
+
+        for prompt_node in self._mcts_selected_path:
+            # keep track of number of visited nodes
+            prompt_node.visited_num += 1
+
+        self._last_choice_node = current
+        # returns the best child
+        return current
+
+    def _best_UCT_score(self):
+        """Function to compute the Upper Confidence Bounds for Trees(UCT) score for each seed. The highest-scoring seed will be selected as the next seed.
+
+        This is an extension of the Monte carlo tree search (MCTS) algorithm which applies Upper Confidence Bound (UCB) to the node.
+
+        The UCB function determines the confidence interval for each node and returns the highest value which will be selected as the next seed.
+        """
+        return lambda pn: pn.rewards  / (pn.visited_num + 1) + self._frequency_weight * np.sqrt(
+            2 * np.log(self._step) / (pn.visited_num + 0.01)
+        )  # self._frequency_weight - constant that balances between the seed with high reward and the seed that is selected fewer times.
+
+    def _update(self, prompt_nodes: PromptNode):
+        """
+        Updates the reward of all the nodes in the last chosen path.
+        """
+        success_number = self._num_jailbreak
+
+        #last_chosen_node = self._prompt_nodes[self._last_choice_index]
+        last_chosen_node = self._last_choice_node
+        for prompt_node in reversed(self._mcts_selected_path):
+            # The output from the template converter will always be a single template so the length of prompt_nodes will be 1.
+            reward = success_number / (len(self._prompts) * 1)  # len(prompt_nodes))
+            prompt_node.rewards += reward * max(
+                self._minimum_reward, (1 - self._reward_penalty * last_chosen_node.level)
+            )
+
+    @pyrit_placeholder_retry
+    async def _apply_template_converter(self, template:str):
+        """
+        Asynchronously applies template converter.
+
+        Args:
+            current_seed: The template that is selected.
+
+        Returns:
+            converted template with placeholder for prompt.
+
+        Raises:
+            MissingPromptHolderException: If the prompt placeholder is still missing.
+        """
+        template_converter = random.choice(self._template_converter)
+
+        target_seed_obj = await template_converter.convert_async(prompt=template)
+        if TEMPLATE_PLACEHOLDER not in target_seed_obj.output_text:
+            raise MissingPromptPlaceHolderException(status_code=204,message="Prompt placeholder is empty.")
+        return target_seed_obj
+
+@dataclass
+class FuzzerResult:
+    jailbreak: bool
+    nodes: PromptNode
+    prompt_target_conversation_id: Optional[list[str]] = None
+
+    def __str__(self) -> str:
+        return (
+            "FuzzerResult("
+            f"success={self.success}, "
+            f"template={self.template}, "
+            f"prompt_target_conversation_id={self.prompt_target_conversation_id})",
+            f"description={self.description}"
+
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
