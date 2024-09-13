34ac2917e583d9d7e0c91f49c1f4a0d5b79bb8e6
Roman Lutz <romanlutz13@gmail.com>
2024-09-13 10:39:37 -0700

    notebook updates

--- doc/code/memory/6_prompt_database.py
+++ doc/code/memory/6_prompt_database.py
@@ -16,7 +16,7 @@
 # %% [markdown]
 # ## Prompt Database in Azure
 #
-# TODO context
+# With AzureSQLMemory we have a centralized database in Azure. Apart from storing results it's also useful to store datasets of prompts and prompt templates that we may want to use at a later point. This can help us in curating prompts with custom metadata like harm categories.
 
 # %%
 import os
@@ -29,12 +29,33 @@ default_values.load_default_env()
 azure_memory = AzureSQLMemory()
 
 
-# %%
-import os
-os.environ
+# %% [markdown]
+# ## Adding prompts to the database
 
 # %%
-azure_memory.
+from pyrit.models import PromptDataset
+from pyrit.common.path import DATASETS_PATH
+import pathlib
 
+prompt_dataset = PromptDataset.from_yaml_file(pathlib.Path(DATASETS_PATH) / "prompts" / "illegal.prompt")
+
+print(prompt_dataset.prompts[0])
+
+azure_memory.add_prompts_to_memory(prompts=prompt_dataset.prompts, added_by="test")
+
+# %% [markdown]
+# # Retrieving prompts from the database
+#
+# First, let's get an idea of what datasets are represented in the database.
+
+# %%
+azure_memory.get_prompt_dataset_names()
+
+# %% [markdown]
+# The dataset we just uploaded (called "test illegal") is also represented. To get all prompts from that dataset, we can query as follows:
+
+# %%
+prompts = azure_memory.get_prompts(dataset_name="test illegal")
+print(prompts[0].__dict__)
 
 # %%
