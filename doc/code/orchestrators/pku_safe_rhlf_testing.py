e1020bb27222def804912572870e7364d5bb4a34
Emilie Rajka <emilierajka@microsoft.com>
2024-09-17 15:49:25 -0700

    fixed typo

--- doc/code/orchestrators/pku_safe_rhlf_testing.py
+++ doc/code/orchestrators/pku_safe_rhlf_testing.py
@@ -1,8 +1,8 @@
 # %% [markdown]
-# # PKU-SafeRHLF dataset testing
+# # PKU-SafeRLHF dataset testing
 #
-# This notebook demonstrates the process of using PKU-SafeRHLF examples to perform AI red teaming on a target language model (LLM).
-# It involves fetching potentially harmful behavior prompts from the PKU-SafeRHLF repository, sending them to the LLM, and analyzing the responses.
+# This notebook demonstrates the process of using PKU-SafeRLHF examples to perform AI red teaming on a target language model (LLM).
+# It involves fetching potentially harmful behavior prompts from the PKU-SafeRLHF repository, sending them to the LLM, and analyzing the responses.
 # The goal is to identify vulnerabilities, inappropriate responses, or weaknesses in the model's handling of harmful or ethically sensitive prompts.
 
 
