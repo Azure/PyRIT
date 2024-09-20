039ba9655c5698a4645ed13747c25a31c1a22242
Blake Bullwinkel <blakebullwinkel@gmail.com>
2024-09-19 18:00:32 -0700

    corrected gcg import paths

--- pyrit/auxiliary_attacks/gcg/GreedyCoordinateGradient/gcg/gcg_attack.py
+++ pyrit/auxiliary_attacks/gcg/GreedyCoordinateGradient/gcg/gcg_attack.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from tqdm.auto import tqdm
 
-from pyrit.auxiliary_attacks.GreedyCoordinateGradient.base.attack_manager import (
+from pyrit.auxiliary_attacks.gcg.GreedyCoordinateGradient.base.attack_manager import (
     AttackPrompt,
     MultiPromptAttack,
     PromptManager,
