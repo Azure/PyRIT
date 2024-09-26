0701c0ffc200137bb3349aae342cec2d753335b5
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2024-09-26 13:42:09 -0700

    adding url converter

--- pyrit/prompt_converter/__init__.py
+++ pyrit/prompt_converter/__init__.py
@@ -33,6 +33,7 @@ from pyrit.prompt_converter.tone_converter import ToneConverter
 from pyrit.prompt_converter.translation_converter import TranslationConverter
 from pyrit.prompt_converter.unicode_confusable_converter import UnicodeConfusableConverter
 from pyrit.prompt_converter.unicode_sub_converter import UnicodeSubstitutionConverter
+from pyrit.prompt_converter.url_converter import UrlConverter
 from pyrit.prompt_converter.variation_converter import VariationConverter
 
 
@@ -69,5 +70,6 @@ __all__ = [
     "TranslationConverter",
     "UnicodeConfusableConverter",
     "UnicodeSubstitutionConverter",
+    "UrlConverter",
     "VariationConverter",
 ]
