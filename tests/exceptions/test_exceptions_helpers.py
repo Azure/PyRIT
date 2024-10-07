a28b41a0e662b3d8b8942a592117f53f3f11a0d3
meisman-ms <meisman@microsoft.com>
2024-10-07 10:09:40 -0700

    add test case & parametrize tests

--- tests/exceptions/test_exceptions_helpers.py
+++ tests/exceptions/test_exceptions_helpers.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-
+import pytest
 from pyrit.exceptions.exception_classes import (
     extract_json_from_string,
     remove_end_md_json,
@@ -10,39 +10,61 @@ from pyrit.exceptions.exception_classes import (
 )
 
 
-def test_remove_start_md_json():
-    assert remove_start_md_json('```json\n{"key": "value"}') == '{"key": "value"}'
-    assert remove_start_md_json('`json\n{"key": "value"}') == '{"key": "value"}'
-    assert remove_start_md_json('json{"key": "value"}') == '{"key": "value"}'
-    assert remove_start_md_json('{"key": "value"}') == '{"key": "value"}'
-    assert remove_start_md_json("No JSON here") == "No JSON here"
-    assert remove_start_md_json('```jsn\n{"key": "value"}\n```') == 'jsn\n{"key": "value"}\n```'
+@pytest.mark.parametrize(
+    "input_str, expected_output",
+    [
+        ('```json\n{"key": "value"}', '{"key": "value"}'),
+        ('`json\n{"key": "value"}', '{"key": "value"}'),
+        ('json{"key": "value"}', '{"key": "value"}'),
+        ('{"key": "value"}', '{"key": "value"}'),
+        ("No JSON here", "No JSON here"),
+        ('```jsn\n{"key": "value"}\n```', 'jsn\n{"key": "value"}\n```'),
+    ],
+)
+def test_remove_start_md_json(input_str, expected_output):
+    assert remove_start_md_json(input_str) == expected_output
 
 
-def test_remove_end_md_json():
-    assert remove_end_md_json('{"key": "value"}\n```') == '{"key": "value"}'
-    assert remove_end_md_json('{"key": "value"}\n`') == '{"key": "value"}'
-    assert remove_end_md_json('{"key": "value"}`') == '{"key": "value"}'
-    assert remove_end_md_json('{"key": "value"}') == '{"key": "value"}'
+@pytest.mark.parametrize(
+    "input_str, expected_output",
+    [
+        ('{"key": "value"}\n```', '{"key": "value"}'),
+        ('{"key": "value"}\n`', '{"key": "value"}'),
+        ('{"key": "value"}`', '{"key": "value"}'),
+        ('{"key": "value"}', '{"key": "value"}'),
+    ],
+)
+def test_remove_end_md_json(input_str, expected_output):
+    assert remove_end_md_json(input_str) == expected_output
 
 
-def test_extract_json_from_string():
-    assert extract_json_from_string('Some text before JSON {"key": "value"} some text after JSON') == '{"key": "value"}'
-    assert extract_json_from_string("Some text before JSON [1, 2, 3] some text after JSON") == "[1, 2, 3]"
-    assert extract_json_from_string('{"key": "value"}') == '{"key": "value"}'
-    assert extract_json_from_string("[1, 2, 3]") == "[1, 2, 3]"
-    assert extract_json_from_string("No JSON here") == "No JSON here"
-    assert extract_json_from_string('jsn\n{"key": "value"}\n```') == '{"key": "value"}'
+@pytest.mark.parametrize(
+    "input_str, expected_output",
+    [
+        ('Some text before JSON {"key": "value"} some text after JSON', '{"key": "value"}'),
+        ("Some text before JSON [1, 2, 3] some text after JSON", "[1, 2, 3]"),
+        ('{"key": "value"}', '{"key": "value"}'),
+        ("[1, 2, 3]", "[1, 2, 3]"),
+        ("No JSON here", "No JSON here"),
+        ('jsn\n{"key": "value"}\n```', '{"key": "value"}'),
+        ('Some text before JSON {"a": [1,2,3], "b": {"c": 4}} some text after JSON', '{"a": [1,2,3], "b": {"c": 4}}'),
+    ],
+)
+def test_extract_json_from_string(input_str, expected_output):
+    assert extract_json_from_string(input_str) == expected_output
 
 
-def test_remove_markdown_json():
-    assert remove_markdown_json('```json\n{"key": "value"}\n```') == '{"key": "value"}'
-    assert remove_markdown_json('```json\n{"key": "value"}') == '{"key": "value"}'
-    assert remove_markdown_json('{"key": "value"}\n```') == '{"key": "value"}'
-    assert (
-        remove_markdown_json('Some text before JSON ```json\n{"key": "value"}\n``` some text after JSON')
-        == '{"key": "value"}'
-    )
-    assert remove_markdown_json('```json\n{"key": "value"\n```') == 'Invalid JSON response: {"key": "value"'
-    assert remove_markdown_json("No JSON here") == "Invalid JSON response: No JSON here"
-    assert remove_markdown_json('```jsn\n{"key": "value"}\n```') == '{"key": "value"}'
+@pytest.mark.parametrize(
+    "input_str, expected_output",
+    [
+        ('```json\n{"key": "value"}\n```', '{"key": "value"}'),
+        ('```json\n{"key": "value"}', '{"key": "value"}'),
+        ('{"key": "value"}\n```', '{"key": "value"}'),
+        ('Some text before JSON ```json\n{"key": "value"}\n``` some text after JSON', '{"key": "value"}'),
+        ('```json\n{"key": "value"\n```', 'Invalid JSON response: {"key": "value"'),
+        ("No JSON here", "Invalid JSON response: No JSON here"),
+        ('```jsn\n{"key": "value"}\n```', '{"key": "value"}'),
+    ],
+)
+def test_remove_markdown_json(input_str, expected_output):
+    assert remove_markdown_json(input_str) == expected_output
