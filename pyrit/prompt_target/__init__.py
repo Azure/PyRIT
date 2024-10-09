d899557636b627c1a1f6957963550732a3b51388
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2024-10-09 13:17:21 -0700

    merge conflict

--- pyrit/prompt_target/__init__.py
+++ pyrit/prompt_target/__init__.py
@@ -18,8 +18,6 @@ from pyrit.prompt_target.prompt_chat_target.ollama_chat_target import OllamaChat
 from pyrit.prompt_target.azure_openai_completion_target import AzureOpenAICompletionTarget
 from pyrit.prompt_target.prompt_shield_target import PromptShieldTarget
 from pyrit.prompt_target.http_target.http_target import HTTPTarget
-from pyrit.prompt_target.hugging_face_chat_target import HuggingFaceChatTarget
-from pyrit.prompt_target.hugging_face_enpoint_target import HuggingFaceEndpointTarget
 
 
 __all__ = [
