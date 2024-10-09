7cc5f32a961f079bead5657be6d4848b42b12c2a
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2024-10-08 18:03:37 -0700

    cleanup:

--- doc/code/targets/http_target.py
+++ doc/code/targets/http_target.py
@@ -27,7 +27,6 @@ from pyrit.orchestrator import PromptSendingOrchestrator, RedTeamingOrchestrator
 from pyrit.prompt_target import AzureMLChatTarget, AzureOpenAIGPT4OChatTarget, HTTPTarget
 from pyrit.prompt_target.http_target.http_target import parse_json_factory, parse_using_regex_text_factory
 from pyrit.score import SelfAskTrueFalseScorer
-
 # -
 
 # ## AOAI Example
@@ -35,9 +34,9 @@ from pyrit.score import SelfAskTrueFalseScorer
 # +
 default_values.load_default_env()
 
-deployment_name = os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT")
-endpoint = os.environ.get("AZURE_OPENAI_CHAT_ENDPOINT")
-api_key = os.environ.get("AZURE_OPENAI_CHAT_KEY")
+deployment_name=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT")
+endpoint=os.environ.get("AZURE_OPENAI_CHAT_ENDPOINT")
+api_key=os.environ.get("AZURE_OPENAI_CHAT_KEY")
 
 url = f"{endpoint}openai/deployments/{deployment_name}/chat/completions?api-version=2024-02-01"
 
@@ -62,9 +61,7 @@ raw_http_request = f"""
 
 # For AOAI the response content is located in the path choices[0].message.content - for other responses this should be in the documentation or you can manually test the output to find the right path
 parsing_function = parse_json_factory(key="choices[0].message.content")
-http_prompt_target = HTTPTarget(
-    http_request=raw_http_request, prompt_regex_string="{PROMPT}", callback_function=parsing_function
-)
+http_prompt_target = HTTPTarget(http_request=raw_http_request, prompt_regex_string="{PROMPT}", callback_function=parsing_function)
 
 with PromptSendingOrchestrator(prompt_target=http_prompt_target) as orchestrator:
     response = await orchestrator.send_prompts_async(prompt_list=[prompt])  # type: ignore
@@ -97,9 +94,7 @@ scorer = SelfAskTrueFalseScorer(
 )
 
 # GPT 3.5 target, this is what we're testing the security of
-http_prompt_target = HTTPTarget(
-    http_request=raw_http_request, prompt_regex_string="{PROMPT}", callback_function=parsing_function
-)
+http_prompt_target = HTTPTarget(http_request=raw_http_request, prompt_regex_string="{PROMPT}", callback_function=parsing_function)
 
 
 with RedTeamingOrchestrator(
@@ -111,7 +106,7 @@ with RedTeamingOrchestrator(
     verbose=True,
 ) as red_teaming_orchestrator:
     score = await red_teaming_orchestrator.apply_attack_strategy_until_completion_async(max_turns=3)  # type: ignore
-    await red_teaming_orchestrator.print_conversation()  # type: ignore
+    await red_teaming_orchestrator.print_conversation() # type: ignore
 # -
 
 # ## BIC Example
@@ -124,7 +119,6 @@ with RedTeamingOrchestrator(
 http_req = """
 POST /images/create?q={PROMPT}&rt=4&FORM=GENCRE HTTP/2
 Host: www.bing.com
-Cookie: MUID=31634C83B1176E602EB1587CB0A46FD2; _EDGE_V=1; MUIDB=31634C83B1176E602EB1587CB0A46FD2; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=5B1DFCD14D1A48B6B13907C3889689B7&dmnchg=1; SRCHHPGUSR=SRCHLANG=en; ANON=A=2CC18B9928FACFB213E2035EFFFFFFFF&E=1e43&W=1; NAP=V=1.9&E=1de9&C=E_w4lrVceUz_hrkLtRO_E2WgtHJf_n4EpuWayB0LaCtRBKHWrPq8SA&W=1; PPLState=1; MMCASM=ID=C9A596F60B4C410E93060321675E1C35; GI_FRE_COOKIE=gi_prompt=5; _SS=SID=2C7B1AA8323C6B8E19280FAE33B46A73&R=205&RB=205&GB=0&RG=0&RP=205; CSRFCookie=953f305a-0247-470b-816f-1bbd3d027da9; _EDGE_S=SID=1EC549A55334658A16D95CB552EB64FB; KievRPSSecAuth=FACKBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACOZTvtQ3aSWnSAS95a7wt5W0LmH1K2lp8bTCQNyQl6R5IBK6d1QcZ6uWhCFkeCOqqg9fAGscRjVrDcCJR/Acd7ZKN0bALpbMVJjfG+Z2LCtcAmSADXK/O61omrVWuohDkETOUi/yJ4qlWmbdmPdl2ICll+Xxpcv5tyKf5jn3lQzZulqP9BGjn9CfqvM6qD0xUa0MwHek+A4UN55GOTGvNQTqYCUXOw9l7ItkXszTwLoKZo2BUTPuzv73iOiYzGfKWcGteRBAeT38u8SS31nRjf6LyMmTBfh/ItAWc8yYp2zdLuzdfZYq5bkwrrgpFeE9O5HCxfVnxCY8ZpDudFatKB66It9dSDAoa1iMugx9mXERyRJe9nLOoKIQyv/kDfBGkZ+RLmdEmSEb+nv57nCED+pAARuwtQr3dXjatmCYCcR5RmMmzfEb0WvxcATUzQRoIAXW1X0bwtOx7OVC3pnJ3XFc3a83GwgEhx6mtLqejKQwu+RS+ZTqN54wYECxcezNN3p3Br/F829fbkR1J2hoRTOksLsLyE+sn5P+fJXQbbrH3DKNxELYyvmrg5ngJr7/gKzlub0qIqmtfHJsM93KWJxay10JusVngwSut5fDVUXrdb0HAhNHvIERhg8Rx9j+dYTfPyBWsHIRDzykslGlQCx1dWtGiJ+GaYpuOicBmsBGvjNgT/H+J1cz41SLxviu5fB6tl79gBqbsFATQqg7jVT4ibplKKEhKulVRwQeAtRMDqDSQjU72NS5lQYdtNzTH/AcBlKWi+olExzK+N7/fsDITS8gC3ocGK22Rq6mxwTF0r/ZA9Yi7BP9DFFovVIPjWrCkzp4SubS4zGtmp5p8a6dDz0OaYHeDH+4NYCH0Qd95hys89eSMIpC9iHgymlZExSQ7sKQfwOoFUAtKSoNf2/LDe1kFo6co7WHqYZLH3cbe9vL/lwpBWe2A7iuOvCsxwYCPZRL6FWgPBXztfy8KjpvAkzrhE8AOCdAwZfweQjCmsm3BOmQA9A2zM0rMSTtqh7mgjxgTmQs1IjoKp8hNxUgnYkXykY0jHFTrNy/bumOUDpE5rl0L9e3poNOl0wqz8rgeRxkO1Wrwoz1ODpGHHByWJY9/JoUG/sApaAj/gB95RwX4lR8rWQZ6WsLet3Co031AljpKPs634XLpIhjZtxgMnpNlkc+6q044DvV1DKJh26D7NEBu7cLgoi7LsHhMAxbINa720fFOMuxkpUUHzucD8GxNopJzB3SO4XP/abRcerg1S6vRDtQe+ynSCTzUqudm5wlHfvAdt+Kt0+80Vg7V+b+1AsmBH0RLhDXi3qVy58qqrpD2aBT7Fv4Xo9UyN9xnzJsGpROas2gmEKbYQq3RHGQltJtTGwGgMUThIg8R4yCu85ZGL6IBJWxtdQ2p8tisFt4WFSW69bN84lkcz7AwNkm2EywTrd/E969jKulpqze9o4KpJSeNwNRnt8eauEVFACJroIhtQ9AUjlrOCueFnD0Z8/qfw==; _U=1D4JISmO-kcaAE2G7FOagN2ypJu5krNZftJRvLyeMufInFy2Vs1HK_RWydUtw2M4DdgrRHprcJNQ5jWp4tK8njicqNRentTizhbGnmh0PorCF_EmNb1iW1KRXr_VfXyUdP_iZ2CTzvOO4gmOnbppVBbKylwrOC17lRuMU8bE5g9i4ddlpGv9Idrgw7lZp-xm13RoFhOcURO9-P9aRbxzcYlh3v1Vpit2V-03dR_X_1vk; WLS=C=0f3786b79f759f46&N=airt; WLID=XJI9SjVKm90FCcWcw2eolskaT5pUOO6bv4bYfTo2UQV/J/FBJNVq2ir1him+z1mFTsUbk63YZ9n9pf9YmpuHJQ7eX2ZXbKA+GNoB2e11hWo=; SRCHUSR=DOB=20240919&TPC=1728175447000&T=1728175537000&POEX=W; _Rwho=u=d&ts=2024-10-06; _C_ETH=1; _RwBf=mta=0&rc=205&rb=205&gb=0&rg=0&pc=205&mtu=0&rbb=0.0&g=0&cid=&clo=0&v=3&l=2024-10-05T07:00:00.0000000Z&lft=0001-01-01T00:00:00.0000000&aof=0&ard=0001-01-01T00:00:00.0000000&rwdbt=-62135539200&rwflt=-62135539200&rwaul2=0&o=0&p=MSAAUTOENROLL&c=MR000T&t=5584&s=2024-09-12T20:53:07.8976531+00:00&ts=2024-10-06T00:47:43.8859397+00:00&rwred=0&wls=2&wlb=0&wle=0&ccp=2&cpt=0&lka=0&lkt=0&aad=0&TH=&e=bS6vlg7dmNtJRcHc7-5tB1yd39S0awMGvVYLsvKGy6IJhBGpY15h8YoL1dnRu3vNqwf-2qgeJNhe2x-psguLEA&A=2CC18B9928FACFB213E2035EFFFFFFFF; _clck=50a90i%7C2%7Cfpu%7C0%7C1723; _clsk=1wuqqbn%7C1728425964118%7C1%7C0%7Co.clarity.ms%2Fcollect
 Content-Length: 13
 Cache-Control: max-age=0
 Ect: 4g
@@ -159,13 +153,9 @@ q={PROMPT}&qs=ds
 ## Add the prompt you want to send to the URL
 prompt = "clarinet"
 
-parsing_function = parse_using_regex_text_factory(
-    key=r'\/images\/create\/async\/results\/[^\s"]+', url="https://www.bing.com"
-)
-http_prompt_target = HTTPTarget(
-    http_request=http_req, prompt_regex_string="{PROMPT}", callback_function=parsing_function
-)
+parsing_function = parse_using_regex_text_factory(key = r'\/images\/create\/async\/results\/[^\s"]+', url = "https://www.bing.com")
+http_prompt_target = HTTPTarget(http_request=http_req, prompt_regex_string="{PROMPT}", callback_function=parsing_function)
 
 with PromptSendingOrchestrator(prompt_target=http_prompt_target) as orchestrator:
     response = await orchestrator.send_prompts_async(prompt_list=[prompt])  # type: ignore
-    await orchestrator.print_conversations()  # This is the link that holds the image generated by the prompt - would need to download and save like in DALLE target
+    await orchestrator.print_conversations() # This is the link that holds the image generated by the prompt - would need to download and save like in DALLE target
