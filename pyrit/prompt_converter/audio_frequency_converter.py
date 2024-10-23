43ba50a9b1a93bda337bbd07b9f20b31ee54ad2f
michellemorales <ms.morales3@gmail.com>
2024-10-23 10:53:44 -0400

    Work in progress - change read/write to leveraging data serializer

--- pyrit/prompt_converter/audio_frequency_converter.py
+++ pyrit/prompt_converter/audio_frequency_converter.py
@@ -4,12 +4,14 @@
 import logging
 from typing import Literal, Optional
 
+import io
 import numpy as np
 from scipy.io import wavfile
 
 
 
 from pyrit.models import PromptDataType
+from pyrit.models.data_type_serializer import data_serializer_factory
 from pyrit.prompt_converter import PromptConverter,  ConverterResult
 from pyrit.memory import MemoryInterface, DuckDBMemory
 
@@ -25,28 +27,42 @@ class AudioFrequencyConverter(PromptConverter):
         audio_format (str): The format of the audio file. Defaults to "wav".
     """
     AcceptedAudioFormats = Literal["wav"]
-    def __init__(self, *, audio_format: AcceptedAudioFormats = "wav") -> None:
-        self._audio_format = audio_format
-
+    def __init__(self, *,  output_format: AcceptedAudioFormats = "wav", memory: Optional[MemoryInterface] = None) -> None:
+        self._output_format = output_format
+        self._memory = memory or DuckDBMemory()
 
     def input_supported(self, input_type: PromptDataType) -> bool:
         return input_type == "audio_path"
+
 
     async def convert_async(self, *, prompt: str, input_type: PromptDataType = "audio_path", shift_value: int) -> ConverterResult:
         if not self.input_supported(input_type):
             raise ValueError("Input type not supported")
         try:
-            sample_rate, data = wavfile.read(prompt)
+            audio_serializer = data_serializer_factory(data_type="audio_path", extension=self._output_format, value=prompt, memory=self._memory)
+            audio_bytes = await audio_serializer.read_data()
+            bytes_io = io.BytesIO(audio_bytes)
+            sample_rate, data = wavfile.read(bytes_io)
             shifted_data = data * np.exp(1j * 2 * np.pi * shift_value * np.arange(len(data)) / sample_rate)
-            converted_file_path = prompt.replace('.wav', '_converted.wav')
-            wavfile.write(converted_file_path, sample_rate, shifted_data.real.astype(np.int16))
+            # Convert the real part of the shifted data to int16
+            shifted_data_int16 = shifted_data.real.astype(np.int16)
+
+            # Write the NumPy array to the in-memory buffer as a WAV file
+            wavfile.write(bytes_io, sample_rate, shifted_data_int16)
+
+            # Retrieve the WAV bytes
+            converted_bytes = bytes_io.getvalue() 
+
+            # Use data serializer to write bytes to wav file
+            await audio_serializer.save_data(data=converted_bytes)
+            audio_serializer_file = str(audio_serializer.value)
             logger.info(
                     "Speech synthesized for text [{}], and the audio was saved to [{}]".format(
-                        prompt, converted_file_path
+                        prompt, audio_serializer_file
                     )
                 )
 
         except Exception as e:
             logger.error("Failed to convert prompt to audio: %s", str(e))
             raise
-        return ConverterResult(output_text=converted_file_path, output_type=input_type)
+        return ConverterResult(output_text=audio_serializer_file, output_type=input_type)
