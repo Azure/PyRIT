e060818fe06694e43620be1d92a412620a4f18ab
rdheekonda <rdheekonda@microsoft.com>
2024-10-27 18:20:45 -0700

    Adding demo notebooks and sample multimodal seed prompt yaml file

--- doc/code/memory/8_seed_prompt_database.py
+++ doc/code/memory/8_seed_prompt_database.py
@@ -6,11 +6,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.3
+#       jupytext_version: 1.16.2
 #   kernelspec:
-#     display_name: pyrit-kernel
+#     display_name: pyrit-dev
 #     language: python
-#     name: pyrit-kernel
+#     name: python3
 # ---
 
 # %% [markdown]
@@ -41,7 +41,7 @@ from pyrit.models import SeedPromptDataset
 from pyrit.common.path import DATASETS_PATH
 import pathlib
 
-seed_prompt_dataset = SeedPromptDataset.from_yaml_file(pathlib.Path(DATASETS_PATH) / "prompts" / "illegal.prompt")
+seed_prompt_dataset = SeedPromptDataset.from_yaml_file(pathlib.Path(DATASETS_PATH) / "seed_prompts" / "illegal.prompt")
 
 print(seed_prompt_dataset.prompts[0])
 
@@ -60,7 +60,28 @@ azure_memory.get_seed_prompt_dataset_names()
 # To get all seed prompts from that dataset, we can query as follows:
 
 # %%
-prompts = azure_memory.get_seed_prompts(dataset_name="test illegal")
+dataset_name = "test illegal"
+prompts = azure_memory.get_seed_prompts(dataset_name=dataset_name)
+print(f"Total number of the prompts with dataset name '{dataset_name}':", len(prompts))
 print(prompts[0].__dict__)
+
+# %% [markdown]
+# # Adding seed prompt groups to the database
+# %%
+from pyrit.models import SeedPromptGroup
+
+seed_prompt_group = SeedPromptGroup.from_yaml_file(pathlib.Path(DATASETS_PATH) / "seed_prompts" / "illegal-multimodal.prompt")
+
+azure_memory.add_seed_prompt_groups_to_memory(prompt_groups=[seed_prompt_group], added_by="test multimodal illegal")
+
+# %% [markdown]
+# # Retrieving seed prompt groups from the memory with dataset_name as "test multimodal"
+
+# %%
+
+multimodal_dataset_name = "test multimodal"
+seed_prompt_groups = azure_memory.get_seed_prompt_groups(dataset_name=multimodal_dataset_name)
+print(f"Total number of the seed prompt groups with dataset name '{multimodal_dataset_name}':", len(seed_prompt_groups))
+print(seed_prompt_groups[0].__dict__)
 
 # %%
