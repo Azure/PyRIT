bca60c15405d2e885bb2c45f4a634e762607a872
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2024-11-05 15:15:45 -0800

    precommit

--- pyrit/score/human_in_the_loop_scorer.py
+++ pyrit/score/human_in_the_loop_scorer.py
@@ -173,6 +173,9 @@ class HumanInTheLoopScorer(Scorer):
 
             elif user_input == "3":
                 return await self.rescore(request_response, task=task)
+            
+            else:
+                raise ValueError("Invalid input. Please enter '1', '2', or '3'.")
 
     def _get_user_input(self, message) -> str:
         root = tk.Tk()
