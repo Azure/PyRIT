slight clean up, removing inheritance from PAIROrchestrator, correcting logprobs exposed (not toplogprobs)

pyrit/orchestrator/simple_adaptive_orchestrator.py
@@ -7,7 +7,7 @@ import string
 import numpy as np
 import random
 from collections import defaultdict
-from typing import Optional
+from typing import Optional, Dict
 
 from tqdm.auto import tqdm
 
@@ -24,7 +24,7 @@ from pyrit.score import Scorer
 logger = logging.getLogger(__name__)
 
 
-class SimpleAdaptiveOrchestrator(PAIROrchestrator):
+class SimpleAdaptiveOrchestrator(Orchestrator):
     """
     This orchestrator implements the Prompt Automatic Iterative Refinement (PAIR) algorithm
 
@@ -58,32 +58,40 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
         n_chars_change_max: int = 1,
         number_of_iterations: int = 10,
         number_of_final_retries: int = 1,
-
+        number_of_optimized_tokens: int = 2,
     ) -> None:
         """
         Initializes the orchestrator.
         """
-        assert isinstance(red_teaming_chat, OpenAIChatTarget), "SimpleAdaptiveOrchestrator only supports OpenAIChatTarget for now."
         super().__init__(
-            memory = memory,
-            memory_labels = memory_labels,
-            verbose = verbose,
-            prompt_target = prompt_target,
-            desired_target_response_prefix = desired_target_response_prefix,
-            red_teaming_chat = red_teaming_chat,
-            conversation_objective = conversation_objective,
-            number_of_conversation_streams = number_of_conversation_streams,
-            max_conversation_depth = max_conversation_depth,
-            stop_on_first_success = stop_on_first_success,
-            scorer = scorer,
-            scorer_sensitivity = scorer_sensitivity,
-            prompt_converters = prompt_converters,
-            single_turn_jailbreak_only = single_turn_jailbreak_only
+            memory=memory, memory_labels=memory_labels, verbose=verbose, prompt_converters=prompt_converters
         )
+
+        assert isinstance(prompt_target, OpenAIChatTarget), "SimpleAdaptiveOrchestrator only supports OpenAIChatTarget for now."
+        
+        self.successful_jailbreaks: list[PromptRequestResponse] = []
+        self._prompt_target = prompt_target
+        self._adversarial_target = red_teaming_chat
+        self._max_conversation_depth = max_conversation_depth
+        self._stop_on_first_success = stop_on_first_success
+        self._conversation_objective = conversation_objective
+        self._number_of_conversation_streams = number_of_conversation_streams
+        self._last_attacker_conversation_id = ""
+        self._prompt_normalizer = PromptNormalizer(memory=self._memory)
+        self._single_turn_jailbreak_only = single_turn_jailbreak_only
+        self._scorer_sensitivity = scorer_sensitivity
+        self._scorer = scorer
+        # Set the scorer and scorer._prompt_target memory to match the orchestrator's memory.
+        if self._scorer:
+            self._scorer._memory = self._memory
+            if hasattr(self._scorer, "_prompt_target"):
+                self._scorer._prompt_target._memory = self._memory
+        self._desired_target_response_prefix = desired_target_response_prefix
 
         self._max_n_tokens = self._prompt_target._max_tokens # keep track since we will be changing this
         self._number_of_iterations = number_of_iterations
         self._n_chars_change_max = n_chars_change_max
+        self._number_of_optimized_tokens = number_of_optimized_tokens
         
         # Set to 1 if single_turn_jailbreak_only is True
         if single_turn_jailbreak_only and number_of_final_retries != 1:
@@ -100,8 +108,15 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
 
         self._adversarial_suffix = self._get_adversarial_suffix()
         self._best_adversarial_suffix = self._adversarial_suffix
-        self._logprobs_dict = {}
+        self._best_logprobs_dict = {}
         self._best_logprobs = -np.inf
+    
+    def _validate(self) -> None:
+        if self._scorer_sensitivity < 0 or self._scorer_sensitivity > 1:
+            raise ValueError("scorer_sensitivity must be between 0 and 1")
+
+        if self._scorer.scorer_type != "float_scale":
+            raise ValueError("PAIR orchestrator requires a scorer with a 'float_scale' scorer_type")
     
     def _get_adversarial_suffix(self) -> str:
         """
@@ -116,54 +131,87 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
             )
         # TODO add more target types here (r2d2, various Llama models, Gemma, Mistral etc.)
         else:
-            raise NotImplementedError(f"Adversarial suffix not implemented for target type {type(self._adversarial_target)}")
+            raise NotImplementedError(f"Adversarial suffix not implemented for target type {type(self._prompt_target)}")
 
 
-    @pyrit_json_retry
-    async def _get_attacker_response_and_store(
-        self, *, target_response: str, start_new_conversation: bool = False
-    ) -> str:
-        """
-        Generates an attacker response based on the target response.
+    # @pyrit_json_retry
+    # async def _get_attacker_response_and_store(
+    #     self, *, target_response: str, start_new_conversation: bool = False
+    # ) -> str:
+    #     """
+    #     Generates an attacker response based on the target response.
 
+    #     Args:
+    #         target_response: The response from the target.
+    #         start_new_conversation: Whether to start a new conversation with the attacker. Defaults to False.
+
+    #     Returns:
+    #         The attacker response.
+    #     """
+    #     # Initialise prompt from template to feed to the target
+        
+        
+    #     if start_new_conversation:
+    #         self._last_attacker_conversation_id = str(uuid.uuid4())
+    #         attacker_system_prompt = self._attacker_prompt_template.render_template_value(
+    #             goal=self._conversation_objective,
+    #             target_str=self._desired_target_response_prefix,
+    #             adv_suffix=self._adversarial_suffix
+    #         )
+    #         self._adversarial_target.set_system_prompt(
+    #             system_prompt=attacker_system_prompt, conversation_id=self._last_attacker_conversation_id
+    #         )
+
+    #     attacker_response = await self._prompt_normalizer.send_prompt_async(
+    #         normalizer_request=NormalizerRequest(
+    #             request_pieces=[
+    #                 NormalizerRequestPiece(
+    #                     request_converters=self._prompt_converters,
+    #                     prompt_value=target_response,
+    #                     prompt_data_type="text",
+    #                     memory=self._memory,
+    #                 )
+    #             ],
+    #             conversation_id=self._last_attacker_conversation_id,
+    #         ),
+    #         target=self._prompt_target,
+    #         labels=self._global_memory_labels,
+    #         orchestrator_identifier=self.get_identifier(),
+    #     )
+
+    #     return self._parse_attacker_response(response=attacker_response)
+
+    async def _get_target_response_and_store(
+        self, *, text: str, conversation_id: Optional[str] = None
+    ) -> PromptRequestResponse:
+        """Interact with the target in a zero-shot fashion.
+
+        A new UUID will be generated for each interaction with the target. This zero-shot interation is how the PAIR
+        algorithm is specified
         Args:
-            target_response: The response from the target.
-            start_new_conversation: Whether to start a new conversation with the attacker. Defaults to False.
+            text: prompt to send to target
 
         Returns:
-            The attacker response.
+            response from target.
         """
-        if start_new_conversation:
-            self._last_attacker_conversation_id = str(uuid.uuid4())
-            attacker_system_prompt = self._attacker_prompt_template.render_template_value(
-                goal=self._conversation_objective,
-                target_str=self._desired_target_response_prefix,
-                adv_suffix=self._adversarial_suffix
-            )
-            self._adversarial_target.set_system_prompt(
-                system_prompt=attacker_system_prompt, conversation_id=self._last_attacker_conversation_id
-            )
-
-        attacker_response = await self._prompt_normalizer.send_prompt_async(
+        curr_conversation_id = conversation_id or str(uuid.uuid4())
+        target_response = await self._prompt_normalizer.send_prompt_async(
             normalizer_request=NormalizerRequest(
                 request_pieces=[
                     NormalizerRequestPiece(
                         request_converters=self._prompt_converters,
-                        prompt_value=target_response,
+                        prompt_value=text,
                         prompt_data_type="text",
                         memory=self._memory,
                     )
                 ],
-                conversation_id=self._last_attacker_conversation_id,
+                conversation_id=curr_conversation_id,
             ),
             target=self._prompt_target,
             labels=self._global_memory_labels,
             orchestrator_identifier=self.get_identifier(),
         )
-        attacker_response
-
-        breakpoint()
-        return self._parse_attacker_response(response=attacker_response)
+        return target_response
 
 
     async def _process_conversation_stream(self, *, index: int) -> list[PromptRequestResponse]:
@@ -171,56 +219,60 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
         Processes a single conversation stream in the Simple Adaptive algorithm.
         """
         jailbreaks: list[PromptRequestResponse] = []
-        prompt_to_improve_upon = "Hello world!"
         t = tqdm(range(self._number_of_iterations), disable=not self._verbose, leave=False, desc="Iterations for RS")
         conversation_id = str(uuid.uuid4())
+        
         # Predict single token for each iteration in RS
-        self._prompt_target.max_tokens = 1
+        self._prompt_target.max_tokens = self._number_of_optimized_tokens
+        
         for iteration in t:
-            try:
-                attacker_prompt_suggestion = await self._get_attacker_response_and_store(
-                    target_response=prompt_to_improve_upon, start_new_conversation=(iteration == 0)
-                )
-            except InvalidJsonException:
-                logger.warn("Invalid JSON response from attacker despite retries.")
-                break
+            attacker_prompt_suggestion = self._attacker_prompt_template.render_template_value(
+                goal=self._conversation_objective,
+                target_str=self._desired_target_response_prefix,
+                adv_suffix=self._adversarial_suffix
+            )
+
             if self._single_turn_jailbreak_only:
                 # Create a new conversation ID for each turn
                 conversation_id = str(uuid.uuid4())
             
-            breakpoint()
-
             target_response = await self._get_target_response_and_store(
                 text=attacker_prompt_suggestion,
                 conversation_id=conversation_id,
             )
 
             logprob_dict = target_response.request_pieces[0].logprobs
-            breakpoint()
+            message = target_response.request_pieces[0].converted_value
 
-            if self._update_improved_logprobs(logprobs=logprob_dict):
-                logger.info(f"Improved logprobs: {self._best_logprobs}")
-                self._best_adversarial_suffix = self._adversarial_suffix
+            if self._desired_target_response_prefix in message[:len(self._desired_target_response_prefix)]:
+                if sum(logprob_dict.values()) > self._best_logprobs:
+                    self._best_logprobs_dict = logprob_dict
+                    self._best_adversarial_suffix = self._adversarial_suffix
+            elif " " + self._desired_target_response_prefix in message:
+                if sum(logprob_dict.values()) > self._best_logprobs:
+                    self._best_logprobs_dict = logprob_dict
+                    self._best_adversarial_suffix = self._adversarial_suffix
             
             # Randomly change the adversarial suffix
             self._adversarial_suffix = self._adjust_adversarial_suffix()
 
         # If no improvement report failure
         if self._best_logprobs == -np.inf:
+            breakpoint()
             logger.info(f"No improvement in logprobs after {self._number_of_iterations} iterations.")
             return jailbreaks
         
         # Set the max tokens back to the original value and generate output
         self._prompt_target.max_tokens = self._max_n_tokens
         t = tqdm(range(self._number_of_final_retries), disable=not self._verbose, leave=False, desc="Retries for final output")
+        
         for retry in t:
-            try:
-                attacker_prompt_suggestion = await self._get_attacker_response_and_store(
-                    target_response=prompt_to_improve_upon, start_new_conversation=True
-                )
-            except InvalidJsonException:
-                logger.warn("Invalid JSON response from attacker despite retries.")
-                break
+            attacker_prompt_suggestion = self._attacker_prompt_template.render_template_value(
+                goal=self._conversation_objective,
+                target_str=self._desired_target_response_prefix,
+                adv_suffix=self._adversarial_suffix
+            )
+
             if self._single_turn_jailbreak_only:
                 # Create a new conversation ID for each turn
                 conversation_id = str(uuid.uuid4())
@@ -239,37 +291,15 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
             if self._is_jailbreak_successful(score=score):
                 jailbreaks.append(target_response)
                 break
-
-            prompt_to_improve_upon = target_response.request_pieces[0].converted_value
         
         return jailbreaks
-
-    
-    def _update_improved_logprobs(self, *, logprobs: dict) -> bool:
-        """
-        Determines if the logprobs have improved.
-        """
-        if not logprobs:
-            return False
-        # check if target_token is in logprobs
-        elif self._desired_target_response_prefix in logprobs:
-            # return True if the logprob is greater than the previous logprob
-            target_logprob = logprobs[self._desired_target_response_prefix]
-            if target_logprob > self._best_logprobs:
-                self._best_logprobs = target_logprob
-                return True
-        elif " " + self._desired_target_response_prefix in logprobs:
-            target_logprob = logprobs[" " + self._desired_target_response_prefix]
-            if target_logprob > self._best_logprobs:
-                self._best_logprobs = target_logprob
-                return True
-        return False
     
 
     def _adjust_adversarial_suffix(self) -> str:
         """
         Randomly changes the adversarial suffix.
         """
+        adv_suffix = self._adversarial_suffix
         n_chars_change = self._n_chars_change_max
         substitution_set = string.digits + string.ascii_letters + string.punctuation + ' '
         substitute_pos_start = random.choice(range(len(self._adversarial_suffix)))
@@ -291,14 +321,7 @@ class SimpleAdaptiveOrchestrator(PAIROrchestrator):
         Runs the PAIR algorithm against the target and attacker.
         """
 
-        prompt_response_pair_for_jailbreaks: list[PromptRequestResponse] = []
+        new_jailbreaks = await self._process_conversation_stream(index=0)
 
-        t = tqdm(range(self._number_of_conversation_streams), disable=not self._verbose, desc="Stream")
-        for conversation_stream_index in t:
-            if self._should_stop(prompt_responses=prompt_response_pair_for_jailbreaks):
-                break
-            new_jailbreaks = await self._process_conversation_stream(index=conversation_stream_index)
-            prompt_response_pair_for_jailbreaks += new_jailbreaks
-
-        self.successful_jailbreaks.extend(prompt_response_pair_for_jailbreaks)
-        return prompt_response_pair_for_jailbreaks
+        self.successful_jailbreaks.append(new_jailbreaks)
+        return new_jailbreaks