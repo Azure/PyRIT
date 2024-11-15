8d96c56e6bdfeda6049243c96347bd51b9d84362
rdheekonda <rdheekonda@microsoft.com>
2024-11-14 16:25:23 -0800

    Fix invalid keyword arg top TAP orchestartor

--- pyrit/orchestrator/tree_of_attacks_with_pruning_orchestrator.py
+++ pyrit/orchestrator/tree_of_attacks_with_pruning_orchestrator.py
@@ -362,8 +362,7 @@ class TreeOfAttacksWithPruningOrchestrator(Orchestrator):
 
                         cloned_orchestrator._memory.duplicate_conversation_for_new_orchestrator(
                             new_orchestrator_id=cloned_orchestrator.get_identifier()["id"],
-                            conversation_id=orchestrator._red_teaming_chat_conversation_id,
-                            new_conversation_id=cloned_orchestrator._red_teaming_chat_conversation_id,
+                            conversation_id=orchestrator._red_teaming_chat_conversation_id
                         )
                         cloned_orchestrators.append(cloned_orchestrator)
 
