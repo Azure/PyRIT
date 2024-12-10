c651c80b912fe94f75412aa232d6cfa2fc72003f
rlundeen2 <rlundeen@microsoft.com>
2024-12-10 11:30:13 -0800

    fixing test

--- tests/test_prompt_request_piece.py
+++ tests/test_prompt_request_piece.py
@@ -100,7 +100,7 @@ def test_orchestrators_serialize(mock_memory_instance):
 
     assert entry.orchestrator_identifier["id"] is not None
     assert entry.orchestrator_identifier["__type__"] == "PromptSendingOrchestrator"
-    assert entry.orchestrator_identifier["__module__"] == "pyrit.orchestrator.prompt_sending_orchestrator"
+    assert entry.orchestrator_identifier["__module__"] == "pyrit.orchestrator.single_turn.prompt_sending_orchestrator"
 
 
 @pytest.mark.asyncio
