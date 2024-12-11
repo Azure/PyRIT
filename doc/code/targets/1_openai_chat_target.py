6ecb39e7b16457366dc170ae0e8301e81941145a
rlundeen2 <rlundeen@microsoft.com>
2024-12-11 11:24:49 -0800

    Clarifying OpenAI docs

--- doc/code/targets/1_openai_chat_target.py
+++ doc/code/targets/1_openai_chat_target.py
@@ -13,7 +13,7 @@
 # ---
 
 # %% [markdown]
-# # 1. Azure OpenAI Chat Target
+# # 1. OpenAI Chat Target
 #
 # In this demo, we show an example of the `OpenAIChatTarget`, which includes many openAI models including `gpt-4o`, `gpt-4`, and `gpt-3.5`. Internally, this is one of our most-used chat targets for our own infrastructure.
 #
@@ -49,9 +49,13 @@ with PromptSendingOrchestrator(objective_target=target) as orchestrator:
 # %% [markdown]
 # ## OpenAI Configuration
 #
-# All `OpenAITarget`s can communicate either to [Azure OpenAI](https://learn.microsoft.com/en-us/azure/ai-services/openai/reference) or [OpenAI](https://platform.openai.com/docs/api-reference/introduction).
+# All `OpenAITarget`s can communicate either to [Azure OpenAI (AOAI)](https://learn.microsoft.com/en-us/azure/ai-services/openai/reference) or [OpenAI](https://platform.openai.com/docs/api-reference/introduction). By default, it uses Azure OpenAI (we are Microsoft funded after all!) but you can configure a target to use OpenAI by passing the `is_azure_target=False` flag.
 #
-# You can configure a target to use OpenAI by passing the `is_azure_target=False` flag. By default, `is_azure_target` is true. By default, these targets will use an API key configured within environment variables to authenticate (`OPENAI_CHAT_KEY` for OpenAI and `AZURE_OPENAI_CHAT_KEY` for Azure).
+# Like most targets, all `OpenAITarget`s need an `endpoint`, `deployment_name`, and `api_key`. These can be passed into the constructor or configured with environment variables (or in .env).
 #
-# There is an option to use the DefaultAzureCredential for User Authentication as well, for all AOAI Chat Targets. When `use_aad_auth=True`, ensure the user has 'Cognitive Service OpenAI User' role assigned on the AOAI Resource and `az login` is used to authenticate with the correct identity.
+# - api_key: By default, these targets will use an API key configured within environment variables (or .env) to authenticate (`OPENAI_CHAT_KEY` for OpenAI and `AZURE_OPENAI_CHAT_KEY` for AOAI).
+# - endpoint: AOAI needs an endpoint URI from your deployment. For OpenAI, these are just "https://api.openai.com/v1/chat/completions" and do not need to be configured by the user.
+# - deployment_name: Azure Open AI needs this from your deployment. For OpenAI, these are any available model name and are listed here: https://platform.openai.com/docs/models
+#
+# For AOAI, There is an option to use the DefaultAzureCredential for User Authentication as well, for all AOAI Chat Targets. When `use_aad_auth=True`, ensure the user has 'Cognitive Service OpenAI User' role assigned on the AOAI Resource and `az login` is used to authenticate with the correct identity.
 #
