ebfdfb4e50ff310287f28b4d1a4468474b8fa56d
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2024-12-16 08:36:48 -0800

    pre commit fix

--- pyrit/common/__init__.py
+++ pyrit/common/__init__.py
@@ -1,16 +1,17 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-def combine_dict(dict1: dict[str, str] = None, dict2: dict[str, str]= None) -> dict[str, str]:
+
+def combine_dict(dict1: dict[str, str] = None, dict2: dict[str, str] = None) -> dict[str, str]:
     """
     Combines two dictionaries containing string keys and values into one
-    Args:  
+    Args:
         dict1: Dictionary 1
         dict2: Dictionary 2
     Returns: combined dictionary
     """
     combined_dict = dict1.copy() if dict1 else {}
-    if dict2: 
+    if dict2:
         for key, value in dict2.items():
             if dict1:
                 combined_dict[key] = value
