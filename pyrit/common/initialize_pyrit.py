52815e50ddf8904a050aaa1c6450a67e6fe87a6c
Nina Chikanov <nichikan@microsoft.com>
2024-12-20 12:53:57 -0800

    fix bug

--- pyrit/common/initialize_pyrit.py
+++ pyrit/common/initialize_pyrit.py
@@ -5,7 +5,7 @@ from enum import Enum
 import logging
 
 from pyrit.common import default_values
-from pyrit.memory import AzureSQLMemory, CentralMemory, DuckDBMemory, MemoryInterface
+from pyrit.memory import AzureSQLMemory, CentralMemory, DuckDBMemory
 
 
 logger = logging.getLogger(__name__)
@@ -15,12 +15,12 @@ class MemoryInstance(Enum):
     """
     An enumeration of the available memory instances for PyRIT.
     """
-    IN_MEMORY = DuckDBMemory(db_path=":memory:")
-    ON_DISK = DuckDBMemory()
-    AZURE_SQL = AzureSQLMemory()
+    IN_MEMORY = 0
+    ON_DISK = 1
+    AZURE_SQL = 2
 
 
-def initialize_pyrit(memory_instance: MemoryInterface) -> None:
+def initialize_pyrit(memory_instance: MemoryInstance) -> None:
     """
     Initializes PyRIT with the provided memory instance and loads environment files.
 
@@ -29,4 +29,11 @@ def initialize_pyrit(memory_instance: MemoryInterface) -> None:
     """
     default_values.load_environment_files()
 
-    CentralMemory.set_memory_instance(memory_instance.value)
+    if memory_instance == MemoryInstance.IN_MEMORY:
+        memory = DuckDBMemory(db_path=":memory:")
+    elif memory_instance == MemoryInstance.ON_DISK:
+        memory = DuckDBMemory(db_path=None)
+    else:
+        memory = AzureSQLMemory()
+
+    CentralMemory.set_memory_instance(memory)
