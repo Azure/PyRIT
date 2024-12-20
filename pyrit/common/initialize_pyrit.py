c37ab97a6be78fd7ba7e072be1c58da3e4ab2c2c
Nina Chikanov <nichikan@microsoft.com>
2024-12-20 15:33:41 -0800

    pre-commit hooks

--- pyrit/common/initialize_pyrit.py
+++ pyrit/common/initialize_pyrit.py
@@ -5,7 +5,7 @@ import logging
 from typing import Literal, get_args
 
 from pyrit.common import default_values
-from pyrit.memory import AzureSQLMemory, CentralMemory, DuckDBMemory
+from pyrit.memory import AzureSQLMemory, CentralMemory, DuckDBMemory, MemoryInterface
 
 
 logger = logging.getLogger(__name__)
@@ -21,16 +21,18 @@ def initialize_pyrit(memory_db_type: MemoryDatabaseType) -> None:
         memory (MemoryInterface): The memory instance to use for PyRIT.
     """
     if memory_db_type not in get_args(MemoryDatabaseType):
-            raise ValueError(f"Memory database type '{memory_db_type}' is not a supported type {get_args(MemoryDatabaseType)}")
+        raise ValueError(
+            f"Memory database type '{memory_db_type}' is not a supported type {get_args(MemoryDatabaseType)}"
+        )
 
     default_values.load_environment_files()
 
+    memory: MemoryInterface = None
     if memory_db_type == "InMemory":
         memory = DuckDBMemory(db_path=":memory:")
     elif memory_db_type == "DuckDB":
         memory = DuckDBMemory(db_path=None)
     else:
-        # AzureSQL
         memory = AzureSQLMemory()
 
     CentralMemory.set_memory_instance(memory)
