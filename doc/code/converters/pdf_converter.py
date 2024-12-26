6863dedb0d41c44667eaafb774bbeb0982d2a14a
jsong468 <songjustin@microsoft.com>
2024-12-26 11:31:26 -0800

    fix heading in pdf nb for build error

--- doc/code/converters/pdf_converter.py
+++ doc/code/converters/pdf_converter.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# **PDF Converter with Multiple Modes**
+# # PDF Converter with Multiple Modes:
 #
 # This script demonstrates the use of the `PDFConverter` for generating PDFs in two different modes:
 #
