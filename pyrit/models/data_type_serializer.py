0c990718c6ec995269401d02549799b968caf75e
Raja Sekhar Rao Dheekonda <43563047+rdheekonda@users.noreply.github.com>
2025-02-12 13:19:12 -0800

    Update pyrit/models/data_type_serializer.py
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

--- pyrit/models/data_type_serializer.py
+++ pyrit/models/data_type_serializer.py
@@ -114,7 +114,7 @@ class DataTypeSerializer(abc.ABC):
         """
         if self._is_azure_storage_url(self.value):
             # Scenarios where a user utilizes an in-memory DuckDB but also needs to interact
-            # with an Azure Storage Account, ex., XPiAOrchestrator.
+            # with an Azure Storage Account, ex., XPIAOrchestrator.
             from pyrit.memory import AzureSQLMemory, CentralMemory
 
             CentralMemory.set_memory_instance(AzureSQLMemory())
