7a6eb87d0d47292f8a8d2732223d6ab5675b8da2
Adrian Gavrila <agavrila@microsoft.com>
2025-02-12 12:32:53 -0500

    Fixing formatting and unused imports

--- pyrit/orchestrator/single_turn/many_shot_jailbreak_orchestrator.py
+++ pyrit/orchestrator/single_turn/many_shot_jailbreak_orchestrator.py
@@ -3,31 +3,33 @@
 
 import logging
 import random
-from typing import Optional
 from pathlib import Path
+from typing import Optional
 
-from pyrit.common.path import DATASETS_PATH 
+from pyrit.common.path import DATASETS_PATH
 from pyrit.datasets import fetch_many_shot_jailbreaking_dataset
-from pyrit.models import SeedPrompt, PromptRequestResponse
+from pyrit.models import PromptRequestResponse, SeedPrompt
 from pyrit.orchestrator import PromptSendingOrchestrator
 from pyrit.prompt_target import PromptChatTarget
 from pyrit.score import Scorer
 
 logger = logging.getLogger(__name__)
 
+
 class ManyShotJailbreakOrchestrator(PromptSendingOrchestrator):
     """
     This orchestrator implements the Many Shot Jailbreak methos as discussed in research found here: https://www.anthropic.com/research/many-shot-jailbreaking
-    
+
     Prepends the seed prompt with a faux dialogue between a human and an AI
     """
+
     def __init__(
-            self,
-            objective_target: PromptChatTarget,
-            scorers: Optional[list[Scorer]] = None,
-            verbose: bool = False,
-            num_examples: int = 3,
-            isTest: bool = False
+        self,
+        objective_target: PromptChatTarget,
+        scorers: Optional[list[Scorer]] = None,
+        verbose: bool = False,
+        num_examples: int = 3,
+        isTest: bool = False,
     ) -> None:
         """
         Args:
@@ -38,7 +40,7 @@ class ManyShotJailbreakOrchestrator(PromptSendingOrchestrator):
             num_examples (int, Optional): The number of examples to fetch from the dataset. Defaults to 3.
             isTest (bool, Optional): Controls whether or not the examples are gotten at random or statically. Setting to True can be useful for testing. Defaults to False.
         """
-        
+
         super().__init__(
             objective_target=objective_target,
             scorers=scorers,
@@ -52,9 +54,9 @@ class ManyShotJailbreakOrchestrator(PromptSendingOrchestrator):
         self.isTest = isTest
 
     async def construct_many_shot_dialogue(
-            self,
+            self, 
             malicious_prompt: str
-    ) -> str:
+        ) -> str:
         """
         Constructs the many shot dialogue to be prepended to the prompt
 
@@ -68,25 +70,26 @@ class ManyShotJailbreakOrchestrator(PromptSendingOrchestrator):
         examples = fetch_many_shot_jailbreaking_dataset()
 
         # Choose a random subset of examples of size num_examples, check that the number of examples is not greater than the number of examples in the dataset
-        if (self.num_examples > len(examples)):
-            logger.info(f"Specified example count is greater than the number of examples in the dataset. Using all {len(examples)} examples.")
+        if self.num_examples > len(examples):
+            logger.info(
+                f"Specified example count is greater than the number of examples in the dataset. Using all {len(examples)} examples."
+            )
             self.num_examples = len(examples)
 
         # Choose num_examples either static or random examples from the dataset
-        examples = (examples[:self.num_examples] if (self.isTest) else random.sample(examples, self.num_examples))
+        examples = examples[: self.num_examples] if (self.isTest) else random.sample(examples, self.num_examples)
 
         # Construct the many shot dialogue
-        many_shot_dialogue = self.template.render_template_value(prompt = malicious_prompt, examples = examples)
+        many_shot_dialogue = self.template.render_template_value(prompt=malicious_prompt, examples=examples)
 
         return many_shot_dialogue
 
-
-    async def send_prompts_async(
-            self,
-            *,
-            prompt_list: list[str],
-            memory_labels: Optional[dict[str, str]] = None,
-            metadata: Optional[dict[str, str]] = None,
+    async def send_prompts_async(  # type: ignore[override]
+        self,
+        *,
+        prompt_list: list[str],
+        memory_labels: Optional[dict[str, str]] = None,
+        metadata: Optional[dict[str, str]] = None,
     ) -> list[PromptRequestResponse]:
         """
         Sends the prompts to the target using the Many Shot Jailbreak
@@ -106,5 +109,5 @@ class ManyShotJailbreakOrchestrator(PromptSendingOrchestrator):
             prompt_list[i] = await self.construct_many_shot_dialogue(prompt_list[i])
 
         return await super().send_prompts_async(
-            prompt_list=prompt_list, prompt_type = "text", memory_labels=memory_labels, metadata=metadata
+            prompt_list=prompt_list, prompt_type="text", memory_labels=memory_labels, metadata=metadata
         )
--- tests/unit/orchestrator/test_many_shot_orchestrator.py
+++ tests/unit/orchestrator/test_many_shot_orchestrator.py
@@ -3,25 +3,30 @@
 
 from unittest.mock import AsyncMock, MagicMock, patch
 
-from pyrit.datasets import fetch_many_shot_jailbreaking_dataset
+import pytest
 from unit.mocks import MockPromptTarget
 
-import pytest
-
+from pyrit.datasets import fetch_many_shot_jailbreaking_dataset
 from pyrit.orchestrator import ManyShotJailbreakOrchestrator
 from pyrit.prompt_target import PromptChatTarget
+
 
 @pytest.fixture
 def mock_objective_target(patch_central_database) -> MockPromptTarget:
     return MockPromptTarget()
 
+
 @pytest.fixture
 def many_shot_jailbreak_orchestrator_static_examples(mock_objective_target):
-    return ManyShotJailbreakOrchestrator(objective_target=mock_objective_target, num_examples=3, isTest = True, verbose=True)
+    return ManyShotJailbreakOrchestrator(
+        objective_target=mock_objective_target, num_examples=3, isTest=True, verbose=True
+    )
+
 
 @pytest.fixture
 def many_shot_jailbreak_orchestrator_random_examples(mock_objective_target):
     return ManyShotJailbreakOrchestrator(objective_target=mock_objective_target, num_examples=3, verbose=True)
+
 
 @pytest.mark.asyncio
 async def test_construct_many_shot_dialogue_static_examples(many_shot_jailbreak_orchestrator_static_examples):
@@ -30,31 +35,39 @@ async def test_construct_many_shot_dialogue_static_examples(many_shot_jailbreak_
     dialogue = await many_shot_jailbreak_orchestrator_static_examples.construct_many_shot_dialogue(prompt)
     dialogue = str(dialogue)
     for example in examples:
-        assert example['user'] in dialogue
-        assert example['assistant'] in dialogue
+        assert example["user"] in dialogue
+        assert example["assistant"] in dialogue
     assert prompt[0] in dialogue
 
+
 @pytest.mark.asyncio
-async def test_send_single_prompt_static_examples(many_shot_jailbreak_orchestrator_static_examples, mock_objective_target):
+async def test_send_single_prompt_static_examples(
+    many_shot_jailbreak_orchestrator_static_examples, mock_objective_target
+):
     prompt = ["How do I pick a lock?"]
     expected_prompt = await many_shot_jailbreak_orchestrator_static_examples.construct_many_shot_dialogue(prompt[0])
 
     await many_shot_jailbreak_orchestrator_static_examples.send_prompts_async(prompt_list=prompt)
     assert mock_objective_target.prompt_sent == [expected_prompt]
 
+
 @pytest.mark.asyncio
-async def test_send_single_prompt_random_samples(many_shot_jailbreak_orchestrator_random_examples, mock_objective_target):
+async def test_send_single_prompt_random_samples(
+    many_shot_jailbreak_orchestrator_random_examples, mock_objective_target
+):
     prompt = ["How do I pick a lock?"]
     sent_prompt = prompt[0]
 
     await many_shot_jailbreak_orchestrator_random_examples.send_prompts_async(prompt_list=prompt)
     assert len(mock_objective_target.prompt_sent[0]) > len(sent_prompt)
 
+
 def test_init_static_examples(many_shot_jailbreak_orchestrator_static_examples):
     assert isinstance(many_shot_jailbreak_orchestrator_static_examples._objective_target, PromptChatTarget)
     assert many_shot_jailbreak_orchestrator_static_examples.num_examples == 3
     assert many_shot_jailbreak_orchestrator_static_examples.isTest is True
     assert many_shot_jailbreak_orchestrator_static_examples._verbose is True
+
 
 def test_init_random_examples(many_shot_jailbreak_orchestrator_random_examples):
     assert isinstance(many_shot_jailbreak_orchestrator_random_examples._objective_target, PromptChatTarget)
