53b9b9c76a77236dc07a467e48ae11b35930ffe6
Martin Pouliot <mpouliot@microsoft.com>
2025-02-18 11:30:28 -0500

    Fixed an import issue

--- pyrit/score/human_in_the_loop_gradio.py
+++ pyrit/score/human_in_the_loop_gradio.py
@@ -4,13 +4,14 @@ from pyrit.models import Score, PromptRequestPiece
 from typing import Optional
 
 class HumanInTheLoopScorerGradio(Scorer):
-    # Import here to avoid importing rpyc in the main module that might not be installed
-    from pyrit.ui.rpc import AppRpcServer
 
     def __init__(self, *, open_browser=False, scorer: Scorer = None, re_scorers: list[Scorer] = None) -> None:
+        # Import here to avoid importing rpyc in the main module that might not be installed
+        from pyrit.ui.rpc import AppRpcServer
+
         self._scorer = scorer
         self._re_scorers = re_scorers
-        self._rpc_server = self.AppRpcServer(open_browser=open_browser)
+        self._rpc_server = AppRpcServer(open_browser=open_browser)
         self._rpc_server.start()
 
 
