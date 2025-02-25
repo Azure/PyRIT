75cd61114ef75f481a5ae9e2a7bdd7f6c9d7b440
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2025-02-25 13:54:23 -0800

    added changed notebooks

--- build_scripts/remove_notebook_headers.py
+++ build_scripts/remove_notebook_headers.py
@@ -16,9 +16,9 @@ def remove_kernelspec_from_ipynb_files(file_path: str):
             if "metadata" in content and "kernelspec" in content["metadata"]:
                 modified_files.append(file_path)
                 del content["metadata"]["kernelspec"]
-            # Save the modified content back to the .ipynb file
-            with open(file_path, "w", encoding="utf-8") as f:
-                json.dump(content, f, indent=2)
+                # Save the modified content back to the .ipynb file
+                with open(file_path, "w", encoding="utf-8") as f:
+                    json.dump(content, f, indent=1)
         if modified_files:
             print("Modified files:", modified_files)
             sys.exit(1)
@@ -30,4 +30,3 @@ if __name__ == "__main__":
     # # Remove the "kernelspec" section from all .ipynb files in the specified file
     for file in files:
         remove_kernelspec_from_ipynb_files(file)
-
