a598b14d7517406bd345a0fe086a76f09c038b24
Bolor-Erdene Jagdagdorj <bjagdagdorj@microsoft.com>
2025-02-26 14:47:42 -0800

    working version of video converter still draft though

--- pyrit/prompt_converter/add_image_to_video_converter.py
+++ pyrit/prompt_converter/add_image_to_video_converter.py
@@ -1,20 +1,32 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from pathlib import Path
+import cv2
 import logging
+import numpy as np
 import os
 
-import cv2
-
+from pyrit.common.path import DB_DATA_PATH
 from pyrit.models import PromptDataType, data_serializer_factory
 from pyrit.prompt_converter import ConverterResult, PromptConverter
 
 logger = logging.getLogger(__name__)
 
+# Get input file extension
+
+# Choose the codec based on extension
+video_encoding_map = {
+    "mp4": "mp4v",
+    "avi": "XVID",
+    "mov": "MJPG",
+    "mkv": "X264",
+}
 
 class AddImageVideoConverter(PromptConverter):
     """
-    Adds an image to a video at a specified position.
+    Adds an image to a video at a specified position. 
+    Also, currently the image is placed in the whole video, not at a specific timepoint
 
     Args:
         video_path (str): File path of video to add image to
@@ -33,12 +45,12 @@ class AddImageVideoConverter(PromptConverter):
         if not video_path:
             raise ValueError("Please provide valid image path")
 
-        self.output_path = output_path
-        self.img_position = img_position
-        self.img_resize_size = img_resize_size
+        self._output_path = output_path
+        self._img_position = img_position
+        self._img_resize_size = img_resize_size
         self._video_path = video_path
 
-    def _add_image_to_video(self, image_path: str, output_path: str):
+    async def _add_image_to_video(self, image_path: str, output_path: str):
         """
         Adds image to video
         Args:
@@ -47,57 +59,81 @@ class AddImageVideoConverter(PromptConverter):
         Returns:
             Image.Image: The image with added text.
         """
+
         if not image_path:
             raise ValueError("Please provide valid image path value")
 
-        if not os.path.exists(image_path):
-            print(image_path)
-            raise ValueError("Image path does not exist")
+        input_image_data = data_serializer_factory(category="prompt-memory-entries", data_type="image_path", value=image_path)
+        input_video_data = data_serializer_factory(category="prompt-memory-entries", data_type="video_path", value=self._video_path)
 
-        if not os.path.exists(self._video_path):
-            print(self._video_path)
-            raise ValueError("Video path does not exist")
+        # Open the video to ensure it exists
+        video_bytes = await input_video_data.read_data()
+        
+        azure_storage_flag = False
+        video_path = self._video_path
+        
+        try:
+            if input_video_data._is_azure_storage_url(self._video_path):
+                # If the video is in Azure storage, download it first
 
-        # Open the video
-        cap = cv2.VideoCapture(self._video_path)
+                # Save the video bytes to a temporary file
+                local_temp_path = Path(DB_DATA_PATH, "temp_video.mp4")
+                with open(local_temp_path, "wb") as f:
+                    f.write(video_bytes)
+                video_path = str(local_temp_path)
+                azure_storage_flag = True
 
-        # Get video properties
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec for MP4
-        out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+       
+            cap = cv2.VideoCapture(video_path)
 
-        # Load and resize the overlay image
-        overlay = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-        overlay = cv2.resize(overlay, self.img_resize_size)
+            # Get video properties
+            fps = int(cap.get(cv2.CAP_PROP_FPS))
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            file_extension = video_path.split(".")[-1].lower()
+            if file_extension in video_encoding_map:
+                video_char_code = cv2.VideoWriter_fourcc(*video_encoding_map[file_extension])
+                out = cv2.VideoWriter(output_path, video_char_code, fps, (width, height))
+            else:
+                raise ValueError(f"Unsupported video format: {file_extension}")
+            
 
-        # Get overlay image dimensions
-        h, w, _ = overlay.shape
-        x, y = self.img_position  # Position where the overlay will be placed
+            # Load and resize the overlay image
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+            input_image_bytes = await input_image_data.read_data()
+            image_np_arr = np.frombuffer(input_image_bytes, np.uint8)
+            overlay = cv2.imdecode(image_np_arr, cv2.IMREAD_UNCHANGED)
+            overlay = cv2.resize(overlay, self._img_resize_size)
 
-            # Ensure overlay fits within the frame boundaries
-            if x + w > width or y + h > height:
-                print("Overlay image is too large for the video frame. Resizing to fit.")
-                overlay = cv2.resize(overlay, (width - x, height - y))
+            # Get overlay image dimensions
+            h, w, _ = overlay.shape
+            x, y = self._img_position  # Position where the overlay will be placed
 
-            # Blend overlay with frame
-            frame[y : y + h, x : x + w] = overlay
+            while cap.isOpened(): # TODO: use dataserializer to write
+                ret, frame = cap.read()
+                if not ret:
+                    break
 
-            # Write the modified frame to the output video
-            out.write(frame)
+                # Ensure overlay fits within the frame boundaries
+                if x + w > width or y + h > height:
+                    print("Overlay image is too large for the video frame. Resizing to fit.")
+                    overlay = cv2.resize(overlay, (width - x, height - y))
 
-        # Release everything
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+                # Blend overlay with frame
+                frame[y : y + h, x : x + w] = overlay
 
-        print(f"Video saved as {output_path}")
+                # Write the modified frame to the output video
+                out.write(frame)
+
+        finally: 
+            # Release everything
+            cap.release()
+            out.release()
+            cv2.destroyAllWindows()
+            if azure_storage_flag:
+                os.remove(local_temp_path)
+
+        logger.info(f"Video saved as {output_path}")
 
         return output_path
 
@@ -116,14 +152,17 @@ class AddImageVideoConverter(PromptConverter):
 
         output_video_serializer = data_serializer_factory(category="prompt-memory-entries", data_type="video_path")
 
-        if not self.output_path:
+        if not self._output_path:
             output_video_serializer.value = output_video_serializer.get_data_filename()
         else:
-            output_video_serializer.value = self.output_path
+            output_video_serializer.value = self._output_path
 
-        # # Add video to the image
-        updated_video = self._add_image_to_video(image_path=prompt, output_path=output_video_serializer.value)
+        # Add video to the image
+        updated_video = await self._add_image_to_video(image_path=prompt, output_path=output_video_serializer.value)
         return ConverterResult(output_text=str(updated_video), output_type="video_path")
 
     def input_supported(self, input_type: PromptDataType) -> bool:
         return input_type == "image_path"
+    
+    def output_supported(self, output_type: PromptDataType) -> bool:
+        return output_type == "video_path"
