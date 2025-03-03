e031c9ab6bcde078588eec96c168316e22e0c21c
Roman Lutz <romanlutz13@gmail.com>
2025-03-02 21:56:55 -0500

    Update fetch_example_datasets.py

--- pyrit/datasets/fetch_example_datasets.py
+++ pyrit/datasets/fetch_example_datasets.py
@@ -514,7 +514,7 @@ def fetch_red_team_social_bias_prompts_dataset() -> SeedPromptDataset:
         For more information and access to the original dataset and related materials, visit:
         https://svannie678.github.io/svannie678-red_team_repo_social_bias. This dataset
         contains aggregated and unified existing red-teaming prompts designed to identify
-        stereotypes,discrimination, hate speech, and other representationharms in text-based
+        stereotypes, discrimination, hate speech, and other representationharms in text-based
         Large Language Models (LLMs).
         Written by Simone Van Taylor.
     """
