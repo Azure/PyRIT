e34795f3f131c8e06818f9af36e86723ec8bd4ec
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-03-06 19:12:10 +0100

    update the docstring

--- pyrit/datasets/fetch_example_datasets.py
+++ pyrit/datasets/fetch_example_datasets.py
@@ -608,7 +608,10 @@ def fetch_pku_safe_rlhf_dataset(
 
     Args:
         include_safe_prompts (bool): all prompts in the dataset are returned if True; the dataset has
-        RLHF markers for unsafe responses, so if False we only return the unsafe subset
+            RLHF markers for unsafe responses, so if False we only return the unsafe subset
+        filter_harm_categories: List of harm categories to filter the examples.
+            Defaults to None, which means all categories are included.
+            Otherwise, only prompts with at least one matching category are included.
 
     Returns:
         SeedPromptDataset: A SeedPromptDataset containing the examples.
