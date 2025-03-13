936ab975beb9cc223464d5fcd8b8a3e5577861bf
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-03-13 14:41:47 +0100

    update `datasets/__init__.py` after the refactoring

--- pyrit/datasets/__init__.py
+++ pyrit/datasets/__init__.py
@@ -1,23 +1,23 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from .fetch_examples import (
-    fetch_adv_bench_dataset,
-    fetch_aya_redteaming_dataset,
-    fetch_babelscape_alert_dataset,
-    fetch_decoding_trust_stereotypes_dataset,
-    fetch_examples,
-    fetch_forbidden_questions_dataset,
-    fetch_harmbench_dataset,
-    fetch_librAI_do_not_answer_dataset,
+from pyrit.datasets.adv_bench_dataset import fetch_adv_bench_dataset
+from pyrit.datasets.aya_redteaming_dataset import fetch_aya_redteaming_dataset
+from pyrit.datasets.babelscape_alert_dataset import fetch_babelscape_alert_dataset
+from pyrit.datasets.decoding_trust_stereotypes_dataset import fetch_decoding_trust_stereotypes_dataset
+from pyrit.datasets.fetch_examples import fetch_examples
+from pyrit.datasets.forbidden_questions_dataset import fetch_forbidden_questions_dataset
+from pyrit.datasets.harmbench_dataset import fetch_harmbench_dataset
+from pyrit.datasets.librAI_do_not_answer_dataset import fetch_librAI_do_not_answer_dataset
+from pyrit.datasets.llm_latent_adversarial_training_harmful_dataset import (
     fetch_llm_latent_adversarial_training_harmful_dataset,
-    fetch_many_shot_jailbreaking_dataset,
-    fetch_pku_safe_rlhf_dataset,
-    fetch_seclists_bias_testing_dataset,
-    fetch_tdc23_redteaming_dataset,
-    fetch_wmdp_dataset,
-    fetch_xstest_dataset,
 )
+from pyrit.datasets.many_shot_jailbreaking_dataset import fetch_many_shot_jailbreaking_dataset
+from pyrit.datasets.pku_safe_rlhf_dataset import fetch_pku_safe_rlhf_dataset
+from pyrit.datasets.seclists_bias_testing_dataset import fetch_seclists_bias_testing_dataset
+from pyrit.datasets.tdc23_redteaming_dataset import fetch_tdc23_redteaming_dataset
+from pyrit.datasets.wmdp_dataset import fetch_wmdp_dataset
+from pyrit.datasets.xstest_dataset import fetch_xstest_dataset
 
 __all__ = [
     "fetch_aya_redteaming_dataset",
