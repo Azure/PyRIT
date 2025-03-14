84befc7cfa1aa5b96dd65139166b0b305b67a116
Bashir Partovi <bpartovi@microsoft.com>
2025-03-13 22:57:15 -0400

    [VS Code][Dev Container] Creating a uniform dev experience

---
+++ .devcontainer/devcontainer_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# initializing conda in the current shell session
+eval "$(conda shell.bash hook)"
+
+conda create -n pyrit-dev python=3.11 -y
+conda activate pyrit-dev
+conda install ipykernel -y
+pip install -e ."[dev,playwright]"
+sudo chown -R vscode:vscode /workspace
