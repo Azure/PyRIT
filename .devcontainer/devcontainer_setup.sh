0a8f21b1650fcf153b63f923804e7025cd4e49f4
Bashir Partovi <bpartovi@microsoft.com>
2025-03-14 12:26:58 -0400

    addressing Roman's comments

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -6,5 +6,5 @@ eval "$(conda shell.bash hook)"
 conda create -n pyrit-dev python=3.11 -y
 conda activate pyrit-dev
 conda install ipykernel -y
-pip install -e ."[dev,playwright]"
+pip install -e ."[dev,all]"
 sudo chown -R vscode:vscode /workspace
