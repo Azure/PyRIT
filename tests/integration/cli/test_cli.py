2f1c6f782b8055cd22d823d16e88a1753eb2e84a
Roman Lutz <romanlutz13@gmail.com>
2025-03-14 23:10:33 -0700

    integration test for scanner

---
+++ tests/integration/cli/test_cli.py
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import shlex
+
+import pytest
+
+from pyrit.cli.__main__ import main
+from pyrit.orchestrator import PromptSendingOrchestrator, CrescendoOrchestrator, RedTeamingOrchestrator, TreeOfAttacksWithPruningOrchestrator
+
+test_cases_success = [
+    (
+        "--config-file 'tests/unit/cli/mixed_multiple_orchestrators_args_success.yaml'",
+        [PromptSendingOrchestrator, TreeOfAttacksWithPruningOrchestrator, CrescendoOrchestrator, RedTeamingOrchestrator],
+        ["send_normalizer_requests_async", "run_attack_async", "run_attack_async", "run_attack_async"]
+    ),
+]
+
+
+@pytest.mark.parametrize("command, orchestrator_classes, methods", test_cases_success)
+def test_cli_integration_success(command, orchestrator_classes, methods):
+    main(shlex.split(command))
