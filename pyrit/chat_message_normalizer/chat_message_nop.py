02ab4ee40562a373e1fa56588b1e1d09736a3aa0
Roman Lutz <romanlutz13@gmail.com>
2025-03-15 00:45:53 -0700

    chat message normalizer docstrings

--- pyrit/chat_message_normalizer/chat_message_nop.py
+++ pyrit/chat_message_normalizer/chat_message_nop.py
@@ -6,8 +6,17 @@ from pyrit.models import ChatMessage
 
 
 class ChatMessageNop(ChatMessageNormalizer[list[ChatMessage]]):
+    """
+    A no-op chat message normalizer that does not modify the input messages.
+    """
     def normalize(self, messages: list[ChatMessage]) -> list[ChatMessage]:
         """
-        returns the same list as was passed in
+        Returns the same list as was passed in.
+
+        Args:
+            messages (list[ChatMessage]): The list of messages to normalize.
+
+        Returns:
+            list[ChatMessage]: The normalized list of messages.
         """
         return messages
--- pyrit/chat_message_normalizer/generic_system_squash.py
+++ pyrit/chat_message_normalizer/generic_system_squash.py
@@ -7,11 +7,16 @@ from pyrit.models import ChatMessage, ChatMessageRole
 
 class GenericSystemSquash(ChatMessageNormalizer[list[ChatMessage]]):
     def normalize(self, messages: list[ChatMessage]) -> list[ChatMessage]:
-        """
-        Returns the first system message combined with the first user message
-        using a format that uses generic instruction tags
-        """
+        """Returns the first system message combined with the first user message.
+        
+        The format of the result uses generic instruction tags.
 
+        Args:
+            messages (list[ChatMessage]): The list of messages to normalize.
+
+        Returns:
+            list[ChatMessage]: The normalized list of messages.
+        """
         normalized_messages = ChatMessageNormalizer.squash_system_message(
             messages=messages, squash_function=GenericSystemSquash.combine_system_user_message
         )
@@ -26,6 +31,7 @@ class GenericSystemSquash(ChatMessageNormalizer[list[ChatMessage]]):
         Args:
             system_message (str): The system message.
             user_message (str): The user message.
+            msg_type (ChatMessageRole, optional): The message type. Defaults to "user".
 
         Returns:
             ChatMessage: The combined message.
