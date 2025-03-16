8433ff9d025f1a88cb23b1841948be2a98321209
Volkan Kutal <50370416+KutalVolkan@users.noreply.github.com>
2025-03-16 09:34:22 +0100

    Update pyrit/prompt_converter/pdf_converter.py
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

--- pyrit/prompt_converter/pdf_converter.py
+++ pyrit/prompt_converter/pdf_converter.py
@@ -323,7 +323,7 @@ class PDFConverter(PromptConverter):
         Returns:
             DataTypeSerializer: The serializer object containing metadata about the saved file.
         """
-        original_filname_ending = self._existing_pdf_path.stem if self._existing_pdf_path else ""
+        original_filename_ending = self._existing_pdf_path.stem if self._existing_pdf_path else ""
 
         pdf_serializer = data_serializer_factory(
             category="prompt-memory-entries", data_type="url", value=content, extension=f"{original_filname_ending}.pdf"
