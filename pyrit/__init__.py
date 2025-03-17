000ade4042a5b92042e3513c0ee6be2f3f52b9ff
jsong468 <songjustin@microsoft.com>
2025-03-16 19:46:19 -0700

    release version

--- pyrit/__init__.py
+++ pyrit/__init__.py
@@ -5,4 +5,4 @@ from .show_versions import show_versions  # noqa: F401
 
 __name__ = "pyrit"
 # Remove dev suffix when releasing and keep in sync with pyproject.toml
-__version__ = "0.7.1.dev0"
+__version__ = "0.7.0"
