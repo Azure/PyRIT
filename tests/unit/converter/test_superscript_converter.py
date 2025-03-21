0878a9a748981d3cb1a2a711b9d252ffcbddb6c4
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-03-21 13:39:44 +0100

    add tests for 'random' mode

--- tests/unit/converter/test_superscript_converter.py
+++ tests/unit/converter/test_superscript_converter.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 import pytest
+import random
 
 from pyrit.prompt_converter import ConverterResult, SuperscriptConverter
 
@@ -28,3 +29,25 @@ async def test_superscript_converter():
         ["word1 word2 word3 word4 word5"],
         ["ʷᵒʳᵈ¹ word2 ʷᵒʳᵈ³ word4 ʷᵒʳᵈ⁵"],
     )
+
+@pytest.mark.asyncio
+async def test_random_superscript_converter():
+    full_random_converter = SuperscriptConverter(mode="random", random_percentage=100)
+    await _check_conversion(
+        full_random_converter,
+        ["Let's test random mode"],
+        ["ᴸᵉᵗ'ˢ ᵗᵉˢᵗ ʳᵃⁿᵈᵒᵐ ᵐᵒᵈᵉ"],
+    )
+    # zero_random_converter = SuperscriptConverter(mode="random", random_percentage=0)
+    # await _check_conversion(
+    #     zero_random_converter,
+    #     ["Let's test random mode"],
+    #     ["Let's test random mode"],
+    # )
+
+    random.seed(32)  # with seed=32 and 6 words, words at [1,2,5] will be converted
+    half_random_converter = SuperscriptConverter(mode="random", random_percentage=50)
+    test_text = "one two three four five six"
+    expected_output = "ᵒⁿᵉ ᵗʷᵒ three four ᶠⁱᵛᵉ six"
+    result = await half_random_converter.convert_async(prompt=test_text, input_type="text")
+    assert result.output_text == expected_output
