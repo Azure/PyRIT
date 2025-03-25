00505af16d48630fbb6ab969130637ef5d4db817
Roman Lutz <romanlutz13@gmail.com>
2025-03-25 15:53:30 -0700

    move warning removal to global init to ensure precedence

--- pyrit/common/turn_off_transformers_warning.py
+++ pyrit/common/turn_off_transformers_warning.py
@@ -4,4 +4,4 @@
 import os
 
 # Most people install PyRIT without torch so transformers prints a warning
-os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = True
+os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "True"
