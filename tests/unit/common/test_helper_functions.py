949a44826f73b523527fccaf7805cc82969c12bd
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-03-28 10:36:15 +0100

    add tests for `select_word_indices` util function

--- tests/unit/common/test_helper_functions.py
+++ tests/unit/common/test_helper_functions.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from pyrit.common.utils import combine_dict
+from pyrit.common.utils import combine_dict, select_word_indices
 
 
 def test_combine_non_empty_dict():
@@ -32,3 +32,9 @@ def test_combine_dict_same_keys():
     dict1 = {"c": "b"}
     dict2 = {"c": "d"}
     assert combine_dict(dict1, dict2) == {"c": "d"}
+
+
+def test_word_indices_selection():
+    assert select_word_indices(words=["word1", "word2", "word3"], mode="all") == [0, 1, 2]
+    assert select_word_indices(words=["word1", "word2", "pyrit", "word4"], mode="keywords", keywords=["pyrit"]) == [2]
+    assert select_word_indices(words=["word1", "word2", "pyrit", "word4"], mode="regex", regex=r"word\d") == [0, 1, 3]
