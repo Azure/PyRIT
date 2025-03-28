6e527ddbf2735985db5b21b4405c024d788be73f
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-03-28 12:41:48 +0100

    refactor `ROT13Converter` to inherit from `WordLevelConverter`

--- pyrit/prompt_converter/rot13_converter.py
+++ pyrit/prompt_converter/rot13_converter.py
@@ -3,24 +3,11 @@
 
 import codecs
 
-from pyrit.models import PromptDataType
-from pyrit.prompt_converter import ConverterResult, PromptConverter
+from pyrit.prompt_converter.word_level_converter import WordLevelConverter
 
 
-class ROT13Converter(PromptConverter):
+class ROT13Converter(WordLevelConverter):
+    """Simple converter that just ROT13 encodes the prompt"""
 
-    async def convert_async(self, *, prompt: str, input_type: PromptDataType = "text") -> ConverterResult:
-        """
-        Simple converter that just ROT13 encodes the prompts
-        """
-        if not self.input_supported(input_type):
-            raise ValueError("Input type not supported")
-
-        result = ConverterResult(output_text=codecs.encode(prompt, "rot13"), output_type="text")
-        return result
-
-    def input_supported(self, input_type: PromptDataType) -> bool:
-        return input_type == "text"
-
-    def output_supported(self, output_type: PromptDataType) -> bool:
-        return output_type == "text"
+    async def convert_word_async(self, word: str) -> str:
+        return codecs.encode(word, "rot13")
