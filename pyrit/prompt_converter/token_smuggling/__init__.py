a8a51683efbbb1f62fade6621b9e26b7e9aba0c6
KutalVolkan <vkutal@outlook.com>
2025-04-03 09:27:55 +0200

    Run hooks and merge with upstream/main

--- pyrit/prompt_converter/token_smuggling/__init__.py
+++ pyrit/prompt_converter/token_smuggling/__init__.py
@@ -3,7 +3,9 @@
 
 from pyrit.prompt_converter.token_smuggling.ascii_smuggler_converter import AsciiSmugglerConverter
 from pyrit.prompt_converter.token_smuggling.sneaky_bits_smuggler_converter import SneakyBitsSmugglerConverter
-from pyrit.prompt_converter.token_smuggling.variation_selector_smuggler_converter import VariationSelectorSmugglerConverter
+from pyrit.prompt_converter.token_smuggling.variation_selector_smuggler_converter import (
+    VariationSelectorSmugglerConverter,
+)
 
 __all__ = [
     "AsciiSmugglerConverter",
