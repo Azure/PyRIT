753c93f948fefe33fe16f3a4aa3c1a6e8756e9eb
Bashir Partovi <bpartovi@microsoft.com>
2025-04-08 21:13:39 -0400

    FEAT: optimized .devcontainer and added python code intelligence to vscode

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-# initializing conda in the current shell session
 eval "$(conda shell.bash hook)"
 
-conda create -n pyrit-dev python=3.11 -y
 conda activate pyrit-dev
-conda install ipykernel -y
-pip install -e ."[dev,all]"
 
-# excluding .git directories from the chown command to avoid permission errors in MacOS
-sudo find /workspace \
-  -not -path '*/.git/*' \
-  -exec chown vscode:vscode {} +
+if [ ! -d "/workspace/pyrit.egg-info" ]; then
+    echo "Installing PyRIT dependencies..."
+    cd /workspace
+    pip install -e ".[dev,all]"
+fi
