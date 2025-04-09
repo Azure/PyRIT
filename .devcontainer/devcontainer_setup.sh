0c5f5fb9fe58f201645285f730d68225370bd3ce
Bashir Partovi <bpartovi@microsoft.com>
2025-04-08 21:56:30 -0400

    added the kernel

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -7,5 +7,6 @@ conda activate pyrit-dev
 if [ ! -d "/workspace/pyrit.egg-info" ]; then
     echo "Installing PyRIT dependencies..."
     cd /workspace
+    conda install ipykernel -y
     pip install -e ".[dev,all]"
 fi
