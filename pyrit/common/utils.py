513477e976612052a2f34e29b63b2d0f5f4ff951
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-04-19 14:19:51 +0200

    add warning for out-of-bounds indices

--- pyrit/common/utils.py
+++ pyrit/common/utils.py
@@ -132,4 +132,8 @@ def select_word_indices(
 
         case "custom":
             custom_indices = kwargs.get("indices", [])
-            return [i for i in custom_indices if 0 <= i < len(words)]
+            valid_indices = [i for i in custom_indices if 0 <= i < len(words)]
+            invalid_indices = [i for i in custom_indices if i < 0 or i >= len(words)]
+            if invalid_indices:
+                logger.warning(f"Ignoring out-of-bounds indices: {invalid_indices}")
+            return valid_indices
