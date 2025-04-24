fb82283aa329a0c788bd6340a1f160f0d5fdf14e
Nina Chikanov <nichikan@microsoft.com>
2025-04-24 22:57:11 +0000

    pr feedback: adding integration tests (in-progress)

--- tests/integration/cli/test_cli_integration.py
+++ tests/integration/cli/test_cli_integration.py
@@ -24,6 +24,21 @@ test_cases_success = [
         ],
         ["send_normalizer_requests_async", "run_attack_async", "run_attack_async", "run_attack_async"],
     ),
+    (
+        "--config-file 'tests/integration/cli/prompt_send_success.yaml'",
+        [PromptSendingOrchestrator],
+        ["send_normalizer_requests_async"],
+    ),
+    (
+        "--config-file 'tests/integration/cli/prompt_send_success_converters_text_default.yaml'",
+        [PromptSendingOrchestrator],
+        ["send_normalizer_requests_async"],
+    ),
+    (
+        "--config-file 'tests/integration/cli/prompt_send_success_converters_image_default.yaml'",
+        [PromptSendingOrchestrator],
+        ["send_normalizer_requests_async"],
+    ),
 ]
 
 
