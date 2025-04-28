af55935d6a43244985c9859d25010a1cd226e306
Richard Lundeen <rlundeen@microsoft.com>
2025-04-28 09:52:25 -0700

    pr feedback

--- pyrit/score/__init__.py
+++ pyrit/score/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from pyrit.score.scorer import Scorer
+
 from pyrit.score.azure_content_filter_scorer import AzureContentFilterScorer
+from pyrit.score.composite_scorer import CompositeScorer
 from pyrit.score.float_scale_threshold_scorer import FloatScaleThresholdScorer
 from pyrit.score.general_scorer import SelfAskGeneralScorer
 from pyrit.score.gandalf_scorer import GandalfScorer
@@ -10,7 +13,7 @@ from pyrit.score.human_in_the_loop_gradio import HumanInTheLoopScorerGradio
 from pyrit.score.insecure_code_scorer import InsecureCodeScorer
 from pyrit.score.markdown_injection import MarkdownInjectionScorer
 from pyrit.score.prompt_shield_scorer import PromptShieldScorer
-from pyrit.score.scorer import Scorer
+from pyrit.score.score_aggregator import AND_, OR_, XOR_, MAJORITY_, ScoreAggregator
 from pyrit.score.self_ask_category_scorer import ContentClassifierPaths, SelfAskCategoryScorer
 from pyrit.score.self_ask_likert_scorer import LikertScalePaths, SelfAskLikertScorer
 from pyrit.score.self_ask_refusal_scorer import SelfAskRefusalScorer
@@ -20,8 +23,10 @@ from pyrit.score.substring_scorer import SubStringScorer
 from pyrit.score.true_false_inverter_scorer import TrueFalseInverterScorer
 
 __all__ = [
+    "AND_",
     "AzureContentFilterScorer",
     "ContentClassifierPaths",
+    "CompositeScorer",
     "FloatScaleThresholdScorer",
     "GandalfScorer",
     "SelfAskGeneralScorer",
@@ -29,9 +34,12 @@ __all__ = [
     "HumanInTheLoopScorerGradio",
     "InsecureCodeScorer",
     "LikertScalePaths",
+    "MAJORITY_",
     "MarkdownInjectionScorer",
+    "OR_",
     "PromptShieldScorer",
     "Scorer",
+    "ScoreAggregator",
     "SelfAskCategoryScorer",
     "SelfAskLikertScorer",
     "SelfAskRefusalScorer",
@@ -41,4 +49,5 @@ __all__ = [
     "TrueFalseInverterScorer",
     "TrueFalseQuestion",
     "TrueFalseQuestionPaths",
+    "XOR_",
 ]
--- pyrit/score/composite_scorer.py
+++ pyrit/score/composite_scorer.py
@@ -1,31 +1,33 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import List, Literal, Optional
+from typing import List, Optional
 
 from pyrit.models import PromptRequestPiece, Score
 from pyrit.score.scorer import Scorer
+from pyrit.score.score_aggregator import ScoreAggregator
 
-LogicalCombination = Literal["AND", "OR"]
+class CompositeScorer(Scorer):
+    """A scorer that aggregates other true_false scorers using a specified aggregation function.
 
-
-class CompoundScorer(Scorer):
-    """A scorer that aggregates other true_false scorers, either using AND or OR logic.
-
-    It returns a single score of True or False based on the logical combination of the scores of the constituent
+    It returns a single score of True or False based on the aggregation of the scores of the constituent
     scorers.
 
     Args:
-        logical_combination: The logical operation to combine scores ("AND" or "OR")
+        aggregator: The aggregation function to use (e.g. AND_, OR_, XOR_, MAJORITY_)
         scorers: List of true_false scorers to combine
         score_category: Optional category for the score
     """
 
     def __init__(
-        self, *, logical_combination: LogicalCombination, scorers: List[Scorer], score_category: Optional[str] = None
+        self,
+        *,
+        aggregator: ScoreAggregator,
+        scorers: List[Scorer],
+        score_category: Optional[str] = None
     ) -> None:
         self.scorer_type = "true_false"
-        self._logical_combination = logical_combination
+        self._aggregator = aggregator
         self._score_category = score_category
 
         if not scorers:
@@ -53,37 +55,20 @@ class CompoundScorer(Scorer):
         identifier_dict = self.get_identifier()
         identifier_dict["sub_identifier"] = [scorer.get_identifier() for scorer in self._scorers]
 
+        result = self._aggregator(scores)
+
         return_score = Score(
-            score_value=str(True),
+            score_value=str(result.value),
             score_value_description=None,
             score_type=self.scorer_type,
             score_category=self._score_category,
             score_metadata=None,
-            score_rationale="",
+            score_rationale=result.rationale,
             scorer_class_identifier=identifier_dict,
             prompt_request_response_id=request_response.id,
             task=task,
         )
 
-        sub_rationale = "\n\n".join(f"{score.scorer_class_identifier} {score.score_rationale}" for score in scores)
-
-        # Determine final score and rationale based on logical combination
-        if self._logical_combination == "AND":
-            return_score.score_value = str(all(score.get_value() for score in scores))
-            top_rationale = (
-                "All constituent scorers returned True in an AND compound scorer."
-                if return_score.get_value() is True
-                else "At least one constituent scorer returned False in an AND compound scorer."
-            )
-        else:
-            return_score.score_value = str(any(score.get_value() for score in scores))
-            top_rationale = (
-                "At least one constituent scorer returned True in an OR compound scorer."
-                if return_score.get_value() is True
-                else "All constituent scorers returned False in an OR compound scorer."
-            )
-
-        return_score.score_rationale = f"{top_rationale}\n\n{sub_rationale}"
         return [return_score]
 
     async def _score_all_async(
