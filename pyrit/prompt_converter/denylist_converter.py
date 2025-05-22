ab5e1f314d03c72fac1a406bf9a29a7daddc8e2a
Hannah Westra <hannahwestra@microsoft.com>
2025-05-22 15:13:41 -0400

    run pre-commit

--- pyrit/prompt_converter/denylist_converter.py
+++ pyrit/prompt_converter/denylist_converter.py
@@ -1,20 +1,23 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 import logging
 import pathlib
 
 from pyrit.common.path import DATASETS_PATH
-from pyrit.models import (
-    PromptDataType,
-    SeedPrompt
-)
+from pyrit.models import PromptDataType, SeedPrompt
 from pyrit.prompt_converter import ConverterResult, LLMGenericTextConverter
 from pyrit.prompt_target import PromptChatTarget
 
 logger = logging.getLogger(__name__)
 
+
 class DenylistConverter(LLMGenericTextConverter):
     """Converts input forbidding certain words or phrases"""
 
-    def __init__(self, *, converter_target: PromptChatTarget, prompt_template: SeedPrompt = None, denylist: list[str] = []):
+    def __init__(
+        self, *, converter_target: PromptChatTarget, prompt_template: SeedPrompt = None, denylist: list[str] = []
+    ):
         # set to default strategy if not provided
         prompt_template = (
             prompt_template
@@ -23,9 +26,8 @@ class DenylistConverter(LLMGenericTextConverter):
                 pathlib.Path(DATASETS_PATH) / "prompt_converters" / "denylist_converter.yaml"
             )
         )
-        
-        super().__init__(converter_target=converter_target, system_prompt_template=prompt_template, denylist=denylist)
 
+        super().__init__(converter_target=converter_target, system_prompt_template=prompt_template, denylist=denylist)
 
     async def convert_async(self, *, prompt: str, input_type: PromptDataType = "text") -> ConverterResult:
         """
--- tests/unit/converter/test_denylist_converter.py
+++ tests/unit/converter/test_denylist_converter.py
@@ -2,9 +2,9 @@
 # Licensed under the MIT license.
 
 import pytest
+from unit.mocks import MockPromptTarget
 
 from pyrit.prompt_converter.denylist_converter import DenylistConverter
-from unit.mocks import MockPromptTarget
 
 
 def test_prompt_denylist_init_templates_not_null(duckdb_instance) -> None:
@@ -15,10 +15,7 @@ def test_prompt_denylist_init_templates_not_null(duckdb_instance) -> None:
 
 @pytest.mark.asyncio
 async def test_denylist_not_provided() -> None:
-    converter = DenylistConverter(
-        converter_target=MockPromptTarget(),
-        prompt_template=None
-    )
+    converter = DenylistConverter(converter_target=MockPromptTarget(), prompt_template=None)
     output = await converter.convert_async(prompt="how to cut down a tree?", input_type="text")
     assert output.output_text == "how to cut down a tree?"
     assert output.output_type == "text"
@@ -26,22 +23,15 @@ async def test_denylist_not_provided() -> None:
 
 @pytest.mark.asyncio
 async def test_denylist_no_match() -> None:
-    converter = DenylistConverter(
-        converter_target=MockPromptTarget(),
-        prompt_template=None,
-        denylist=["branch"]
-    )
+    converter = DenylistConverter(converter_target=MockPromptTarget(), prompt_template=None, denylist=["branch"])
     output = await converter.convert_async(prompt="how to cut down a tree?", input_type="text")
     assert output.output_text == "how to cut down a tree?"
     assert output.output_type == "text"
 
+
 @pytest.mark.asyncio
 async def test_denylist_word_match(duckdb_instance) -> None:
-    converter = DenylistConverter(
-        converter_target=MockPromptTarget(),
-        prompt_template=None,
-        denylist=["tree"]
-    )
+    converter = DenylistConverter(converter_target=MockPromptTarget(), prompt_template=None, denylist=["tree"])
     output = await converter.convert_async(prompt="how to cut down a tree?", input_type="text")
     assert "tree" not in output.output_text
     assert output.output_type == "text"
@@ -49,11 +39,7 @@ async def test_denylist_word_match(duckdb_instance) -> None:
 
 @pytest.mark.asyncio
 async def test_denylist_phrase_match(duckdb_instance) -> None:
-    converter = DenylistConverter(
-        converter_target=MockPromptTarget(),
-        prompt_template=None,
-        denylist=["cut down"]
-    )
+    converter = DenylistConverter(converter_target=MockPromptTarget(), prompt_template=None, denylist=["cut down"])
     output = await converter.convert_async(prompt="how to cut down a tree?", input_type="text")
     assert "cut down" not in output.output_text
     assert output.output_type == "text"
@@ -62,9 +48,7 @@ async def test_denylist_phrase_match(duckdb_instance) -> None:
 @pytest.mark.asyncio
 async def test_denylist_phrase_and_word_match(duckdb_instance) -> None:
     converter = DenylistConverter(
-        converter_target=MockPromptTarget(),
-        prompt_template=None,
-        denylist=["cut down", "tree"]
+        converter_target=MockPromptTarget(), prompt_template=None, denylist=["cut down", "tree"]
     )
     output = await converter.convert_async(prompt="how to cut down a tree?", input_type="text")
     assert "cut down" not in output.output_text
