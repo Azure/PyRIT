dcc091757e7f267defba5d56784b924800a10012
Nina Chikanov <nichikan@microsoft.com>
2025-05-28 01:12:35 +0000

    add integration test files + pre-commit

--- tests/integration/cli/test_cli_integration.py
+++ tests/integration/cli/test_cli_integration.py
@@ -3,6 +3,7 @@
 
 import os
 import pathlib
+import re
 import shlex
 import tempfile
 from typing import Literal
@@ -12,12 +13,13 @@ import yaml
 
 from pyrit.cli.__main__ import main
 from pyrit.common.path import DATASETS_PATH
-from pyrit.models.seed_prompt import SeedPrompt
-
 
 test_cases_success = [
     "--config-file 'tests/integration/cli/mixed_multiple_orchestrators_args_success.yaml'",
     "--config-file 'tests/integration/cli/prompt_send_success.yaml'",
+    "--config-file 'tests/unit/cli/prompt_send_success_converters_custom_target.yaml'",
+    "--config-file 'tests/unit/cli/prompt_send_success_converters_no_target.yaml'",
+    "--config-file 'tests/integration/cli/multiple_converters_success.yaml'",
 ]
 
 
@@ -95,7 +97,9 @@ converters = [
         "text",
         {
             "type": "FuzzerCrossOverConverter",
-            "prompt_templates": [str(pathlib.Path(DATASETS_PATH, "prompt_converters", "fuzzer_converters", "expand_converter.yaml"))],
+            "prompt_templates": [
+                str(pathlib.Path(DATASETS_PATH, "prompt_converters", "fuzzer_converters", "expand_converter.yaml"))
+            ],
         },
     ),
     (
@@ -240,11 +244,12 @@ converters = [
     ),
 ]
 
-def _create_data(data_type: str) -> dict[str, dict[str, str]|list[dict[str, str]]]:
+
+def _create_data(data_type: str) -> dict[str, dict[str, str] | list[dict[str, str]]]:
     data = {
         "scenarios": [{"type": "PromptSendingOrchestrator"}],
         "adversarial_chat": {"type": "OpenAIChatTarget"},
-        "objective_target": {"type": "TextTarget"},
+        "objective_target": {"type": "OpenAIChatTarget"},
         "database": {"type": "DuckDB"},
     }
 
@@ -254,22 +259,46 @@ def _create_data(data_type: str) -> dict[str, dict[str, str]|list[dict[str, str]
         data["datasets"] = ["./tests/integration/cli/illegal_integration_test_image_input.prompt"]
     elif data_type == "audio":
         data["datasets"] = ["./tests/integration/cli/illegal_integration_test_audio_input.prompt"]
-    
+
     return data
 
+
 @pytest.mark.parametrize("input_type, converter_data", converters)
-def test_cli_integration_success_converters(input_type: Literal["text", "image", "audio"], converter_data: dict[str, str]):
+def test_cli_integration_success_converters_all(
+    input_type: Literal["text", "image", "audio"], converter_data: dict[str, str]
+):
     # scenario: verify converter functionality with all converters as of v0.9.0
     # adversarial_chat available as default converter_target when necessary
     # converters that do not need a target also included
     data = _create_data(input_type)
     data["converters"] = [converter_data]
 
+    use_text_target_types = ["PDFConverter", "AddImageVideoConverter", "AudioFrequencyConverter"]
+    if converter_data["type"] in use_text_target_types:
+        # OpenAIChatTarget only allows text and image_path data types as input, and these converters
+        # output different data types so we need to use TextTarget
+        data["objective_target"] = {"type": "TextTarget"}
+
     # create temp yaml file for each converter
-    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".yaml") as temp_file:
+    with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".yaml") as temp_file:
         yaml.dump(data, temp_file)
         main(shlex.split(s=f"--config-file '{temp_file.name}'"))
 
         # clean up temp file
         temp_file.close()
         os.remove(temp_file.name)
+
+
+test_cases_error = [
+    (
+        "--config-file 'tests/integration/cli/prompt_send_converters_wrong_data_type.yaml'",
+        "Input type not supported",
+        ValueError,
+    ),
+]
+
+
+@pytest.mark.parametrize("command, expected_output, error_type", test_cases_error)
+def test_cli_integration_error(command, expected_output, error_type):
+    with pytest.raises(error_type, match=re.escape(expected_output)):
+        main(shlex.split(command))
