3ebc059746c4467212f1e3629a464b615cb9dc98
Roman Lutz <romanlutz13@gmail.com>
2025-06-23 21:34:53 -0700

    Update doc/code/scoring/prompt_shield_scorer.py

--- doc/code/scoring/prompt_shield_scorer.py
+++ doc/code/scoring/prompt_shield_scorer.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.17.1
 #   kernelspec:
-#     display_name: pyrit-python313-fresh2
+#     display_name: pyrit-dev
 #     language: python
 #     name: python3
 # ---
