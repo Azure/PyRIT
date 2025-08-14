8aeca3bb999c2e14a0130a05f9d8601d4882ff46
jsong468 <songjustin@microsoft.com>
2025-08-13 17:41:38 -0700

    Sora docstring pt 5

--- pyrit/prompt_target/openai/openai_sora_target.py
+++ pyrit/prompt_target/openai/openai_sora_target.py
@@ -118,8 +118,8 @@ class OpenAISoraTarget(OpenAITarget):
             n_seconds (int, Optional): The duration of the generated video (in seconds). Defaults to 5.
                 Sora API will support duration up to 20s. For 1080p, maximum duration is 10s.
             n_variants (int, Optional): The number of generated videos. Defaults to 1.
-                Sora API will support up to 2 variants for resolutions of 720p, but only 1 for resolutions
-                of 1080p.
+                Sora API will support up to 2 variants for resolutions of 720p but only 1 for resolutions of
+                1080p.
             output_filename (str, Optional): The name of the output file for the generated video.
                 DO NOT SET if using target with PromptSendingOrchestrator. The default filename is
                 `{job_id}_{generation_id}.mp4` as returned by the model.
