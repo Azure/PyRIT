9394538bde5ec3206c63f1d381a1a32b62efde2b
Victor Valbuena <vvalbuena@microsoft.com>
2025-09-02 14:40:49 -0700

    Precommit

--- pyrit/score/look_back_scorer.py
+++ pyrit/score/look_back_scorer.py
@@ -32,7 +32,7 @@ class LookBackScorer(Scorer):
         self.exclude_instruction_prompts = exclude_instruction_prompts
 
         behavior_change_prompt_path = Path(SCORER_CONFIG_PATH, "scales", "behavior_change_system_prompt.yaml").resolve()
-        behavior_change_scale_path = Path(SCORER_CONFIG_PATH, "likert_scales", "behavior_change.yaml")
+        behavior_change_scale_path = Path(SCORER_CONFIG_PATH, "likert_scales", "behavior_change.yaml").resolve()
         behavior_change_scale = yaml.safe_load(behavior_change_scale_path.read_text(encoding="utf-8"))
 
         scoring_instructions_template = SeedPrompt.from_yaml_file(behavior_change_prompt_path)
