50c1d644e373b991f395dc80b6d53cc8db116b83
PHD Student <45600306+ComputerScienceMasterStudent@users.noreply.github.com>
2025-09-21 15:20:29 +0300

    Update __init__.py

--- pyrit/executor/attack/single_turn/__init__.py
+++ pyrit/executor/attack/single_turn/__init__.py
@@ -9,6 +9,7 @@ from pyrit.executor.attack.single_turn.single_turn_attack_strategy import (
 from pyrit.executor.attack.single_turn.prompt_sending import PromptSendingAttack
 from pyrit.executor.attack.single_turn.context_compliance import ContextComplianceAttack
 from pyrit.executor.attack.single_turn.flip_attack import FlipAttack
+from pyrit.executor.attack.single_turn.implicare_attack import ImplicareAttack
 from pyrit.executor.attack.single_turn.many_shot_jailbreak import ManyShotJailbreakAttack
 from pyrit.executor.attack.single_turn.role_play import RolePlayAttack, RolePlayPaths
 from pyrit.executor.attack.single_turn.skeleton_key import SkeletonKeyAttack
