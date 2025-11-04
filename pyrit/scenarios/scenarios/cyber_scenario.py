f008d701e52db7c91a66aa03757aee510f1d0d6c
Victor Valbuena <vvalbuena@microsoft.com>
2025-11-04 14:59:19 -0800

    Adding cyber scenario.

---
+++ pyrit/scenarios/scenarios/cyber_scenario.py
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Cyber scenario implementation.
+
+This module provides a scenario that demonstrates how a model can be
+broken to provide support in escalating privileges the user should not have.
+"""
+
+...
