373148803befa510c7a7f2fba16756f7d114023e
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-11-15 18:14:28 +0100

    use google convention

--- pyrit/auxiliary_attacks/gcg/attack/gcg/gcg_attack.py
+++ pyrit/auxiliary_attacks/gcg/attack/gcg/gcg_attack.py
@@ -24,18 +24,17 @@ def token_gradients(model, input_ids, input_slice, target_slice, loss_slice):
     """
     Computes gradients of the loss with respect to the coordinates.
 
-    Parameters
-    ----------
-    model : Transformer Model
-        The transformer model to be used.
-    input_ids : torch.Tensor
-        The input sequence in the form of token ids.
-    input_slice : slice
-        The slice of the input sequence for which gradients need to be computed.
-    target_slice : slice
-        The slice of the input sequence to be used as targets.
-    loss_slice : slice
-        The slice of the logits to be used for computing the loss.
+    Args:
+        model (Transformer Model):
+            The transformer model to be used.
+        input_ids (torch.Tensor):
+            The input sequence in the form of token ids.
+        input_slice (slice):
+            The slice of the input sequence for which gradients need to be computed.
+        target_slice (slice):
+            The slice of the input sequence to be used as targets.
+        loss_slice (slice):
+            The slice of the logits to be used for computing the loss.
 
     Returns:
         torch.Tensor: The gradients of each token in the input_slice with respect to the loss.
