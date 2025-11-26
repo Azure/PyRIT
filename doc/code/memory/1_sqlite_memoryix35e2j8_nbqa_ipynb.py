a0151ca5a3fafd1fbb2ea07bf212850117ade6c4
Richard Lundeen <rlundeen@microsoft.com>
2025-11-25 20:36:29 -0800

    deleting temp files

--- doc/code/memory/1_sqlite_memoryix35e2j8_nbqa_ipynb.py
+++
@@ -1,8 +0,0 @@
-# %%NBQA-CELL-SEP52c935
-from pyrit.memory import SQLiteMemory
-
-# Use in-memory database to avoid file corruption issues
-memory = SQLiteMemory(db_path=":memory:")
-memory.print_schema()
-
-memory.dispose_engine()
