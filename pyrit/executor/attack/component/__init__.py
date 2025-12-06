49f673a527f1a841801feab4c9858ca80ac7214a
eve1ne <113043175+eve1ne@users.noreply.github.com>
2025-12-06 17:43:02 -0500

    MAINT: Fix docstrings for pyrit/executor modules (partial #1176) (#1225)

--- pyrit/executor/attack/component/__init__.py
+++ pyrit/executor/attack/component/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+"""Attack components module."""
+
 from pyrit.executor.attack.component.conversation_manager import ConversationManager, ConversationState
 from pyrit.executor.attack.component.objective_evaluator import ObjectiveEvaluator
 
