64185cd621f609f5d7dadd6f9cb4571c007dfa0a
Royce Moon <71098497+0xm00n@users.noreply.github.com>
2025-12-19 19:46:53 -0500

    Update __init__.py
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

--- pyrit/datasets/__init__.py
+++ pyrit/datasets/__init__.py
@@ -5,7 +5,6 @@
 Dataset fetching and loading utilities for various red teaming and safety evaluation datasets.
 """
 
-from pyrit.datasets.anthropic_evals_dataset import fetch_anthropic_evals_dataset
 from pyrit.datasets.jailbreak.text_jailbreak import TextJailBreak
 from pyrit.datasets.seed_datasets.seed_dataset_provider import SeedDatasetProvider
 from pyrit.datasets.seed_datasets import local, remote  # noqa: F401
