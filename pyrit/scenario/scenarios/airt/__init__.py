41dab62601335f99f4dc3744edf2a9039a35f56d
Varun Joginpalli <vjoginpalli@microsoft.com>
2025-12-24 05:38:11 +0000

    Leakage Scenario addition

--- pyrit/scenario/scenarios/airt/__init__.py
+++ pyrit/scenario/scenarios/airt/__init__.py
@@ -3,15 +3,18 @@
 
 """AIRT scenario classes."""
 
-from pyrit.scenario.scenarios.airt.cyber_scenario import CyberScenario, CyberStrategy
 from pyrit.scenario.scenarios.airt.content_harms_scenario import (
     ContentHarmsScenario,
     ContentHarmsStrategy,
 )
+from pyrit.scenario.scenarios.airt.cyber_scenario import CyberScenario, CyberStrategy
+from pyrit.scenario.scenarios.airt.leakage_scenario import LeakageScenario, LeakageStrategy
 
 __all__ = [
-    "CyberScenario",
-    "CyberStrategy",
     "ContentHarmsScenario",
     "ContentHarmsStrategy",
+    "CyberScenario",
+    "CyberStrategy",
+    "LeakageScenario",
+    "LeakageStrategy",
 ]
