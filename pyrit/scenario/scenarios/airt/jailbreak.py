b14f564fd7a75733141725e6d99c4ce947996e67
Victor Valbuena <vvalbuena@microsoft.com>
2026-02-14 00:30:05 +0000

    adversarial chat

--- pyrit/scenario/scenarios/airt/jailbreak.py
+++ pyrit/scenario/scenarios/airt/jailbreak.py
@@ -275,6 +275,12 @@ class Jailbreak(Scenario):
             case "skeleton":
                 attack = SkeletonKeyAttack(**args)
             case "role_play":
+                args["adversarial_chat"] = OpenAIChatTarget(
+                    endpoint=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_ENDPOINT"),
+                    api_key=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_KEY"),
+                    model_name=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_MODEL"),
+                    temperature=1.2,
+                )
                 attack = RolePlayAttack(**args)
             case _:
                 raise ValueError(f"Unknown JailbreakStrategy `{strategy}`.")
