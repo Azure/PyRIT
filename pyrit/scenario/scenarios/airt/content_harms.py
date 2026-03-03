b1955e6c1458143a941e42a5a93f8a83868a0661
hannahwestra25 <hannahwestra@microsoft.com>
2026-03-03 11:41:44 -0500

    update target and scorer

--- pyrit/scenario/scenarios/airt/content_harms.py
+++ pyrit/scenario/scenarios/airt/content_harms.py
@@ -6,6 +6,7 @@ import os
 from collections.abc import Sequence
 from typing import Any, Optional, TypeVar
 
+from pyrit.auth import get_azure_openai_auth
 from pyrit.common import apply_defaults
 from pyrit.common.deprecation import print_deprecation_message
 from pyrit.executor.attack import (
@@ -197,20 +198,22 @@ class ContentHarms(Scenario):
         self._objectives_by_harm = objectives_by_harm
 
     def _get_default_adversarial_target(self) -> OpenAIChatTarget:
+        endpoint = os.environ.get("AZURE_OPENAI_GPT4_CHAT_ENDPOINT")
         return OpenAIChatTarget(
-            endpoint=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_ENDPOINT"),
-            api_key=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_KEY"),
-            model_name=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_MODEL"),
+            endpoint=endpoint,
+            api_key=get_azure_openai_auth(endpoint), #type: ignore
+            model_name=os.environ.get("AZURE_OPENAI_GPT4_CHAT_MODEL"),
             temperature=1.2,
         )
 
     def _get_default_scorer(self) -> TrueFalseInverterScorer:
+        endpoint = os.environ.get("AZURE_OPENAI_GPT4_CHAT_ENDPOINT")
         return TrueFalseInverterScorer(
             scorer=SelfAskRefusalScorer(
                 chat_target=OpenAIChatTarget(
-                    endpoint=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_ENDPOINT"),
-                    api_key=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_KEY"),
-                    model_name=os.environ.get("AZURE_OPENAI_GPT4O_UNSAFE_CHAT_MODEL"),
+                    endpoint=endpoint,
+                    api_key=get_azure_openai_auth(endpoint), #type: ignore
+                    model_name=os.environ.get("AZURE_OPENAI_GPT4_CHAT_MODEL"),
                     temperature=0.9,
                 )
             ),
